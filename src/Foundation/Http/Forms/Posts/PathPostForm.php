c0a77d3742b493feae899715d60d19d3b82d6653
Pertsev Roman <pertsev.roman@gmail.com>
2016-12-19 14:13:31 +0300

    Сохранение открытие маршрутов

--- src/Foundation/Http/Forms/Posts/PathPostForm.php
+++ src/Foundation/Http/Forms/Posts/PathPostForm.php
@@ -2,6 +2,7 @@
 
 namespace Orchid\Foundation\Http\Forms\Posts;
 
+use Orchid\Foundation\Core\Models\Post;
 use Orchid\Foundation\Services\Forms\Form;
 
 class PathPostForm extends Form
@@ -17,26 +18,37 @@ class PathPostForm extends Form
      * @param Post|null $post
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function get($type = null, Post $post = null)
-    {
+    public function get($type = null, Post $post = null) {
+        $route_json = null;
+
+        if($post != null) {
+            $content = $post->getAttribute('content');
+
+            $route = $content['route'];
+
+            $route_json = json_encode($route);
+
+//            dd($route_json);
+        }
+
         return view('dashboard::container.posts.modules.path', [
+            'route' => $route_json
         ]);
     }
 
     /**
      * @param null $type
      * @param null $post
+     * @return mixed|void
      */
     public function persist($type = null, $post = null)
     {
-        dd($this->request->all());
-
         $route = $this->request->input('route');
 
         $content = $post->content;
 
         foreach ($content as $lang => $item) {
-            $content[$lang]['route'] = json_decode($route);
+            $content['route'] = json_decode($route);
         }
 
         $post->content = $content;
--- src/Foundation/Resources/views/container/posts/modules/path.blade.php
+++ src/Foundation/Resources/views/container/posts/modules/path.blade.php
@@ -1,5 +1,9 @@
 <div class="form-group">
     <div id="route-app" class="input-group w-full">
-        <path-input field-name="route" field-value='[]'></path-input>
+        @if ($route != null)
+            <path-input field-name="route" field-value='{{$route}}'></path-input>
+        @else
+            <path-input field-name="route" field-value='[]'></path-input>
+        @endif
     </div>
 </div>
