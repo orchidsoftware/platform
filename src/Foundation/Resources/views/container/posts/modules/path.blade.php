ca5c6d9ce255ccf147e6fb2b51995f676eaf1080
Pertsev Roman <pertsev.roman@gmail.com>
2016-12-23 14:05:42 +0300

    доработка сохранения-отображения роутинга

--- src/Foundation/Resources/views/container/posts/modules/path.blade.php
+++ src/Foundation/Resources/views/container/posts/modules/path.blade.php
@@ -1,9 +1,5 @@
 <div class="form-group">
     <div id="route-app" class="input-group w-full">
-        @if ($route != null)
-            <path-input field-name="route" icons-resource="{{route('dashboard::icons')}}" template="{{route('dashboard::partials.path')}}" field-value='{{$route}}'></path-input>
-        @else
-            <path-input field-name="route" icons-resource="{{route('dashboard::icons')}}" template="{{route('dashboard::partials.path')}}" field-value='[]'></path-input>
-        @endif
+        <path-input field-name="route" icons-resource="{{route('dashboard::icons')}}" template="{{route('dashboard::partials.path')}}" field-value='{{$route}}'></path-input>
     </div>
 </div>
