d1a7cb290b98cc8e3de7c8acd060da70abdd5d47
tabuna <bliz48rus@gmail.com>
2017-01-21 01:33:38 +0300

    Unused use statement should be avoided

--- src/Foundation/Core/Models/Post.php
+++ src/Foundation/Core/Models/Post.php
@@ -78,6 +78,9 @@ class Post extends Model
         return $this->where('type', $this->dataType->slug);
     }
 
+    /**
+     * @return null
+     */
     public function getTypeObject()
     {
         if (!is_null($this->dataType)) {
