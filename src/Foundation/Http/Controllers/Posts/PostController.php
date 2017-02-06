cf0ac7846f20360cff9a627a42b21e31d082c84a
tabuna <bliz48rus@gmail.com>
2017-02-06 14:48:20 +0300

    Фильтр для уникальных значений

--- src/Foundation/Http/Controllers/Posts/PostController.php
+++ src/Foundation/Http/Controllers/Posts/PostController.php
@@ -73,7 +73,7 @@ class PostController extends Controller
 
         $Slugs = $post->where('slug', $post->slug)->count();
         if ($Slugs != 0) {
-            $post->slug = $post->slug.'-'.($Slugs + 1);
+            $post->slug = $post->slug.'-'.($Slugs + $Slugs);
         }
 
         $post->save();
