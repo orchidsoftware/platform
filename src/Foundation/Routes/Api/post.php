ac29fcc0a0cc69a04257df8dca64fe056a52b644
Alexandr <bliz48rus@gmail.com>
2017-02-07 17:36:17 +0300

    Apply fixes from StyleCI (#130)
    [ci skip] [skip ci]

--- src/Foundation/Routes/Api/post.php
+++ src/Foundation/Routes/Api/post.php
@@ -10,15 +10,12 @@
 */
 
 Route::group(['middleware' => ['api'], 'prefix'=> 'api', 'namespace' => 'Orchid\Foundation\Http\Controllers\Api'], function ($router) {
+    Route::get('type/{type?}', 'PostApiController@index');
+    Route::get('type/{type}/{slug}', 'PostApiController@show');
+    Route::post('type/{type}', 'PostApiController@store');
 
+    Route::get('category', 'CategoryApiController@store');
+    Route::get('category/{slug}', 'CategoryApiController@show');
 
-    Route::get('type/{type?}','PostApiController@index');
-    Route::get('type/{type}/{slug}','PostApiController@show');
-    Route::post('type/{type}','PostApiController@store');
-
-    Route::get('category','CategoryApiController@store');
-    Route::get('category/{slug}','CategoryApiController@show');
-
-    Route::get('tags','TagsApiController@index');
-
+    Route::get('tags', 'TagsApiController@index');
 });
