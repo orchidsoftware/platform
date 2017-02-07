fb847919769c443ae3d7cb2509cf25ab88341b57
Alexandr <bliz48rus@gmail.com>
2017-02-08 00:49:30 +0300

    Update post.php

--- src/Foundation/Routes/Api/post.php
+++ src/Foundation/Routes/Api/post.php
@@ -10,6 +10,8 @@
 */
 
 Route::group(['middleware' => ['api'], 'prefix'=> 'api', 'namespace' => 'Orchid\Foundation\Http\Controllers\Api'], function ($router) {
+   
+    /*
     Route::get('type/{type?}', 'PostApiController@index');
     Route::get('type/{type}/{slug}', 'PostApiController@show');
     Route::post('type/{type}', 'PostApiController@store');
@@ -18,4 +20,5 @@ Route::group(['middleware' => ['api'], 'prefix'=> 'api', 'namespace' => 'Orchid\
     Route::get('category/{slug}', 'CategoryApiController@show');
 
     Route::get('tags', 'TagsApiController@index');
+    */
 });
