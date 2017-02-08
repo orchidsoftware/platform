669858e240db44cb046341afbe663a09060ec6dc
tabuna <bliz48rus@gmail.com>
2017-02-08 13:55:40 +0300

    fix menu

--- src/Foundation/Routes/Dashboard/auth.php
+++ src/Foundation/Routes/Dashboard/auth.php
@@ -10,7 +10,7 @@
 */
 
 $this->group([
-    'middleware' => ['web', 'dashboard'],
+    'middleware' => ['web'],
     'prefix'     => 'dashboard',
     'namespace'  => 'Orchid\Foundation\Http\Controllers\Auth',
 ],
