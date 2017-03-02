57401e3cabd6df9a0c78292cbe43a595210e6845
tabuna <bliz48rus@gmail.com>
2017-03-02 10:54:45 +0300

    monitor support

--- src/Foundation/Http/Controllers/Systems/MonitorController.php
+++ src/Foundation/Http/Controllers/Systems/MonitorController.php
@@ -5,6 +5,7 @@ namespace Orchid\Foundation\Http\Controllers\Systems;
 use Orchid\Foundation\Http\Controllers\Controller;
 use Orchid\Monitor\Monitor;
 
+
 class MonitorController extends Controller
 {
     /**
@@ -20,6 +21,12 @@ class MonitorController extends Controller
      */
     public function index()
     {
+
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' || $this->shell_exec_enabled() == false) {
+            return view('dashboard::container.systems.monitor.disable',[]);
+        }
+
+
         $monitor = new Monitor();
 
         return view('dashboard::container.systems.monitor.index', [
@@ -31,4 +38,15 @@ class MonitorController extends Controller
             'storage'     => $monitor->storage(),
         ]);
     }
+
+
+    /**
+     * @return bool
+     */
+    private function shell_exec_enabled()
+    {
+        $disabled = explode(',', ini_get('disable_functions'));
+        return !in_array('shell_exec', $disabled);
+    }
+
 }
