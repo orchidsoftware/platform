36c77d46a0553c60d891c14fcd10225db6c530dd
tabuna <bliz48rus@gmail.com>
2017-03-22 12:35:59 +0300

    Newsletter

---
+++ app/Core/Models/Newsletter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Orchid\Core\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
+
+class Newsletter extends Model
+{
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'email',
+        'lang',
+    ];
+
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [
+        'id',
+        'updated_at',
+        'created_at',
+    ];
+
+    /**
+     * @param Newsletter $newsletter
+     * @return Newsletter
+     */
+    public function creating(Newsletter $newsletter)
+    {
+        if (is_null($newsletter->lang)) {
+            $newsletter->lang = App::getLocale();
+        }
+        return $newsletter;
+    }
+
+}
---
+++ resources/stubs/database/migrations/2017_03_20_222746_create_newsletter_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+
+class CreateNewsletterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('newsletters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email')->unqiue();
+            $table->string('lang')->default(config_path('app.locale'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('newsletters');
+    }
+}
