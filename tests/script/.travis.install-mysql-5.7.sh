a54adb4873740f926b9788bee309d24c9a0df3d8
tabuna <bliz48rus@gmail.com>
2017-04-12 18:31:57 +0300

    travis test

---
+++ tests/script/.travis.install-mysql-5.7.sh
@@ -0,0 +1,7 @@
+echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 |
+sudo debconf-set-selections
+wget http://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
+sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
+sudo apt-get update -q
+sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
+sudo mysql_upgrade
