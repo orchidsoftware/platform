3928a1a3a7390d9a3aae7fbb83b1c69c664a5159
tabuna <bliz48rus@gmail.com>
2017-06-15 11:43:32 +0300

    revert cache page

--- resources/views/container/systems/cache/index.blade.php
+++ resources/views/container/systems/cache/index.blade.php
@@ -11,101 +11,84 @@
 @section('content')
 
 
-    <!-- main content  -->
+
+    <!-- main content -->
     <section class="wrapper">
+        <div class="bg-white-only bg-auto no-border-xs text-center">
 
 
+            <div class="col-xs-4">
 
-        <div class="row">
-            <div class="col-md-12">
-                <h1>Кэш</h1>
-            </div>
-        </div>
-
-
-            <div class="row">
-
-                
-                <div class="col-xs-2">
-                    <div class="bg-white-only bg-auto no-border-xs text-center">
+                <div class="row">
                     <form action="{{route('dashboard.systems.cache')}}" method="POST">
-                        <button class="bg-white b padder-v item w-full" type="submit">
+                        <button class="panel padder-v item w-full" type="submit">
                             <div class="h4 text-info font-thin ">Очистить кэш</div>
                             <span class="text-muted text-xs">Flush the application cache</span>
                         </button>
                         <input name="action" type="hidden" value="cache">
                         {{csrf_field()}}
                     </form>
-                    </div>
                 </div>
 
-                <div class="col-xs-2">
-                    <div class="bg-white-only bg-auto no-border-xs text-center">
-                        <form action="{{route('dashboard.systems.cache')}}" method="POST">
-                            <button class="bg-white b padder-v item  w-full" type="submit">
-                                <div class="h4 text-info font-thin">Очистить настройки конфигурации</div>
-                                <span class="text-muted text-xs">Create a cache file for faster configuration loading</span>
-                            </button>
-                            <input name="action" type="hidden" value="config">
-                            {{csrf_field()}}
-                        </form>
-                    </div>
+                <div class="row">
+                    <form action="{{route('dashboard.systems.cache')}}" method="POST">
+                        <button class="panel padder-v item  w-full" type="submit">
+                            <div class="h4 text-info font-thin">Очистить настройки конфигурации</div>
+                            <span class="text-muted text-xs">Create a cache file for faster configuration loading</span>
+                        </button>
+                        <input name="action" type="hidden" value="config">
+                        {{csrf_field()}}
+                    </form>
                 </div>
 
-                <div class="col-xs-2">
-                    <div class="bg-white-only bg-auto no-border-xs text-center">
-                        <form action="{{route('dashboard.systems.cache')}}" method="POST">
-                            <button class="bg-white b padder-v item  w-full" type="submit">
-                                <div class="h4 text-info font-thin">Очистить маршруты</div>
-                                <span class="text-muted text-xs">Create a route cache file for faster route registration</span>
+                <div class="row">
 
-                            </button>
-                            <input name="action" type="hidden" value="route">
-                            {{csrf_field()}}
-                        </form>
-                    </div>
+                    <form action="{{route('dashboard.systems.cache')}}" method="POST">
+                        <button class="panel padder-v item  w-full" type="submit">
+                            <div class="h4 text-info font-thin">Очистить маршруты</div>
+                            <span class="text-muted text-xs">Create a route cache file for faster route registration</span>
+
+                        </button>
+                        <input name="action" type="hidden" value="route">
+                        {{csrf_field()}}
+                    </form>
+
                 </div>
+                <div class="row">
 
-                <div class="col-xs-2">
-                    <div class="bg-white-only bg-auto no-border-xs text-center">
-                        <form action="{{route('dashboard.systems.cache')}}" method="POST">
-                            <button class="bg-white b padder-v item  w-full" type="submit">
-                                <div class="h4 text-info font-thin">Очистить отображаемые файлы</div>
-                                <span class="text-muted text-xs">Clear all compiled view files</span>
+                    <form action="{{route('dashboard.systems.cache')}}" method="POST">
+                        <button class="panel padder-v item  w-full" type="submit">
+                            <div class="h4 text-info font-thin">Очистить отображаемые файлы</div>
+                            <span class="text-muted text-xs">Clear all compiled view files</span>
 
-                            </button>
-                            <input name="action" type="hidden" value="view">
-                            {{csrf_field()}}
-                        </form>
-                    </div>
+                        </button>
+                        <input name="action" type="hidden" value="view">
+                        {{csrf_field()}}
+                    </form>
+
                 </div>
+                <div class="row">
 
+                    <form action="{{route('dashboard.systems.cache')}}" method="POST">
+                        <button class="panel padder-v item  w-full" type="submit">
+                            <div class="h4 text-info font-thin">Очистить opcache</div>
+                            <span class="text-muted text-xs">Resets the contents of the opcode cache</span>
 
+                        </button>
+                        <input name="action" type="hidden" value="opcache">
+                        {{csrf_field()}}
+                    </form>
 
-                <div class="col-xs-2">
-                    <div class="bg-white-only bg-auto no-border-xs text-center">
-                        <form action="{{route('dashboard.systems.cache')}}" method="POST">
-                            <button class="bg-white b padder-v item  w-full" type="submit">
-                                <div class="h4 text-info font-thin">Очистить opcache</div>
-                                <span class="text-muted text-xs">Resets the contents of the opcode cache</span>
-
-                            </button>
-                            <input name="action" type="hidden" value="opcache">
-                            {{csrf_field()}}
-                        </form>
-                    </div>
                 </div>
-
-
             </div>
 
 
-
-
+        </div>
 
 
     </section>
-    <!-- / main content  -->
+    <!-- / main content -->
+
 
 
 @stop
