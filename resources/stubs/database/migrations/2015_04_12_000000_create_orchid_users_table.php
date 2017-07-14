10a8dd2af352e57f812dfcfedc4d70cbb715abdd
tabuna <bliz48rus@gmail.com>
2017-07-14 07:18:16 +0300

    fix migration

--- resources/stubs/database/migrations/2015_04_12_000000_create_orchid_users_table.php
+++ resources/stubs/database/migrations/2015_04_12_000000_create_orchid_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateUsersTable extends Migration
+class CreateOrchidUsersTable extends Migration
 {
     /**
      * Run the migrations.
