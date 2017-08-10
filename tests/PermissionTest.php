3b9f845aa0e8617c0ced38c4e0cc4990e839606d
Alexandr Chernyaev <bliz48rus@gmail.com>
2017-08-10 04:04:53 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/PermissionTest.php
+++ tests/PermissionTest.php
@@ -7,33 +7,25 @@ use Orchid\Platform\Core\Models\User;
 
 class PermissionTest extends TestCase
 {
-
     /**
-     * Verify permissions
+     * Verify permissions.
      */
     public function test_is_permission()
     {
         $user = $this->createUser();
 
-
         $this->assertEquals($user->hasAccess('access.to.public.data'), true);
         $this->assertEquals($user->hasAccess('access.to.secret.data'), false);
         $this->assertEquals($user->hasAccess('access.roles.to.public.data'), false);
 
-
         $role = $this->createRole();
-
 
         //$this->assertEquals($user->inRole($role),false);
 
         $user->addRole($role);
 
-
         $this->assertEquals($user->hasAccess('access.roles.to.public.data'), true);
-
-
     }
-
 
     /**
      * @return $this|\Illuminate\Database\Eloquent\Model
