5f65e2b190f044b82d3faca0a6eaad7681839cd9
tabuna <bliz48rus@gmail.com>
2017-08-13 14:20:23 +0300

    format

--- src/Access/RoleInterface.php
+++ src/Access/RoleInterface.php
@@ -5,20 +5,6 @@ namespace Orchid\Platform\Access;
 interface RoleInterface
 {
     /**
-     * Returns the role's primary key.
-     *
-     * @return int
-     */
-    public function getRoleId() : int;
-
-    /**
-     * Returns the role's slug.
-     *
-     * @return string
-     */
-    public function getRoleSlug() : string;
-
-    /**
      * Returns all users for the role.
      *
      * @return \IteratorAggregate
