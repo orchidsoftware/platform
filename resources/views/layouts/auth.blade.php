3f0ea9031564e9c88e5eb66c4f335f0388f2246d
tabuna <bliz48rus@gmail.com>
2017-11-16 00:23:21 +0300

    default title uppercase

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>@yield('title','Orchid')</title>
+    <title>@yield('title','ORCHID')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
