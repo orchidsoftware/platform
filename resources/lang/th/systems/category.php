b6f12cf0054743d42318121e5729a1f41ab9b141
Nattanan <reborn_conan_t@hotmail.com>
2017-12-14 02:38:02 +0700

    add systems files Thai language (#373)
    * Thai Language

---
+++ resources/lang/th/systems/category.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'title'       => 'หมวด',
+    'description' => 'หมวดของเว็บไซต์',
+
+    'information' => 'ข้อมูล',
+    'display'     => 'แสดงผล',
+
+    'parent'       => 'หมวดหมู่หลัก',
+    'slug'         => 'ลิงค์',
+    'name'         => 'ชื่อ',
+    'descriptions' => 'ลักษณะ',
+    'not_found'    => 'คุณยังไม่ได้สร้างหมวด',
+    'not_parrent'  => 'โดยไม่ต้องแสดง',
+];
