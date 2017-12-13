f18fd60349bbc0ff0fc59f031e35a7b748909b7b
tonyumme <reborn_conan_t@hotmail.com>
2017-12-13 16:03:05 +0700

    add thai language
    add systems  files thai language

---
+++ resources/lang/th/systems/users.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    'tabs'               => [
+        'information' => 'ข้อมูล',
+        'permission'  => 'อนุญาต',
+    ],
+    'title'              => 'ผู้ใช้งาน',
+    'description'        => 'ผู้ใช้ที่ลงทะเบียนทั้งหมด',
+    'not_found'          => 'ไม่พบผู้ใช้',
+    'create'             => 'สร้าง',
+    'name'               => 'ชื่อ',
+    'email'              => 'อีเมล์',
+    'password'           => 'รหัสผ่าน',
+    'confirmed_password' => 'ยืนยันรหัสผ่าน',
+    'roles'              => 'บทบาท',
+    'select_roles'       => 'เลือกบทบาท',
+
+];
