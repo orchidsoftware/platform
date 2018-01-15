d80c0a50a9ac2c0f13e04b39b3fe6d9e040a3126
Alexandr Chernyaev <bliz48rus@gmail.com>
2018-01-15 19:14:15 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- resources/assets/js/modules/csrf_token.js
+++ resources/assets/js/modules/csrf_token.js
@@ -4,20 +4,20 @@
  * a simple convenience so we don't have to attach every token manually.
  */
 
-document.addEventListener('turbolinks:load', function () {
-    let token = document.head.querySelector('meta[name="csrf_token"]');
+document.addEventListener('turbolinks:load', function() {
+  let token = document.head.querySelector('meta[name="csrf_token"]');
 
-    if (token) {
-        window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-    } else {
-        console.error(
-            'CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token',
-        );
-    }
+  if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+  } else {
+    console.error(
+      'CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token',
+    );
+  }
 
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf_token"]').attr('content'),
-        },
-    });
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-TOKEN': $('meta[name="csrf_token"]').attr('content'),
+    },
+  });
 });
