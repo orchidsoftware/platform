5663006453dc111d592a202f69aa0cf5f8362fd2
tabuna <bliz48rus@gmail.com>
2018-01-20 19:40:14 +0300

    favicon

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -9,12 +9,14 @@
     <meta name="auth" content="{{Auth::check()}}">
     <link rel="stylesheet" type="text/css" href="{{mix('/css/orchid.css','orchid')}}">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/orchid/apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="/orchid/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/orchid/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="/orchid/manifest.json">
-    <link rel="mask-icon" href="/orchid/safari-pinned-tab.svg" color="#ac5ca0">
-    <meta name="theme-color" content="#f8f9fa">
+    <link rel="apple-touch-icon" sizes="180x180" href="/orchid/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/orchid/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/orchid/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/orchid/favicon/manifest.json">
+    <link rel="mask-icon" href="/orchid/favicon/safari-pinned-tab.svg" color="#1a2021">
+    <meta name="apple-mobile-web-app-title" content="ORCHID">
+    <meta name="application-name" content="ORCHID">
+    <meta name="theme-color" content="#ffffff">
 
     <meta name="turbolinks-root" content="/{{Dashboard::prefix()}}">
     <meta name="dashboard-prefix" content="{{Dashboard::prefix()}}">
