f32e4cf5523f9b024b794a74ff890a8f120c1d28
Alexandr Chernyaev <bliz48rus@gmail.com>
2018-02-01 00:22:05 +0300

    Apply fixes from StyleCI (#489)
    [ci skip] [skip ci]

--- resources/assets/js/modules/datetimepicker.js
+++ resources/assets/js/modules/datetimepicker.js
@@ -1,19 +1,19 @@
-document.addEventListener('turbolinks:load', function () {
-    $('.datetimepicker').datetimepicker({
-        defaultDate: moment(),
-        locale     : $('html').attr('lang'),
-        icons      : {
-            time    : 'icon-clock',
-            date    : 'icon-event',
-            up      : 'icon-arrow-up',
-            down    : 'icon-arrow-down',
-            right   : 'icon-arrow-right',
-            left    : 'icon-arrow-left',
-            previous: 'icon-arrow-left',
-            next    : 'icon-arrow-right',
-            today   : 'icon-target',
-            clear   : 'icon-trash',
-            close   : 'icon-close',
-        },
-    });
+document.addEventListener('turbolinks:load', function() {
+  $('.datetimepicker').datetimepicker({
+    defaultDate: moment(),
+    locale: $('html').attr('lang'),
+    icons: {
+      time: 'icon-clock',
+      date: 'icon-event',
+      up: 'icon-arrow-up',
+      down: 'icon-arrow-down',
+      right: 'icon-arrow-right',
+      left: 'icon-arrow-left',
+      previous: 'icon-arrow-left',
+      next: 'icon-arrow-right',
+      today: 'icon-target',
+      clear: 'icon-trash',
+      close: 'icon-close',
+    },
+  });
 });
