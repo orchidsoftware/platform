c6beec9501fd98fed296275e5e8be0c36145c69e
Rendol <sapegin.in@gmail.com>
2018-02-06 22:41:51 +0700

    ref #391 - added virtual methods (#505)

--- src/Platform/Fields/Types/PlaceField.php
+++ src/Platform/Fields/Types/PlaceField.php
@@ -6,6 +6,10 @@ namespace Orchid\Platform\Fields\Types;
 
 use Orchid\Platform\Fields\Field;
 
+/**
+ * Class PlaceField
+ * @method $this name($value = true)
+ */
 class PlaceField extends Field
 {
     /**
