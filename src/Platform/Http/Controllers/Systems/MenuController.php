5d065f9f691f319ce132a2632d338a39bbd109c3
Alexandr Chernyaev <bliz48rus@gmail.com>
2018-02-28 23:05:52 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Platform/Http/Controllers/Systems/MenuController.php
+++ src/Platform/Http/Controllers/Systems/MenuController.php
@@ -59,7 +59,7 @@ class MenuController extends Controller
         $menu = Menu::where('lang', $currentLocale)
             ->where('parent', 0)
             ->where('type', $nameMenu)
-            ->orderBy('sort','asc')
+            ->orderBy('sort', 'asc')
             ->with('children')
             ->get();
 
@@ -97,7 +97,6 @@ class MenuController extends Controller
     private function createMenuElement(array $items, $parent = 0)
     {
         foreach ($items as $item) {
-
             Menu::firstOrNew([
                 'id' => $item['id'],
             ])->fill(array_merge($item, [
