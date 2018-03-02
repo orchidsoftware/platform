5f476b4bc071ab927237499dc8e13c1e76ab6539
PSalador <Salador2005@yandex.ru>
2018-03-03 03:12:31 +0700

    Update UserRoleLayout.php

--- src/Platform/Http/Layouts/User/UserRoleLayout.php
+++ src/Platform/Http/Layouts/User/UserRoleLayout.php
@@ -2,81 +2,53 @@
 
 namespace Orchid\Platform\Http\Layouts\User;
 
-use Orchid\Platform\Fields\Field;
 use Orchid\Platform\Layouts\Rows;
+use Orchid\Platform\Fields\Field;
 
 class UserRoleLayout extends Rows
 {
     /**
-     * Views.
+     * Views
      *
      * @return array
      */
-    public function fields(): array
+	public function fields(): array
     {
-        //dd($this->post);
-        //dd($this->post->getContent('roles'));
+		//dd($this->post);
+		//dd($this->post->getContent('roles'));
 
-        foreach ($this->post->getContent('roles') as $roles) {
-            //dd($roles);
-            $roleoptions[$roles->slug] = $roles->name;
-        }
-        //dd($role);
-        //$roles=$this->post->getContent('roles');
-
-        $fields[] = Field::tag('select')
-            ->options($roleoptions)
-            ->class('select2')
-            ->multiple()
-            ->name('role')
-            ->title('Role')
-            ->placeholder('Select role');
-
-        foreach ($this->post->getContent('permission') as $keyGroup => $itemGroup) {
-            $fields[] = Field::tag('label')
-                ->name('perm')
-                ->title($keyGroup);
-
-            foreach ($itemGroup as $key => $item) {
-                $fields[] = Field::tag('checkbox')
-                   ->name('permissions['.$item['slug'].']')
-                   ->value($item['active'])
-                   ->placeholder($item['description']);
-            }
-        }
-        //dd($fields);
-
-        /*
-        return [
-
-        'select' => Field::tag('select')
-            ->options($roleoptions)
-            ->class('select2')
-            ->multiple()
-            ->name('role')
-            ->title('Select tags')
-            ->help('Allow search bots to index page'),
-
-            Field::tag('input')
-                ->type('text')
-                ->name('name')
-                ->set('value','index1')
-                ->max(255)
-                ->require()
-                ->title(trans('dashboard::systems/users.name'))
-                ->placeholder(trans('dashboard::systems/users.name')),
-
-            Field::tag('input')
-                ->type('text')
-                ->name('roleselects')
-                ->title(trans('dashboard::systems/users.email'))
-                ->placeholder(trans('dashboard::systems/users.email')),
-
-            Field::tag('password')
-                ->name('password')
-                ->title(trans('dashboard::systems/users.password'))
-                ->placeholder('********'),
-        ];*/
-        return $fields;
+		foreach ($this->post->getContent('selroles') as $roles) {
+			//dd($roles);
+			$roleoptions[$roles->slug] =$roles->name;
+		}
+		//dd($role);
+		//$roles=$this->post->getContent('roles');
+		
+		$fields[] = Field::tag('select')
+			->options($roleoptions)
+			->class('select2')
+			->multiple()
+			->name('roles[]')
+			->title('Role')
+			->placeholder('Select role');
+		
+		foreach ($this->post->getContent('userpermission') as $keyGroup => $itemGroup) {
+			
+			$fields[] = Field::tag('label')
+			    ->name('perm')
+				->title($keyGroup);
+			
+			foreach ($itemGroup as $key => $item) {
+				$fields[]= Field::tag('checkbox')
+                   ->name("permissions.".str_replace(".", "_", $item['slug']))
+                   ->placeholder($item['description'])
+				   ->hr(false);
+			}	
+		}
+		//dd($fields);
+		
+		return $fields;
     }
+	
+	
 }
