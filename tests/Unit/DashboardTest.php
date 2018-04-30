6d72e684fa04151de750b634a18bc39047569c59
Alexandr Chernyaev <bliz48rus@gmail.com>
2018-04-30 22:55:45 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Unit/DashboardTest.php
+++ tests/Unit/DashboardTest.php
@@ -3,42 +3,32 @@
 namespace Orchid\Tests\Unit;
 
 use Orchid\Platform\Dashboard;
-use Orchid\Platform\Models\User;
 use Orchid\Tests\TestUnitCase;
+use Orchid\Platform\Models\User;
 
 class DashboardTest extends TestUnitCase
 {
-
-    /**
-     *
-     */
     public function testIsModelDefault()
     {
-        $class = Dashboard::model('UnknownClass',User::class);
+        $class = Dashboard::model('UnknownClass', User::class);
 
         $this->assertEquals($class, User::class);
     }
 
-    /**
-     *
-     */
     public function testIsModelCustom()
     {
-        Dashboard::useModel(User::class,'MyCustomClass');
+        Dashboard::useModel(User::class, 'MyCustomClass');
 
         $user = Dashboard::model(User::class);
 
         $this->assertEquals($user, 'MyCustomClass');
     }
 
-    /**
-     *
-     */
     public function testIsModelConfigure()
     {
         Dashboard::configure([
             'models' => [
-                User::class => 'MyCustomClass'
+                User::class => 'MyCustomClass',
             ],
         ]);
 
