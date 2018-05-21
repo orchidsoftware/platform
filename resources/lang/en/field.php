54df120d09d11d5a1b333c7d1b5f53e7199cfea1
Alexandr Chernyaev <bliz48rus@gmail.com>
2018-05-21 14:39:21 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- resources/lang/en/field.php
+++ resources/lang/en/field.php
@@ -11,18 +11,18 @@ return [
     ],
     'utm' => [
         'Generate UTM'          => 'Generate UTM',
-        'Generate URL'          => 'Generate URL',        
+        'Generate URL'          => 'Generate URL',
         'UTM Generator'         => 'UTM Generator',
         'Campaign Source'       => 'Campaign Source',
         'Campaign Source desc'  => 'Original referrer: (e.g. google, newsletter)',
         'Campaign Medium'       => 'Campaign Medium',
         'Campaign Medium desc'  => 'Marketing medium: (e.g. cpc, ppc, banner, email)',
         'Campaign Name'         => 'Campaign Name',
-        'Campaign Name desc'    => 'Product, promo code, or slogan (e.g. spring_sale)', 
+        'Campaign Name desc'    => 'Product, promo code, or slogan (e.g. spring_sale)',
         'Campaign Term'         => 'Campaign Term',
-        'Campaign Term desc'    => 'Paid keywords: (e.g. running+shoes)',  
+        'Campaign Term desc'    => 'Paid keywords: (e.g. running+shoes)',
         'Campaign Content'      => 'Campaign Content',
-        'Campaign Content desc' => 'Ad-specific content used to differentiate ads',         
+        'Campaign Content desc' => 'Ad-specific content used to differentiate ads',
     ],
 
 ];
--- resources/lang/ru/field.php
+++ resources/lang/ru/field.php
@@ -18,11 +18,11 @@ return [
         'Campaign Medium'       => 'Канал кампании',
         'Campaign Medium desc'  => 'Тип трафика: cpc, ppc, banner, email и т.п.',
         'Campaign Name'         => 'Название кампании',
-        'Campaign Name desc'    => 'Название рекламной кампании: продукт, слоган и т.п.',  
+        'Campaign Name desc'    => 'Название рекламной кампании: продукт, слоган и т.п.',
         'Campaign Term'         => 'Ключевое слово',
-        'Campaign Term desc'    => 'Определяет оплачиваемые ключевые слова',  
+        'Campaign Term desc'    => 'Определяет оплачиваемые ключевые слова',
         'Campaign Content'      => 'Содержание кампании',
-        'Campaign Content desc' => 'Дополнительная информация, позволяющая различать объявления',         
+        'Campaign Content desc' => 'Дополнительная информация, позволяющая различать объявления',
     ],
 
 ];
