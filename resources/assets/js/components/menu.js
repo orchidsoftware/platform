fc889726ea3622de987912bc99c7af64b31ce1d6
Maksim Klimenko <mavsan@gmail.com>
2018-06-13 13:36:32 +1000

    Исправление ошибки сохранения меню.

--- resources/assets/js/components/menu.js
+++ resources/assets/js/components/menu.js
@@ -1,183 +1,209 @@
 document.addEventListener('turbolinks:load', function() {
-  if (document.getElementById('menu-vue') === null) {
-    return;
-  }
+    if (document.getElementById('menu-vue') === null) {
+        return;
+    }
 
-  const menu = new Vue({
-    el: '#menu-vue',
-    data: {
-      count: 0,
-      id: '',
-      label: '',
-      title: '',
-      slug: '',
-      auth: 0,
-      robot: 'follow',
-      style: '',
-      target: '_self',
-      errors: {
-        title: false,
-        label: false,
-        slug: false,
-      },
-    },
-    methods: {
-      load: function(object) {
-        this.id = object.id;
-        this.label = object.label;
-        this.slug = object.slug;
-        this.auth = object.auth;
-        this.robot = object.robot;
-        this.style = object.style;
-        this.target = object.target;
-        this.title = object.title;
-      },
-      checkForm: function() {
-        let valid = false;
-        this.errors = {
-          title: false,
-          label: false,
-          slug: false,
-        };
+    const menu = new Vue({
+        el: '#menu-vue',
+        data: {
+            count: 0,
+            id: '',
+            label: '',
+            title: '',
+            slug: '',
+            auth: 0,
+            robot: 'follow',
+            style: '',
+            target: '_self',
+            errors: {
+                title: false,
+                label: false,
+                slug: false,
+            },
+        },
+        methods: {
+            load: function(object) {
+                this.id = object.id;
+                this.label = object.label;
+                this.slug = object.slug;
+                this.auth = object.auth;
+                this.robot = object.robot;
+                this.style = object.style;
+                this.target = object.target;
+                this.title = object.title;
+            },
+            checkForm: function() {
+                let valid = false;
+                this.errors = {
+                    title: false,
+                    label: false,
+                    slug: false,
+                };
 
-        if (!this.title) {
-          this.errors.title = valid = true;
-        }
+                if (!this.title) {
+                    this.errors.title = valid = true;
+                }
 
-        if (!this.label) {
-          this.errors.label = valid = true;
-        }
+                if (!this.label) {
+                    this.errors.label = valid = true;
+                }
 
-        if (!this.slug) {
-          this.errors.slug = valid = true;
-        }
+                if (!this.slug) {
+                    this.errors.slug = valid = true;
+                }
 
-        return !valid;
-      },
-      add: function() {
-        if (!this.checkForm()) {
-          return;
-        }
+                return !valid;
+            },
+            add: function() {
+                if (!this.checkForm()) {
+                    return;
+                }
 
-        $('.dd > .dd-list').append(
-          "<li class='dd-item dd3-item' data-id='" +
-            this.count +
-            "'> " +
-            "<div  class='dd-handle dd3-handle'>Drag</div><div  class='dd3-content'>" +
-            this.label +
-            '</div> ' +
-            "<div  class='edit icon-pencil'></div>" +
-            '</li>',
-        );
+                let
+                    $vm = this,
+                    $dd = $('.dd'),
+                    data = {
+                        menu: $dd.attr('data-name'),
+                        lang: $dd.attr('data-lang'),
+                        data: {
+                            label: this.label,
+                            title: this.title,
+                            auth: this.auth,
+                            slug: this.slug,
+                            robot: this.robot,
+                            style: this.style,
+                            target: this.target,
+                        }
+                    };
 
-        $('li[data-id=' + this.count + ']').data({
-          label: this.label,
-          title: this.title,
-          auth: this.auth,
-          slug: this.slug,
-          robot: this.robot,
-          style: this.style,
-          target: this.target,
+                axios
+                    .get(dashboard.prefix('/systems/menu/create/'), {params: data})
+                    .then(function(response) {addItem(response.data.id)});
+
+                function addItem(id) {
+                    $('.dd > .dd-list').append(
+                        "<li class='dd-item dd3-item' data-id='" +
+                         id +
+                        "'> " +
+                        "<div  class='dd-handle dd3-handle'>Drag</div><div  class='dd3-content'>" +
+                        $vm.label +
+                        '</div> ' +
+                        "<div  class='edit icon-pencil'></div>" +
+                        '</li>',
+                    );
+
+                    $('li[data-id=' + id + ']').data({
+                        label: $vm.label,
+                        title: $vm.title,
+                        auth: $vm.auth,
+                        slug: $vm.slug,
+                        robot: $vm.robot,
+                        style: $vm.style,
+                        target: $vm.target,
+                    });
+
+                    $vm.count--;
+                    sortItems();
+                    $vm.clear();
+                    $vm.send();
+                }
+            },
+            edit: function(element) {
+                let data = $(element)
+                    .parent()
+                    .data();
+                data.label = $(element)
+                    .prev()
+                    .text();
+                this.load(data);
+            },
+            save: function() {
+                if (!this.checkForm()) {
+                    return;
+                }
+
+                $('li[data-id=' + this.id + ']').data({
+                    label: this.label,
+                    title: this.title,
+                    auth: this.auth,
+                    slug: this.slug,
+                    robot: this.robot,
+                    style: this.style,
+                    target: this.target,
+                });
+                $('li[data-id=' + this.id + '] > .dd3-content').html(this.label);
+
+                this.clear();
+                $('#menuEdit').modal('hide');
+                menu.send();
+            },
+            destroy: function(id) {
+                axios
+                    .delete(dashboard.prefix('/systems/menu/' + id))
+                    .then(function(response) {});
+            },
+            remove: function() {
+                $('li[data-id=' + this.id + ']').remove();
+                $('#menuEdit').modal('hide');
+                this.destroy(this.id);
+                this.clear();
+            },
+            clear: function() {
+                this.label = '';
+                this.title = '';
+                this.auth = 0;
+                this.slug = '';
+                this.robot = 'follow';
+                this.style = '';
+                this.target = '_self';
+                this.id = '';
+            },
+            send: function() {
+                let name = $('.dd').attr('data-name');
+
+                let data = {
+                    lang: $('.dd').attr('data-lang'),
+                    data: $('.dd').nestable('serialize'),
+                };
+
+                axios
+                    .put(dashboard.prefix('/systems/menu/' + name), data)
+                    .then(function(response) {});
+            },
+            exist: function() {
+                return (
+                    Number.isInteger(this.id) &&
+                    $('li[data-id=' + this.id + ']').length > 0
+                );
+            },
+        },
+    });
+
+    let $dd = $('.dd');
+
+    $dd.nestable({});
+
+    sortItems();
+
+    $dd
+        .on('change', function() {
+            sortItems();
+
+            menu.send();
+        })
+        .on('click', '.edit', function() {
+            menu.edit(this);
         });
 
-        this.count--;
-        this.clear();
-        this.send();
-      },
-      edit: function(element) {
-        let data = $(element)
-          .parent()
-          .data();
-        data.label = $(element)
-          .prev()
-          .text();
-        this.load(data);
-      },
-      save: function() {
-        if (!this.checkForm()) {
-          return;
-        }
-
-        $('li[data-id=' + this.id + ']').data({
-          label: this.label,
-          title: this.title,
-          auth: this.auth,
-          slug: this.slug,
-          robot: this.robot,
-          style: this.style,
-          target: this.target,
-        });
-        $('li[data-id=' + this.id + '] > .dd3-content').html(this.label);
-
-        this.clear();
-        $('#menuEdit').modal('hide');
+    $('.menu-save').click(function() {
         menu.send();
-      },
-      destroy: function(id) {
-        axios
-          .delete(dashboard.prefix('/systems/menu/' + id))
-          .then(function(response) {});
-      },
-      remove: function() {
-        $('li[data-id=' + this.id + ']').remove();
-        $('#menuEdit').modal('hide');
-        this.destroy(this.id);
-        this.clear();
-      },
-      clear: function() {
-        this.label = '';
-        this.title = '';
-        this.auth = 0;
-        this.slug = '';
-        this.robot = 'follow';
-        this.style = '';
-        this.target = '_self';
-        this.id = '';
-      },
-      send: function() {
-        let name = $('.dd').attr('data-name');
-
-        let data = {
-          lang: $('.dd').attr('data-lang'),
-          data: $('.dd').nestable('serialize'),
-        };
-
-        axios
-          .put(dashboard.prefix('/systems/menu/' + name), data)
-          .then(function(response) {});
-      },
-      exist: function() {
-        return (
-          Number.isInteger(this.id) &&
-          $('li[data-id=' + this.id + ']').length > 0
-        );
-      },
-    },
-  });
-
-  $('.dd').nestable({});
-
-  $('.dd-item').each(function(i, item) {
-    $(item).data({
-      sort: i,
-    });
-  });
-
-  $('.dd').on('change', function() {
-    $('.dd-item').each(function(i, item) {
-      $(item).data({
-        sort: i,
-      });
     });
 
-    menu.send();
-  });
-
-  $('.dd').on('click', '.edit', function() {
-    menu.edit(this);
-  });
-
-  $('.menu-save').click(function() {
-    menu.send();
-  });
+    function sortItems(){
+        $('.dd-item').each(function(i, item) {
+            $(item).data({
+                sort: i,
+            });
+        });
+    }
 });
