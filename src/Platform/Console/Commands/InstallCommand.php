6f6d8aba349dc9ed3ae420c391f1cba56d3fb868
tabuna <bliz48rus@gmail.com>
2018-08-10 23:31:51 +0300

    fixed install publish configs

--- src/Platform/Console/Commands/InstallCommand.php
+++ src/Platform/Console/Commands/InstallCommand.php
@@ -53,8 +53,9 @@ class InstallCommand extends Command
         $this->info($text);
         sleep(1);
 
-        $this->executeCommand('vendor:publish', ['--provider' => FoundationServiceProvider::class])
-            ->executeCommand('vendor:publish', ['--all' => true])
+        $this
+            ->executeCommand('vendor:publish', ['--all' => true, '--force' => true])
+            ->executeCommand('vendor:publish', ['--provider' => FoundationServiceProvider::class, '--force' => true])
             ->executeCommand('migrate')
             ->executeCommand('storage:link')
             ->executeCommand('orchid:link');
@@ -80,7 +81,7 @@ class InstallCommand extends Command
      */
     private function executeCommand(string $command, $parameters = [])
     {
-        if (! $this->progressBar->getProgress()) {
+        if (!$this->progressBar->getProgress()) {
             $this->progressBar->start();
             echo ' ';
         }
@@ -107,7 +108,7 @@ class InstallCommand extends Command
 
         $this->info(' Attempting to set ORCHID User model as parent to App\User');
 
-        if (! file_exists(app_path('User.php'))) {
+        if (!file_exists(app_path('User.php'))) {
             $this->warn('Unable to locate "app/User.php".  Did you move this file?');
             $this->warn('You will need to update this manually.  Change "extends Authenticatable" to "extends \Orchid\Platform\Models\User" in your User model');
 
@@ -128,7 +129,7 @@ class InstallCommand extends Command
 
         $this->info(' Add semantic links to public files to ignore VCS');
 
-        if (! file_exists(app_path('../.gitignore'))) {
+        if (!file_exists(app_path('../.gitignore'))) {
             $this->warn('Unable to locate ".gitignore".  Did you move this file?');
             $this->warn('A semantic link to public files was not added to the ignore list');
 
@@ -138,7 +139,7 @@ class InstallCommand extends Command
         $str = file_get_contents(app_path('../.gitignore'));
 
         if ($str !== false && strpos($str, '/public/orchid') === false) {
-            file_put_contents(app_path('../.gitignore'), $str.PHP_EOL.'/public/orchid'.PHP_EOL);
+            file_put_contents(app_path('../.gitignore'), $str . PHP_EOL . '/public/orchid' . PHP_EOL);
         }
     }
 
@@ -163,9 +164,9 @@ class InstallCommand extends Command
     }
 
     /**
-     * @param string $question
+     * @param string        $question
      * @param        string $constant
-     * @param string   $default
+     * @param string        $default
      *
      * @return $this
      */
@@ -176,7 +177,7 @@ class InstallCommand extends Command
         $str = file_get_contents(app_path('../.env'));
 
         if ($str !== false && strpos($str, $constant) === false) {
-            file_put_contents(app_path('../.env'), $str.PHP_EOL.$constant.'='.$value.PHP_EOL);
+            file_put_contents(app_path('../.env'), $str . PHP_EOL . $constant . '=' . $value . PHP_EOL);
         }
 
         return $this;
