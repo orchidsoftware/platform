59b2cc0fd859bb5f8254d001bb4d3c04b58ec259
tabuna <bliz48rus@gmail.com>
2018-09-04 11:01:27 +0300

    :fire:

--- src/Platform/Providers/DashboardProvider.php
+++ src/Platform/Providers/DashboardProvider.php
@@ -7,6 +7,7 @@ namespace Orchid\Platform\Providers;
 use Orchid\Platform\Dashboard;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Orchid\Platform\Http\Composers\SystemMenuComposer;
 
 class DashboardProvider extends ServiceProvider
 {
@@ -24,76 +25,12 @@ class DashboardProvider extends ServiceProvider
     {
         $this->dashboard = $dashboard;
 
-        View::composer('platform::container.systems.index', $this->registerMenu());
+        View::composer('platform::container.systems.index', SystemMenuComposer::class);
 
         $this->dashboard
             ->registerResource(config('platform.resource', []))
             ->registerPermissions($this->registerPermissionsMain())
             ->registerPermissions($this->registerPermissionsSystems());
-    }
-
-    protected function registerMenu()
-    {
-        $this->dashboard->menu
-            ->add('Systems', [
-                'slug'       => 'Cache',
-                'icon'       => 'icon-refresh',
-                'label'      => trans('platform::systems/settings.system_menu.Cache configuration'),
-                'active'     => 'platform.systems.*',
-                'permission' => 'platform.systems.cache',
-                'sort'       => 2000,
-            ])
-            ->add('Cache', [
-                'slug'       => 'cache:clear',
-                'icon'       => 'icon-refresh',
-                'route'      => route('platform.systems.cache', ['action' => 'cache']),
-                'label'      => trans('platform::systems/cache.cache'),
-                'groupname'  => trans('platform::systems/cache.cache.description'),
-                'active'     => 'platform.systems.*',
-                'permission' => 'platform.systems.cache',
-                'sort'       => 1000,
-            ])
-            ->add('Cache', [
-                'slug'       => 'config:cache',
-                'icon'       => 'icon-wrench',
-                'route'      => route('platform.systems.cache', ['action' => 'config']),
-                'label'      => trans('platform::systems/cache.config'),
-                'groupname'  => trans('platform::systems/cache.config.description'),
-                'active'     => 'platform.systems.*',
-                'permission' => 'platform.systems.cache',
-                'sort'       => 1000,
-            ])
-            ->add('Cache', [
-                'slug'       => 'route:cache',
-                'icon'       => 'icon-directions',
-                'route'      => route('platform.systems.cache', ['action' => 'route']),
-                'label'      => trans('platform::systems/cache.route'),
-                'groupname'  => trans('platform::systems/cache.route.description'),
-                'active'     => 'platform.systems.*',
-                'permission' => 'platform.systems.cache',
-                'sort'       => 1000,
-            ])
-            ->add('Cache', [
-                'slug'       => 'view:clear',
-                'icon'       => 'icon-monitor',
-                'route'      => route('platform.systems.cache', ['action' => 'view']),
-                'label'      => trans('platform::systems/cache.view'),
-                'groupname'  => trans('platform::systems/cache.view.description'),
-                'active'     => 'platform.systems.*',
-                'permission' => 'platform.systems.cache',
-                'sort'       => 1000,
-            ])
-            ->add('Cache', [
-                'slug'       => 'opcache:clear',
-                'icon'       => 'icon-settings',
-                'route'      => route('platform.systems.cache', ['action' => 'opcache']),
-                'label'      => trans('platform::systems/cache.opcache'),
-                'groupname'  => trans('platform::systems/cache.opcache.description'),
-                'active'     => 'platform.systems.*',
-                'permission' => 'platform.systems.cache',
-                'sort'       => 1000,
-                'show'       => function_exists('opcache_reset'),
-            ]);
     }
 
     /**
