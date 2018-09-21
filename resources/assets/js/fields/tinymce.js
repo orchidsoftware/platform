1f4906b263cf12396b58a419fe68def21532e988
Maksim Klimenko <mavsan@gmail.com>
2018-09-21 11:56:20 +1000

    tinymce.js - удален плагин fullpage, добавляющий лишнее в текст.

--- resources/assets/js/fields/tinymce.js
+++ resources/assets/js/fields/tinymce.js
@@ -6,7 +6,7 @@ let init = (id, theme) => {
 
   if (theme === 'modern') {
     plugins =
-      'print autosave autoresize preview fullpage paste code searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern';
+      'print autosave autoresize preview paste code searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern';
     toolbar1 =
       'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat';
     inline = false;
