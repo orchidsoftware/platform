8d1448f31a6b92ab8955e51f91b23a424fc9d82c
tabuna <bliz48rus@gmail.com>
2018-10-09 02:00:30 +0300

    :fire: relationship

--- resources/views/fields/relationship.blade.php
+++ resources/views/fields/relationship.blade.php
@@ -1,39 +1,16 @@
 @component($typeForm,get_defined_vars())
-    <select id="{{$id}}" @include('platform::partials.fields.attributes', ['attributes' => $attributes])></select>
+    <div data-controller="fields--relationship"
+         data-fields--relationship-id="{{$id}}"
+         data-fields--relationship-placeholder="{{$attributes['placeholder'] ?? ''}}"
+         data-fields--relationship-value="{{$value}}"
+         data-fields--relationship-handler="{{$handler}}"
+         data-fields--relationship-url="{{route('platform.systems.widget',Base64Url\Base64Url::encode($handler))}}"
+         data-fields--relationship-url-value="{{route('platform.systems.widget',[
+                'widget' => Base64Url\Base64Url::encode($handler),
+                'key'    => $value
+         ])}}"
+
+    >
+        <select id="{{$id}}" @include('platform::partials.fields.attributes', ['attributes' => $attributes])></select>
+    </div>
 @endcomponent
-
-@push('scripts')
-    <script>
-        document.addEventListener('turbolinks:load', function () {
-            $('#{{$id}}').select2({
-                theme: "bootstrap",
-                ajax: {
-                    type: "POST",
-                    cache: true,
-                    delay: 250,
-                    url: function (params) {
-                        return '{{route('platform.systems.widget',Base64Url\Base64Url::encode($handler))}}';
-                    },
-                    dataType: 'json'
-                },
-                selectOnClose: true,
-                placeholder: '{{$placeholder ?? ''}}'
-            });
-
-            @if(!is_null($value))
-            axios.post('{{route('platform.systems.widget',[
-                    'widget' => Base64Url\Base64Url::encode($handler),
-                    'key'    => $value
-                ])}}').then(function (response) {
-
-                var selected = response.data;
-                selected = selected[Object.keys(selected)[0]];
-
-                $('#{{$id}}')
-                    .append(new Option(selected.text, selected.id, true, true))
-                    .trigger('change');
-            });
-            @endif
-        });
-    </script>
-@endpush
