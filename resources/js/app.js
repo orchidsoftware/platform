1e4ab46f5a5a88ac100ab907188e22296da36666
Maksim Klimenko <mavsan@gmail.com>
2018-10-15 13:53:19 +1000

    Теперь $ и jQuery доступны глобально.

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,8 @@ import { definitionsFromContext } from 'stimulus/webpack-helpers';
 // remove
 $.fn.select2.defaults.set('theme', 'bootstrap');
 
+global.$ = global.jQuery = require('jquery');
+
 window.application = Application.start();
 const context = require.context('./controllers', true, /\.js$/);
 application.load(definitionsFromContext(context));
