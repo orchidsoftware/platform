d700cf878fa968f45ce03917dede4a1781059ea5
Salador <salador2005@yandex.ru>
2018-10-23 15:07:33 +0000

    test

--- tests/Feature/Platform/SystemTest.php
+++ tests/Feature/Platform/SystemTest.php
@@ -19,46 +19,29 @@ class SystemTest extends TestFeatureCase
     {
         parent::setUp();
 
-        //$this->withoutMiddleware();
         if ($this->user) {
             return $this->user;
         }
         $this->user = factory(User::class)->create();
     }
 
-    /*
-        private function getUser()
-        {
-            if ($this->user) {
-                return $this->user;
-            }
-
-            $this->user = User::where('id', 1)->first();
-            //$this->user = factory(User::class)->create();
-
-            return $this->user;
-        }
-    */
 
     public function test_route_PlatformSystemsIndex()
     {
         $response = $this->actingAs($this->user)
             ->get(route('platform.systems.index'));
-        //->get('dashboard/systems');
-        //->call('GET', 'dashboard/systems');
 
         $response->assertStatus(200);
-        $this->assertContains('Settings', $response->baseResponse->content());
+        $this->assertContains('Systems', $response->baseResponse->content());
     }
 
     public function test_route_PlatformSystemsMenuIndex()
     {
         $response = $this->actingAs($this->user)
             ->get(route('platform.systems.menu.index'));
-        //dump($response->baseResponse->content());
+
         $response->assertStatus(302);
         $response->assertRedirect('/dashboard/press/menu/header');
-        //$this->assertContains('dashboard/press/menu/header', $response->baseResponse->content());
     }
 
     public function test_route_PlatformSystemsMenuShow()
