64c58c32ee9c534e89969d15b7a35e8037dcb1da
tabuna <bliz48rus@gmail.com>
2018-11-10 01:37:06 +0300

    realise 3.0.4

--- resources/views/fields/select.blade.php
+++ resources/views/fields/select.blade.php
@@ -4,10 +4,10 @@
             @foreach($options as $key => $option)
                 <option value="{{$key}}"
                         @isset($value)
-                            @if (is_array($value) && in_array($key, $value)) selected1
-                            @elseif (isset($value[$key]) && $value[$key] == $option) selected2
-                            @elseif ($key == $value) selected3
-                            @endif
+                        @if (is_array($value) && in_array($key, $value)) selected
+                        @elseif (isset($value[$key]) && $value[$key] == $option) selected
+                        @elseif ($key == $value) selected
+                        @endif
                         @endisset
                 >{{$option}}</option>
             @endforeach
--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -11,7 +11,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '3.0.3';
+    public const VERSION = '3.0.4';
 
     /**
      * The Dashboard configuration options.
