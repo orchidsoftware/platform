0fca6c11456a4c58965705e2412b15e29e9dab25
Salador <salador2005@yandex.ru>
2018-11-16 05:28:47 +0000

    Fix #726

--- resources/views/fields/datetime.blade.php
+++ resources/views/fields/datetime.blade.php
@@ -1,6 +1,6 @@
 @component($typeForm,get_defined_vars())
     <div>
         <input data-controller="fields--datetime" @include('platform::partials.fields.attributes', ['attributes' => $attributes])
-                   data-fields--datetime-date-format="{{$format ?? "Y-m-d H:i:S"}}">
+                   data-fields--datetime-date-format="{{$format ?? "Y-m-d H:i:S"}}" autocomplete="off">
     </div>
 @endcomponent
