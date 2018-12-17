963ea2f0a3dea9a43c629eec927a4093978632b1
Alexandr Chernyaev <bliz48rus@gmail.com>
2018-12-17 20:27:57 +0300

    Update Dashboard.php

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -11,7 +11,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '3.1.6';
+    public const VERSION = '3.1.7';
 
     /**
      * The Dashboard configuration options.
