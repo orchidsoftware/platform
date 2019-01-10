1ccf2562bb53925237d7fd52d385f273d068b1de
tabuna <bliz48rus@gmail.com>
2019-01-10 12:38:32 +0300

    remove unused method

--- src/Platform/Traits/AttachTrait.php
+++ src/Platform/Traits/AttachTrait.php
@@ -5,35 +5,13 @@ declare(strict_types=1);
 namespace Orchid\Platform\Traits;
 
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Orchid\Attachment\Models\Attachment;
 
 /**
  * Trait Attachment.
  */
 trait AttachTrait
 {
-    /**
-     * The Eloquent tags model name.
-     *
-     * @var string
-     */
-    protected static $attachmentModel = 'Orchid\Attachment\Models\Attachment';
-
-    /**
-     * @return string
-     */
-    public static function getAttachmentModel(): string
-    {
-        return static::$attachmentModel;
-    }
-
-    /**
-     * @param $model
-     */
-    public static function setAttachmentModel($model)
-    {
-        static::$attachmentModel = $model;
-    }
-
     /**
      * @param string $group
      *
@@ -42,7 +20,7 @@ trait AttachTrait
     public function attachment($group = null): MorphToMany
     {
         $query = $this->morphToMany(
-            static::$attachmentModel,
+            Attachment::class,
             'attachmentable',
             'attachmentable',
             'attachmentable_id',
