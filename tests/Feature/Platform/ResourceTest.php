35dc71abdde20281ef3308dafc05ff9ad0b57d88
tabuna <bliz48rus@gmail.com>
2019-02-07 21:39:51 +0300

    Added test for resource

---
+++ tests/Feature/Platform/ResourceTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\Feature\Platform;
+
+use Orchid\Tests\TestFeatureCase;
+
+class ResourceTest extends TestFeatureCase
+{
+
+    public function test_route_Resource()
+    {
+        $response = $this
+            ->get(route('platform.resource',['orchid','mix-manifest.json']));
+
+        $response->assertOk();
+    }
+}
