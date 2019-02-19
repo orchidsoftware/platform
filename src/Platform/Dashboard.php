39ca4e75ddccf795cee154698eca6543c96a8872
tabuna <bliz48rus@gmail.com>
2019-02-19 10:53:10 +0300

    ## 3.10.0 - 2019-02-19
    ### Added
    - Macros for TD [785](https://github.com/orchidsoftware/platform/issues/785)

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -11,7 +11,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '3.9.6';
+    public const VERSION = '3.10.0';
 
     /**
      * The Dashboard configuration options.
