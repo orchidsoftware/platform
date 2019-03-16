e79e47203f83b8a98d2c3b483fc0d5aad0c12343
Vlad Zhdanov <adm@progger.ru>
2019-03-17 00:32:04 +0300

    Fixes for button field and for loadModalAsync (#812)
    * refs: #807 Added method for the button
    
    * refs: #811 Fix issue with TD loadModalAsync
    
    refs: #811 Fix modal icons for the icons and for custom text. Added title to async modals.
    
    refs: #811 Fix phpdoc for loadModalAsync method

--- resources/views/fields/button.blade.php
+++ resources/views/fields/button.blade.php
@@ -8,11 +8,22 @@
                 data-modal-action="{{ url()->current() }}/{{ $method }}"
                 @include('platform::partials.fields.attributes', ['attributes' => $attributes])
         >
-            <i class="{{ $icon ?? '' }} m-r-xs"></i>{{ $title ?? '' }}
+            @isset($icon)<i class="{{ $icon }} m-r-xs"></i>@endisset
+            {{ $title ?? '' }}
+        </button>
+    @elseif(!is_null($method))
+        <button type="submit"
+                formaction="{{ url()->current() }}/{{ $method }}"
+                form="post-form"
+                class="{{ $class }}"
+                @include('platform::partials.fields.attributes', ['attributes' => $attributes])>
+            @isset($icon)<i class="{{ $icon }} m-r-xs"></i>@endisset
+            {{ $title ?? '' }}
         </button>
     @else
         <a class="{{ $class }}" @include('platform::partials.fields.attributes', ['attributes' => $attributes])>
-            <i class="{{ $icon ?? '' }} m-r-xs"></i>{{ $title }}
+            @isset($icon)<i class="{{ $icon }} m-r-xs"></i>@endisset
+            {{ $title ?? '' }}
         </a>
     @endif
 @endcomponent
