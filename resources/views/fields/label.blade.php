59f547b10b19e9b33536976048c546d05a875d63
Nakukryskin <adm@progger.ru>
2019-03-17 20:31:52 +0300

    refs: #813 Label field doesn`t show value if it exists

--- resources/views/fields/label.blade.php
+++ resources/views/fields/label.blade.php
@@ -1,5 +1,5 @@
 @component($typeForm,get_defined_vars())
-    @isset($fieldNames)
-        <p id="{{$id}}">{{$fieldNames ?? ''}}</p>
-    @endisset
+    <p @include('platform::partials.fields.attributes', ['attributes' => $attributes])>
+        {{ $value }}
+    </p>
 @endcomponent
