016d5dd5a509faebb09150245994bbba9e7f3109
tabuna <bliz48rus@gmail.com>
2019-03-18 23:35:56 +0300

    Translation footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,8 @@
 <div class="text-center">
     <p class="small m-n">
-      The application code is published under the MIT license. 2016 - {{date('Y')}}<br>
+      {{ __('The application code is published under the MIT license.') }} 2016 - {{date('Y')}}<br>
         <a href="http://orchid.software" target="_blank" rel="noopener">
-            Currently v{{\Orchid\Platform\Dashboard::VERSION}}
+            {{ __('Currently') }} v{{\Orchid\Platform\Dashboard::VERSION}}
         </a>
     </p>
 </div>
