d0d4dcb85fa06711b86f1830546363ce1f0aa277
Alexandr Chernyaev <bliz48rus@gmail.com>
2019-03-26 07:08:57 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Screen/Layouts/Selection.php
+++ src/Screen/Layouts/Selection.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Orchid\Screen\Layouts;
 
-use Orchid\Platform\Filters\Filter;
 use Orchid\Screen\Repository;
+use Orchid\Platform\Filters\Filter;
 
 /**
  * Class Selection.
