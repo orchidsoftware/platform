cb2a069b51ed986d2b88bd88b55ed30ffcbbb672
tabuna <bliz48rus@gmail.com>
2019-04-12 21:10:10 +0300

    Update template

--- resources/views/container/layouts/accordion.blade.php
+++ resources/views/container/layouts/accordion.blade.php
@@ -1,29 +1,32 @@
 <style>
-    .card-header.collapsed .icon-arrow-down:before {
+    .accardion-heading .btn:hover{
+        text-decoration: none;
+    }
+
+    .collapsed .icon-arrow-down:before {
         content: "\E914";
     }
 </style>
-<div id="accordion">
-    @foreach($manyForms as $name => $forms)
-        <div class="card">
-            <div class="card-header @if ($loop->index) collapsed @endif" id="heading-{{str_slug($name)}}" data-toggle="collapse" data-target="#collapse-{{str_slug($name)}}" aria-expanded="true" aria-controls="collapse-{{str_slug($name)}}">
-                <h5 class="mb-0">
-                    <span class="btn-link">
-                        {!! $name !!}
-                    </span>
-                    <span class="icon-arrow-down icons pull-right"></span>
-                </h5>
-            </div>
 
-            <div id="collapse-{{str_slug($name)}}" class="collapse @if (!$loop->index) show @endif" aria-labelledby="heading-{{str_slug($name)}}" data-parent="#accordion">
-                <div class="card-body">
-                    <div class="padder-v">
-                        @foreach($forms as $form)
-                            {!! $form !!}
-                        @endforeach
-                    </div>
-                </div>
-            </div>
+<div id="accardion-{{$templateSlug}}" class="accardion">
+    @foreach($manyForms as $name => $forms)
+        <div class="accardion-heading b-b @if ($loop->index) collapsed @endif"
+             id="heading-{{str_slug($name)}}"
+             data-toggle="collapse"
+             data-target="#collapse-{{str_slug($name)}}"
+             aria-expanded="true"
+             aria-controls="collapse-{{str_slug($name)}}">
+            <h6 class="btn btn-link btn-group-justified pt-2 pb-2 mb-0 pr-0 pl-0 v-center">
+                <span class="accardion-icon icon-arrow-down text-xs m-r-xs"></span> {!! $name !!}
+            </h6>
+        </div>
+
+        <div id="collapse-{{str_slug($name)}}" class="b-b mt-2 collapse @if (!$loop->index) show @endif"
+             aria-labelledby="heading-{{str_slug($name)}}"
+             data-parent="#accardion-{{$templateSlug}}">
+                @foreach($forms as $form)
+                    {!! $form !!}
+                @endforeach
         </div>
     @endforeach
 </div>
