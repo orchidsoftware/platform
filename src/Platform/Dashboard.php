fc693ef4fa14a01e60ce967e2d7190934ad5fffc
tabuna <bliz48rus@gmail.com>
2019-04-21 18:04:43 +0300

    ## 4.6.3 - 2019-04-21

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -13,7 +13,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '4.6.2';
+    public const VERSION = '4.6.3';
 
     /**
      * The Dashboard configuration options.
