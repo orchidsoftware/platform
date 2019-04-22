12fbcb408862d5c539060bbb69ef4f78bfb1a352
tabuna <bliz48rus@gmail.com>
2019-04-23 01:56:02 +0300

    Added trait for AsSource

--- src/Platform/Traits/MultiLanguageTrait.php
+++ src/Platform/Traits/MultiLanguageTrait.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace Orchid\Platform\Traits;
 
+use Orchid\Screen\Traits\AsSource;
+
 trait MultiLanguageTrait
 {
+    use AsSource {
+        getContent as getBaseContent;
+    }
     /**
      * Name row localization.
      *
@@ -15,29 +20,30 @@ trait MultiLanguageTrait
 
     /**
      * @param string $field
-     * @param null   $locale
+     * @param null $locale
      *
      * @return mixed|null
      */
     public function getContent(string $field, $locale = null)
     {
-        $attributes = array_keys($this->toArray());
+        return $this->getBaseContent($field) ?? $this->getContentMultiLang($field, $locale);
+    }
 
-        if (method_exists($this, $field)) {
-            return $this->$field($locale);
-        }
-
-        if (in_array($field, $attributes, true)) {
-            return $this->getAttribute($field);
-        }
-
-        $jsonContent = (array) $this->getAttribute($this->jsonColumnName);
-        $fullName = ($locale ?? app()->getLocale()).'.'.$field;
+    /**
+     * @param string $field
+     * @param null $locale
+     *
+     * @return mixed
+     */
+    private function getContentMultiLang(string $field, $locale = null)
+    {
+        $jsonContent = (array)$this->getAttribute($this->jsonColumnName);
+        $fullName    = ($locale ?? app()->getLocale()) . '.' . $field;
 
         if (array_has($jsonContent, $fullName)) {
             return array_get($jsonContent, $fullName);
         }
 
-        return array_get($jsonContent, config('app.fallback_locale').'.'.$field);
+        return array_get($jsonContent, config('app.fallback_locale') . '.' . $field);
     }
 }
