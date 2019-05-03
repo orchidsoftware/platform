215a6cc6c40b57d79422ec7b9b092a5f06c352c8
Alexandr Chernyaev <bliz48rus@gmail.com>
2019-05-03 14:21:45 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Attachment/Models/Attachment.php
+++ src/Attachment/Models/Attachment.php
@@ -92,7 +92,7 @@ class Attachment extends Model
         $url = $this->url();
 
         if (filter_var($url, FILTER_VALIDATE_URL) === false) {
-           return null;
+            return null;
         }
 
         return parse_url($url, PHP_URL_PATH);
