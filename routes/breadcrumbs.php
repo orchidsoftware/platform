2eb55dd52885dfbb505c17875009eaec738a7b74
PSalador <salador2005@yandex.ru>
2019-05-11 10:48:07 +0000

    Delete press 1

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -15,46 +15,8 @@ Breadcrumbs::for('platform.systems.index', function ($trail) {
     $trail->push(__('Systems'), route('platform.systems.index'));
 });
 
-// Platform > System > Menu
-Breadcrumbs::for('platform.systems.menu.index', function ($trail) {
-    $trail->parent('platform.systems.index');
-    $trail->push(__('Menu'), route('platform.systems.menu.index'));
-});
-
-// Platform > System > Menu > Editing
-Breadcrumbs::for('platform.systems.menu.show', function ($trail, $menu) {
-    $trail->parent('platform.systems.menu.index');
-    $trail->push(__('Editing'), route('platform.systems.menu.show', $menu));
-});
-
 // Platform > System > Announcement
 Breadcrumbs::for('platform.systems.announcement', function ($trail) {
     $trail->parent('platform.systems.index');
     $trail->push(__('Announcement'), route('platform.systems.announcement'));
-});
-
-//Posts
-
-// Platform > Posts
-Breadcrumbs::for('platform.entities.type', function ($trail, $type) {
-    $trail->parent('platform.index');
-    $trail->push(__('Posts'), route('platform.entities.type', $type->slug));
-});
-
-// Platform > Posts > Create
-Breadcrumbs::for('platform.entities.type.create', function ($trail, $type) {
-    $trail->parent('platform.entities.type', $type);
-    $trail->push(__('Create'), route('platform.entities.type', $type->slug));
-});
-
-// Platform > Posts > Edit
-Breadcrumbs::for('platform.entities.type.edit', function ($trail, $type, $post) {
-    $trail->parent('platform.entities.type', $type);
-    $trail->push($post->getContent($type->slugFields) ?? '—', route('platform.entities.type.edit', [$type->slug, $post->slug]));
-});
-
-// Platform > Pages
-Breadcrumbs::for('platform.entities.type.page', function ($trail, $page) {
-    $trail->parent('platform.index');
-    $trail->push(__('Pages'), route('platform.entities.type.page', $page));
 });
