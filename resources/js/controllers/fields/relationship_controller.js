9f0cb7a078d6010010c644d2c34a319e5db22563
Nakukryskin <adm@progger.ru>
2019-05-13 14:08:40 +0300

    Issue with turbolinks and select2 when user moving back in the browser: select2 initializing twice

--- resources/js/controllers/fields/relationship_controller.js
+++ resources/js/controllers/fields/relationship_controller.js
@@ -5,6 +5,10 @@ export default class extends Controller {
      *
      */
     connect() {
+        if (document.documentElement.hasAttribute("data-turbolinks-preview")) {
+            return;
+        }
+
         const select = this.element.querySelector('select'),
             allowHtml = select.getAttribute('allowhtml') === 'true',
             options = {
@@ -24,8 +28,8 @@ export default class extends Controller {
             options.escapeMarkup = function (m) {
                 return m;
             };
-            options.templateResult = function(state) {
-                if(state.html) {
+            options.templateResult = function (state) {
+                if (state.html) {
                     return state.html;
                 }
 
@@ -33,29 +37,31 @@ export default class extends Controller {
             };
         }
 
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf_token"]').attr('content'),
-            },
-        });
+        setTimeout(() => {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf_token"]').attr('content'),
+                },
+            });
 
-        $(select).select2(options).on('select2:unselecting', function () {
-            $(this).data('state', 'unselected');
-        }).on('select2:opening', function (e) {
-            if ($(this).data('state') === 'unselected') {
-                e.preventDefault();
-                $(this).removeData('state');
+            $(select).select2(options).on('select2:unselecting', function () {
+                $(this).data('state', 'unselected');
+            }).on('select2:opening', function (e) {
+                if ($(this).data('state') === 'unselected') {
+                    e.preventDefault();
+                    $(this).removeData('state');
+                }
+            });
+
+            if (!this.data.get('value')) {
+                return;
             }
-        });
 
-        if (!this.data.get('value')) {
-            return;
-        }
-
-        axios.post(this.data.get('url-value')).then((response) => {
-            $(select)
-                .append(new Option(response.data.text, response.data.id, true, true))
-                .trigger('change');
+            axios.post(this.data.get('url-value')).then((response) => {
+                $(select)
+                    .append(new Option(response.data.text, response.data.id, true, true))
+                    .trigger('change');
+            });
         });
 
         document.addEventListener('turbolinks:before-cache', () => {
