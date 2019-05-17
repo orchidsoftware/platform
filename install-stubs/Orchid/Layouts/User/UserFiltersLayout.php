dd1dbc112b3afffd3b3fa472e281e05174e032db
PSalador <salador2005@yandex.ru>
2019-05-17 00:00:32 +0000

    Filter fix

--- install-stubs/Orchid/Layouts/User/UserFiltersLayout.php
+++ install-stubs/Orchid/Layouts/User/UserFiltersLayout.php
@@ -3,7 +3,7 @@
 namespace App\Orchid\Layouts\User;
 
 use App\Orchid\Filters\RoleFilter;
-use Orchid\Platform\Filters\Filter;
+use Orchid\Filters\Filter;
 use Orchid\Screen\Layouts\Selection;
 
 class UserFiltersLayout extends Selection
