3f3444177864563f27fd174d2394edf6f476b2db
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-27 23:09:47 +0000

    Deploy orchidsoftware/platform to github.com/orchidsoftware/platform.git:master

--- resources/js/controllers/fields/quill_controller.js
+++ resources/js/controllers/fields/quill_controller.js
@@ -9,7 +9,7 @@ export default class extends Controller {
         const selector = this.element.querySelector('.quill').id;
         const input = this.element.querySelector('input');
 
-        this.editor = new Quill(`#${selector}`, {
+        const editor = new Quill(`#${selector}`, {
             theme: 'snow',
             modules: {
                 toolbar: [
@@ -21,71 +21,71 @@ export default class extends Controller {
             },
         });
 
+        /**
+         * Step1. select local image
+         *
+         */
+        function selectLocalImage() {
+            const input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.click();
+
+            // Listen upload local image and save to server
+            input.onchange = () => {
+                const file = input.files[0];
+
+                // file type is only image.
+                if (/^image\//.test(file.type)) {
+                    saveToServer(file);
+                } else {
+                    console.warn('You could only upload images.');
+                }
+            };
+        }
+
+        /**
+         * Step2. save to server
+         *
+         * @param {File} file
+         */
+        function saveToServer(file) {
+            const formData = new FormData();
+            formData.append('image', file);
+
+            axios
+                .post(platform.prefix('/systems/files'), formData)
+                .then((response) => {
+                    insertToEditor(response.data.url);
+                })
+                .catch((error) => {
+                    console.warn('quill image upload failed');
+                    console.warn(error);
+                });
+        }
+
+        /**
+         * Step3. insert image url to rich editor.
+         *
+         * @param {string} url
+         */
+        function insertToEditor(url) {
+            // push image url to rich editor.
+            const range = editor.getSelection();
+            editor.insertEmbed(range.index, 'image', url);
+        }
+
         // quill editor add image handler
-        this.editor.getModule('toolbar').addHandler('image', () => {
-            this.selectLocalImage();
+        editor.getModule('toolbar').addHandler('image', () => {
+            selectLocalImage();
         });
 
         // set value
         // editor.setText(input.value);
-        this.editor.root.innerHTML = input.value;
+        editor.root.innerHTML = input.value;
 
         // save value
-        this.editor.on('text-change', () => {
-            input.value = this.editor.getText() ? this.editor.root.innerHTML : '';
+        editor.on('text-change', () => {
+            input.value = editor.getText() ? editor.root.innerHTML : '';
         });
-    }
-
-    /**
-     * Step1. select local image
-     *
-     */
-    selectLocalImage() {
-        const input = document.createElement('input');
-        input.setAttribute('type', 'file');
-        input.click();
-
-        // Listen upload local image and save to server
-        input.onchange = () => {
-            const file = input.files[0];
-
-            // file type is only image.
-            if (/^image\//.test(file.type)) {
-                this.saveToServer(file);
-            } else {
-                console.warn('You could only upload images.');
-            }
-        };
-    }
-
-    /**
-     * Step2. save to server
-     *
-     * @param {File} file
-     */
-    saveToServer(file) {
-        const formData = new FormData();
-        formData.append('image', file);
-
-        axios
-            .post(platform.prefix('/systems/files'), formData)
-            .then((response) => {
-                this.insertToEditor(response.data.url);
-            })
-            .catch((error) => {
-                console.warn('quill image upload failed');
-                console.warn(error);
-            });
-    }
-
-    /**
-     * Step3. insert image url to rich editor.
-     *
-     * @param {string} url
-     */
-    insertToEditor(url) {
-        // push image url to rich editor.
-        const range = this.editor.getSelection();
-        this.editor.insertEmbed(range.index, 'image', url);
     }
 }
