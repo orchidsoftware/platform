93be80714d9a4353e5bdbf0e65e1ada3f360d231
tabuna <bliz48rus@gmail.com>
2019-05-29 02:12:02 +0300

    Added test for MultiSourceTest.php

---
+++ tests/Unit/Screen/MultiSourceTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\Unit\Screen;
+
+use Orchid\Screen\AsMultiSource;
+use Orchid\Tests\TestUnitCase;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class SourceTest.
+ */
+class MultiSourceTest extends TestUnitCase
+{
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    protected function setUp(): void
+    {
+        $model = new class extends Model {
+            use AsMultiSource;
+
+            protected $fillable = [
+                'content',
+            ];
+
+            protected $casts = [
+                'options' => 'array',
+            ];
+        };
+
+        $this->model = $model->fill([
+            'content' => [
+                'en' => [
+                    'country' => [
+                        'Russia',
+                        'Ukraine',
+                        'Spain',
+                        'Egypt',
+                        'Belorussia',
+                        'Romania',
+                        'Estonia',
+                    ],
+                ],
+                'ru' => [
+                    'country' => [
+                        'Россия',
+                        'Украина',
+                        'Испания',
+                        'Египет',
+                        'Беларусь',
+                        'Румыния',
+                        'Эстония',
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    public function testMultiLanguageAttribute()
+    {
+        $this->assertContains('Russia', $this->model->getContent('country', 'en'));
+        $this->assertContains('Россия', $this->model->getContent('country', 'ru'));
+    }
+
+}
