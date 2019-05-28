fa99b5d29ec753f698b83b3cc9302df04d64ae5c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-28 15:37:11 +0000

    Deploy orchidsoftware/platform to github.com/orchidsoftware/platform.git:master

--- tests/Unit/HttpFilterTest.php
+++ tests/Unit/HttpFilterTest.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Orchid\Tests\Unit;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Orchid\Filters\Filterable;
 use Orchid\Filters\HttpFilter;
 use Orchid\Tests\TestUnitCase;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
@@ -178,7 +178,8 @@ class HttpFilterTest extends TestUnitCase
      */
     private function getModelBuilder(HttpFilter $filter)
     {
-        $model = new class extends Model {
+        $model = new class extends Model
+        {
             use Filterable;
 
             /**
