78a9aa07d38c321e7e44af62b8c2cf966cd7799e
Vlad Zhdanov <adm@progger.ru>
2019-05-29 09:44:40 +0300

    Add select2 for the select as default (#872)

--- resources/js/controllers/fields/select_controller.js
+++ resources/js/controllers/fields/select_controller.js
@@ -7,10 +7,6 @@ export default class extends Controller {
     connect() {
         const select = this.element.querySelector('select');
 
-        if (select.getAttribute('multiple') === null) {
-            return;
-        }
-
         $(select).select2({
             width: '100%',
             theme: 'bootstrap',
