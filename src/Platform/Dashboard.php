e3597112dafc5e35fcb3cfe0d9075461f82f7686
tabuna <bliz48rus@gmail.com>
2019-06-09 22:09:24 +0300

    squash version

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -14,7 +14,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '5.0.4';
+    public const VERSION = '5.0.3';
 
     /**
      * The Dashboard configuration options.
