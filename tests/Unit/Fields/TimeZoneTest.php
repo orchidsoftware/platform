d4e6980cd6d6f2bf4c3ab8a903f63ad04e1d2088
tabuna <bliz48rus@gmail.com>
2019-06-14 11:11:39 +0300

    Added test for timezone field

---
+++ tests/Unit/Fields/TimeZoneTest.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\Unit\Fields;
+
+use DateTimeZone;
+use Orchid\Screen\Fields\TimeZone;
+
+/**
+ * Class TimeZoneTest.
+ */
+class TimeZoneTest extends TestFieldsUnitCase
+{
+    /**
+     * @test
+     */
+    public function testInstanse()
+    {
+        $textArea = TimeZone::make('time')
+            ->title('Select time zone');
+
+        $view = self::renderField($textArea);
+
+        $this->assertStringContainsString('time', $view);
+        $this->assertStringContainsString('Select time zone', $view);
+    }
+
+    /**
+     * @test
+     */
+    public function testNeedRequire()
+    {
+        $textArea = TimeZone::make('time')
+            ->required();
+
+        $view = self::renderField($textArea);
+
+        $this->assertStringContainsString('required', $view);
+    }
+
+    /**
+     * @test
+     */
+    public function testSetValue()
+    {
+        $select = TimeZone::make('time')
+            ->value('Africa/Accra');
+
+        $view = self::renderField($select);
+        $view = self::minifyOutput($view);
+
+        $this->assertStringContainsString('value="Africa/Accra" selected', $view);
+    }
+
+    /**
+     * @test
+     */
+    public function testListIdentifiers()
+    {
+        $select = TimeZone::make('time')
+            ->listIdentifiers(DateTimeZone::EUROPE)
+            ->value('Africa/Accra');
+
+        $view = self::renderField($select);
+        $view = self::minifyOutput($view);
+
+        foreach (DateTimeZone::listIdentifiers(DateTimeZone::EUROPE) as $time){
+            $this->assertStringContainsString($time, $view);
+        }
+
+        $this->assertStringNotContainsString('value="Africa/Accra" selected', $view);
+    }
+}
