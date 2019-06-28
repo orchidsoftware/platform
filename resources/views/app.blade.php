7512978f58ca43e94eb23b8e34b1c6a2cfb1d42c
Nakukryskin <adm@progger.ru>
2019-06-28 17:53:45 +0300

    Added defer for the scripts
    Removed defer from orchid libraries

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,7 +25,7 @@
     @stack('stylesheets')
 
     @foreach(Dashboard::getResource('scripts') as $scripts)
-        <script src="{{  $scripts }}" type="text/javascript"></script>
+        <script src="{{  $scripts }}" defer type="text/javascript"></script>
     @endforeach
 </head>
 
