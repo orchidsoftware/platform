f34cb62b50c35a3a6b50a25f40faf7b45551d46f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-16 08:34:26 +0000

    Deploy orchidsoftware/platform to github.com/orchidsoftware/platform.git:build

--- resources/views/auth/lockme.blade.php
+++ resources/views/auth/lockme.blade.php
@@ -33,7 +33,7 @@
 <div class="row">
     <div class="form-group col-md-6 col-xs-12">
         <a href="{{ route('platform.login.lock') }}" class="small">
-            {{__('Sign in with another user')}}
+            {{__('Sign in with another user.')}}
         </a>
     </div>
     <div class="form-group col-md-6 col-xs-12">
