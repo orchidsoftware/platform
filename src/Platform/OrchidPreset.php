1af4debfdaf02d5dbd8642f76c0d34c5a9e62445
mojtabaa hn <mojtabaa.hn@gmail.com>
2019-07-29 02:36:59 +0430

    Adds ability to publish Orchid's asset files (#916)

---
+++ src/Platform/OrchidPreset.php
@@ -0,0 +1,97 @@
+<?php
+
+
+namespace Orchid\Platform;
+
+
+use Illuminate\Foundation\Console\Presets\Preset;
+
+class OrchidPreset extends Preset
+{
+    const ORCHID_MIX_CONFIG_PATTERN = "/(\/\* Orchid mix config start \*\/.*\/\* Orchid mix config end \*\/)/s";
+
+    /**
+     * Install the preset.
+     *
+     * @return void
+     */
+    public static function install()
+    {
+        static::updatePackages(true);
+        static::updatePackages(false);
+        static::updateWebpackConfiguration();
+        static::addBabelConfiguration();
+        static::removeNodeModules();
+    }
+
+    /**
+     * Update the given package array.
+     *
+     * @param array $packages
+     * @param $configurationKey
+     * @return array
+     */
+    protected static function updatePackageArray(array $packages, $configurationKey)
+    {
+        $orchidPackages = json_decode(file_get_contents(PLATFORM_PATH.'/package.json'), true);
+
+        return $orchidPackages[$configurationKey] + $packages;
+    }
+
+    /**
+     * Add orchid configurations to webpack.mix.js.
+     */
+    protected static function updateWebpackConfiguration()
+    {
+        $config = self::config();
+        $orchidConfig = self::orchidConfig();
+
+        $config .= PHP_EOL.$orchidConfig;
+        file_put_contents(
+            base_path('webpack.mix.js'),
+            $config
+        );
+    }
+
+    /**
+     * Copy .bablerc file to root directory.
+     */
+    protected static function addBabelConfiguration()
+    {
+        copy(PLATFORM_PATH.'/.babelrc', base_path('.babelrc'));
+    }
+
+    /**
+     * Takes config part of orchid's webpack.mix.js using a signature.
+     * Using this signature, we prevent duplication of mix import and comments.
+     * Then rewrite paths for correct sources and clean & non-conflict destination paths.
+     *
+     * @return string
+     */
+    protected static function orchidConfig(): string
+    {
+        $orchidConfig = file_get_contents(PLATFORM_PATH.'/webpack.mix.js');
+        preg_match(self::ORCHID_MIX_CONFIG_PATTERN, $orchidConfig, $matches);
+
+        $transformedConfig = count($matches) === 2 ? $matches[1] : '';
+        $transformedConfig = str_replace('resources/js', 'resources/js/orchid', $transformedConfig);
+        $transformedConfig = str_replace('resources/sass', 'resources/sass/orchid', $transformedConfig);
+        $transformedConfig = str_replace('css/orchid.css', 'css/orchid/orchid.css', $transformedConfig);
+        $transformedConfig = str_replace('js/orchid.js', 'js/orchid/orchid.js', $transformedConfig);
+
+        return $transformedConfig;
+    }
+
+    /**
+     * Takes root webpack.mix.js and removes orchid's config (if exists).
+     *
+     * @return false|string
+     */
+    protected static function config()
+    {
+        $config = file_get_contents(base_path('webpack.mix.js'));
+        $config = preg_replace(self::ORCHID_MIX_CONFIG_PATTERN, '', $config);
+
+        return $config;
+    }
+}
