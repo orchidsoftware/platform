db493fb4a7e8ddcd8a23bb955f8a06631670cbe8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-02 12:35:31 +0000

    Deploy orchidsoftware/platform to github.com/orchidsoftware/platform.git:build

--- src/Screen/Layouts/Selection.php
+++ src/Screen/Layouts/Selection.php
@@ -13,10 +13,21 @@ use Illuminate\Contracts\View\Factory;
  */
 abstract class Selection extends Base
 {
+
+    /**
+     * Drop down filters
+     */
+    public const TEMPLATE_DROP_DOWN = 'platform::layouts.selection';
+
+    /**
+     * Line filters
+     */
+    public const TEMPLATE_LINE = 'platform::layouts.filter';
+
     /**
      * @var string
      */
-    public $template = 'platform::layouts.selection';
+    public $template = self::TEMPLATE_DROP_DOWN;
 
     /**
      * Base constructor.
