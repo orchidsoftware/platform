999f55806f154795325010d854242c87c67f5d53
tabuna <bliz48rus@gmail.com>
2019-08-12 11:56:16 +0300

    refs #929 Fixed false triggering of the update indicator for dev versions

--- resources/views/partials/update.blade.php
+++ resources/views/partials/update.blade.php
@@ -1,11 +1,13 @@
 @if($status)
 
-    <div class="alert alert-warning m-n" role="alert">
-        <p class="text-center">
-            <a href="https://orchid.software">
-                {{__('A new stable version of ORCHID is available. Please update to the latest version.')}}
-            </a>
-        </p>
+    <div class="row">
+        <div class="w-100 alert alert-warning m-n" role="alert">
+            <p class="text-center">
+                <a href="https://orchid.software">
+                    {{__('A new stable version of ORCHID is available. Please update to the latest version.')}}
+                </a>
+            </p>
+        </div>
     </div>
 
 @endif
