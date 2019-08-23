e946e718b208be5760aa18d8d08f33681ed989b0
tabuna <bliz48rus@gmail.com>
2019-08-23 11:20:22 +0300

    ## 5.5.5 - 2019-08-23

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -17,7 +17,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '5.5.4';
+    public const VERSION = '5.5.5';
 
     /**
      * The Dashboard configuration options.
--- src/Screen/Field.php
+++ src/Screen/Field.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace Orchid\Screen;
 
 use Closure;
-use Throwable;
-use Illuminate\View\View;
-use Illuminate\Support\Str;
-use Illuminate\Support\Collection;
-use Illuminate\Support\ViewErrorBag;
 use Illuminate\Contracts\View\Factory;
+use Illuminate\Support\Collection;
+use Illuminate\Support\MessageBag;
+use Illuminate\Support\Str;
+use Illuminate\View\View;
 use Orchid\Screen\Contracts\FieldContract;
 use Orchid\Screen\Exceptions\FieldRequiredAttributeException;
+use Throwable;
 
 /**
  * Class Field.
@@ -201,6 +201,8 @@ class Field implements FieldContract
         $this->modifyName();
         $this->modifyValue();
 
+        $errors = $this->getErrorsMessage();
+
         return view($this->view, array_merge($this->getAttributes(), [
             'attributes' => $this->getAllowAttributes(),
             'id'         => $id,
@@ -209,7 +211,7 @@ class Field implements FieldContract
             'oldName'    => $this->getOldName(),
             'typeForm'   => $this->typeForm ?? $this->vertical()->typeForm,
         ]))
-            ->withErrors(session()->get('errors', app(ViewErrorBag::class)));
+            ->withErrors($errors);
     }
 
     /**
@@ -446,5 +448,15 @@ class Field implements FieldContract
         foreach ($this->beforeRender as $before) {
             $before->call($this);
         }
+    }
+
+    /**
+     * @return array
+     */
+    private function getErrorsMessage()
+    {
+        $errors = session()->get('errors', new MessageBag());
+
+        return $errors->getMessages();
     }
 }
