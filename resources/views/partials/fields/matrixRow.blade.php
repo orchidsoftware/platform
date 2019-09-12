9f87185f3c08922ad22d1ff6acc4563839afee39
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-12 21:58:51 +0000

    Deploy orchidsoftware/platform to github.com/orchidsoftware/platform.git:build

---
+++ resources/views/partials/fields/matrixRow.blade.php
@@ -0,0 +1,21 @@
+<tr>
+    @foreach($columns as $column)
+        <th class="p-0">
+                        <textarea class="form-control border-0 no-resize"
+                                  rows="auto"
+                                  name="{{$name}}[{{  $key ?? 0 }}][{{ $column }}]"
+                        >{{ $row[$column] ?? '' }}</textarea>
+        </th>
+
+        @if ($loop->last)
+            <th class="no-border text-center align-middle">
+                <a href="#"
+                   data-action="fields--matrix#deleteRow"
+                   class="text-xs text-muted"
+                   title="Remove row">
+                    <i class="icon-trash"></i>
+                </a>
+            </th>
+        @endif
+    @endforeach
+</tr>
---
+++ src/Screen/Fields/Matrix.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Screen\Fields;
+
+use Orchid\Screen\Field;
+
+/**
+ * Class Matrix
+ *
+ * @method self columns(array $columns)
+ */
+class Matrix extends Field
+{
+    /**
+     * @var string
+     */
+    protected $view = 'platform::fields.matrix';
+
+    /**
+     * Default attributes value.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'maxRows' => 0,
+        'columns' => [
+            'key',
+            'value',
+        ],
+    ];
+
+    /**
+     * @param string|null $name
+     *
+     * @return self
+     */
+    public static function make(string $name = null): self
+    {
+        return (new static())->name($name)->addBeforeRender(function () {
+            if ($this->get('value') === null) {
+                $this->set('value', []);
+            }
+        });
+    }
+
+    /**
+     * @param int $count
+     *
+     * @return Field|Matrix
+     */
+    public function maxRows(int $count)
+    {
+        return $this->set('maxRows', $count);
+    }
+}
