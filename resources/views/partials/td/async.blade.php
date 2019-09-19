a273a7ff1ebb52786e415f2a8f4a73b9d8f450a8
tabuna <bliz48rus@gmail.com>
2019-09-19 23:24:20 +0300

    refs #958 Json escaping

--- resources/views/partials/td/async.blade.php
+++ resources/views/partials/td/async.blade.php
@@ -3,7 +3,7 @@
    data-modal-title="{{ $title ?? '' }}"
    data-modal-async="true"
    data-modal-key="{{$modal ?? ''}}"
-   data-modal-params="@json($attributes)"
+   data-modal-params='@json($attributes)'
    data-modal-action="{{route($route ?? Route::currentRouteName(),$attributes)}}/{{$method}}"
 >
     {!! $text !!}
