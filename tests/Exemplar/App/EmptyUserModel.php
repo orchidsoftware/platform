d92df5ade70ce5667c415bfa6c0b5d7d2680fda7
tabuna <bliz48rus@gmail.com>
2019-09-23 11:59:20 +0300

    refs #961 Added tests for relations field

---
+++ tests/Exemplar/App/EmptyUserModel.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\Exemplar\App;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
+/**
+ * Class EmptyUserModel
+ */
+class EmptyUserModel extends Model
+{
+
+    /**
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeAsBuilder(Builder $query) : Builder
+    {
+        return $query->where('name', 'RelationTest');
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function scopeAsCollection(Builder $query) : Collection
+    {
+        return $query->where('name', 'RelationTest')->get();
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return array
+     */
+    public function scopeAsArray(Builder $query): array
+    {
+        return $query->where('name', 'RelationTest')->get()->all();
+    }
+
+}
---
+++ tests/Feature/Platform/RelationsTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\Feature\Platform;
+
+use Illuminate\Foundation\Testing\TestResponse;
+use Illuminate\Support\Facades\Crypt;
+use Orchid\Platform\Models\User;
+use Orchid\Tests\Exemplar\App\EmptyUserModel;
+use Orchid\Tests\TestFeatureCase;
+
+class RelationsTest extends TestFeatureCase
+{
+    /**
+     * @var User
+     */
+    protected $users;
+
+    /**
+     *
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->users = factory(User::class, 10)->create([
+            'name' => 'RelationTest',
+        ]);
+    }
+
+    /**
+     * @return array
+     */
+    public function scopeList() : array
+    {
+        return [
+            ['asBuilder'],
+            ['asCollection'],
+            ['asArray'],
+        ];
+    }
+
+    /**
+     * @param string $scope
+     *
+     * @dataProvider scopeList
+     *
+     * @throws \Throwable
+     */
+    public function testScopeModel(string $scope)
+    {
+        $response = $this->getScope($scope);
+        $json = $this->users->pluck('email', 'id')->toArray();
+
+        $response->assertJson($json);
+    }
+
+    /**
+     * @param string $scope
+     *
+     * @return TestResponse
+     */
+    private function getScope(string $scope): TestResponse
+    {
+        return $this
+            ->actingAs($this->createAdminUser())
+            ->post(route('platform.systems.relation'), [
+                'model' => Crypt::encryptString(EmptyUserModel::class),
+                'name'  => Crypt::encryptString('email'),
+                'key'   => Crypt::encryptString('id'),
+                'scope' => Crypt::encryptString($scope),
+            ]);
+    }
+
+}
