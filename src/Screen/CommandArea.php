a09fa5e90bd0996abcf50636dc249ac116360ddd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-01 20:54:17 +0000

    Deploy orchidsoftware/platform to github.com/orchidsoftware/platform.git:build

---
+++ src/Screen/CommandArea.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Screen;
+
+use Orchid\Screen\Contracts\ActionContract;
+
+trait CommandArea {
+
+    /**
+     * @return Action[]
+     */
+    protected function commandBar(): array
+    {
+        return [];
+    }
+
+    /**
+     * @param Repository $repository
+     *
+     * @return array
+     */
+    private function buildCommandBar(Repository $repository): array
+    {
+        return collect($this->commandBar())
+            ->map(function (ActionContract $command) use ($repository) {
+                return $command->build($repository);
+            })->all();
+    }
+
+}
