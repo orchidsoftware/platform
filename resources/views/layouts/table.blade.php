2968ab0097116083b6acfd117c6bd46ec8eae65a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-21 10:07:20 +0000

    Deploy orchidsoftware/platform to github.com/orchidsoftware/platform.git:build

--- resources/views/layouts/table.blade.php
+++ resources/views/layouts/table.blade.php
@@ -1,47 +1,48 @@
-<div class="row table-responsive-lg @if ($striped) table-striped @endif">
-    <table class="table">
-        <thead>
-        <tr>
-            @foreach($columns as $column)
-                {!! $column->buildTh() !!}
-            @endforeach
-        </tr>
-        </thead>
-        <tbody>
-
-        @foreach($rows as $source)
+<div class="row">
+    <div class="w-full table-responsive-lg @if ($striped) table-striped @endif">
+        <table class="table">
+            <thead>
             <tr>
                 @foreach($columns as $column)
-                   {!! $column->buildTd($source) !!}
+                    {!! $column->buildTh() !!}
                 @endforeach
             </tr>
-        @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
 
-    @if(
-    (method_exists($rows,'total') && optional($rows)->total() === 0) ||
-    (method_exists($rows,'count') && optional($rows)->count() === 0) ||
-    (is_array($rows) && count($rows) === 0)
-    )
+            @foreach($rows as $source)
+                <tr>
+                    @foreach($columns as $column)
+                        {!! $column->buildTd($source) !!}
+                    @endforeach
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
 
-        <div class="text-center bg-white pt-5 pb-5 w-full">
-            <h3 class="font-thin">
-                <i class="{{ $iconNotFound }} block m-b"></i>
-                {!!  $textNotFound !!}
-            </h3>
+        @if(
+        (method_exists($rows,'total') && optional($rows)->total() === 0) ||
+        (method_exists($rows,'count') && optional($rows)->count() === 0) ||
+        (is_array($rows) && count($rows) === 0)
+        )
 
-            {!! $subNotFound !!}
-        </div>
+            <div class="text-center bg-white pt-5 pb-5 w-full">
+                <h3 class="font-thin">
+                    <i class="{{ $iconNotFound }} block m-b"></i>
+                    {!!  $textNotFound !!}
+                </h3>
 
-    @endif
+                {!! $subNotFound !!}
+            </div>
+
+        @endif
 
 
-    @includeWhen($rows instanceof \Illuminate\Contracts\Pagination\Paginator && $rows->total() > 0,
-        'platform::layouts.pagination',
-        ['paginator' => $rows]
-      )
+        @includeWhen($rows instanceof \Illuminate\Contracts\Pagination\Paginator && $rows->total() > 0,
+            'platform::layouts.pagination',
+            ['paginator' => $rows]
+          )
 
+    </div>
 </div>
-
 
--- resources/views/partials/notification.blade.php
+++ resources/views/partials/notification.blade.php
@@ -1,7 +1,8 @@
-<button formaction="{{ url()->current() }}/{{ $notification->id }}/maskNotification" class="btn btn-link text-left">
+<button formaction="{{ url()->current() }}/{{ $notification->id }}/maskNotification" class="btn btn-link text-left p-4">
     <i class="icon-circle text-{{ $notification->data['type'] }} @if($notification->read()) opacity @endif pull-left m-t-sm text-xs"></i>
-    <span class="clear m-l-md block">
-        {{$notification->data['title'] ?? ''}}
+    <span class="clear pl-3 block">
+        <span class="w-full w-b-k w-s-n">{{$notification->data['title'] ?? ''}}</span>
+        <small class="text-muted pl-1">/ {{ $notification->created_at->diffForHumans() }}</small>
         <br>
         <small class="text-muted w-full w-b-k w-s-n">{{$notification->data['message'] ?? ''}}</small>
     </span>
