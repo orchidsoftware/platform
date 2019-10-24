38990a035d913b85b48346353c5ab5f097321441
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-24 17:21:20 +0000

    Deploy orchidsoftware/platform to github.com/orchidsoftware/platform.git:build

--- src/Screen/Fields/ViewField.php
+++ src/Screen/Fields/ViewField.php
@@ -15,64 +15,11 @@ use Orchid\Screen\Field;
 class ViewField extends Field
 {
     /**
-     * Required Attributes.
-     *
-     * @var array
-     */
-    protected $required = [
-        'view',
-    ];
-
-    /**
-     * Default attributes value.
-     *
-     * @var array
-     */
-    protected $attributes = [];
-
-    /**
-     * Attributes available for a particular tag.
-     *
-     * @var array
-     */
-    protected $inlineAttributes = [
-        'accept',
-        'accesskey',
-        'autocomplete',
-        'autofocus',
-        'checked',
-        'disabled',
-        'form',
-        'formaction',
-        'formenctype',
-        'formmethod',
-        'formnovalidate',
-        'formtarget',
-        'language',
-        'lineNumbers',
-        'list',
-        'max',
-        'maxlength',
-        'min',
-        'name',
-        'pattern',
-        'placeholder',
-        'readonly',
-        'required',
-        'size',
-        'src',
-        'step',
-        'tabindex',
-        'type',
-        'value',
-    ];
-
-    /**
      * @param string $view
      *
-     * @return self
+     * @return ViewField
      */
-    public function view(string $view)
+    public function view(string $view): self
     {
         $this->view = $view;
 
