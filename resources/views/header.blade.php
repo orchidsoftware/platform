a36437cea8ebf83f5a540a94ac9aa20f1a324d75
tabuna <bliz48rus@gmail.com>
2019-11-12 01:51:32 +0300

    Dark favicon for future notice

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,17 +1,5 @@
 @push('head')
-    <link rel="manifest" href="{{ route('platform.resource', ['orchid', 'favicon/manifest.json']) }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ route('platform.resource', ['orchid', 'favicon/apple-touch-icon.png']) }}">
-    <link rel="shortcut icon" type="image/png" sizes="32x32" href="{{ route('platform.resource', ['orchid', 'favicon/favicon-32x32.png']) }}">
-    <link rel="shortcut icon" type="image/png" sizes="16x16" href="{{ route('platform.resource', ['orchid', 'favicon/favicon-16x16.png']) }}">
-    <link rel="mask-icon" href="{{ route('platform.resource', ['orchid', 'favicon/safari-pinned-tab.svg']) }}" color="#1a2021">
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="application-name" content="ORCHID">
-    <meta name="apple-mobile-web-app-title" content="ORCHID">
-    <meta name="theme-color" content="#3e4348">
-    <meta name="msapplication-navbutton-color" content="#3e4348">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="msapplication-starturl" content="/">
+    <link id="favicon" rel="icon" href="{{ route('platform.resource', ['orchid', 'favicon/orchid-pinned-tab.svg']) }}">
 @endpush
 
 <p class="h2 n-m font-thin v-center">
