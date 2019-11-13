3a9fd5f0dea2bde2617f34e3d648bb7c91648659
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-13 12:44:15 +0000

    Deploy orchidsoftware/platform to github.com/orchidsoftware/platform.git:build

--- resources/views/fields/range.blade.php
+++ resources/views/fields/range.blade.php
@@ -1,20 +1,19 @@
 @component($typeForm,get_defined_vars())
-    <div class="row">
-        <div class="col-md-6 pr-0">
+    <div class="row" data-controller="fields--datetime"
+         data-fields--datetime-allow-input="true"
+         data-fields--datetime-range="#end_{{ \Illuminate\Support\Str::slug($attributes['name']) }}">
+        <div class="col-md-6 pr-1">
             <div class="form-group">
                 <input type="text"
                        @isset($attributes['form']) form="{{ $attributes['form'] ?? null }}" @endisset
                        name="{{ $attributes['name'] }}[start]"
                        id='start_{{ $attributes['name'] }}'
                        value="{{ $attributes['value']['start'] ?? null }}"
-                       class="form-control"
-                       data-controller="fields--datetime"
-                       data-fields--datetime-allow-input="true"
-                       data-fields--datetime-range="#end_{{ \Illuminate\Support\Str::slug($attributes['name']) }}">
+                       class="form-control">
             </div>
         </div>
 
-        <div class="col-md-6 pl-0">
+        <div class="col-md-6 pl-1">
             <div class="form-group">
                 <input type="text"
                        @isset($attributes['form']) form="{{ $attributes['form'] ?? null }}" @endisset
