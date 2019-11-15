809efbe2f2dc086dd221427c71fa9b709811b5a5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-15 17:56:35 +0000

    Deploy orchidsoftware/platform to github.com/orchidsoftware/platform.git:build

--- resources/views/actions/modal.blade.php
+++ resources/views/actions/modal.blade.php
@@ -5,7 +5,7 @@
             data-modal-title="{{ $modalTitle ?? $title ??  '' }}"
             data-modal-key="{{ $modal ?? '' }}"
             data-modal-async="{{ $async }}"
-            data-modal-params='{{ $asyncParameters }}'
+            data-modal-params='@json($asyncParameters)'
             data-modal-action="{{ $action }}">
         <i class="{{ $icon ?? '' }} m-r-xs"></i>{{ $name ?? '' }}
     </button>
