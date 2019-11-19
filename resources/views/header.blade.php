2a60e67b5d2fd88672b0047e58c7cd19efcdcccf
tabuna <bliz48rus@gmail.com>
2019-11-20 01:38:26 +0300

    refs #1009 Added background update and tab synchronization

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,11 @@
 @push('head')
-    <link id="favicon" rel="icon" href="{{ route('platform.resource', ['orchid', 'favicon/orchid-pinned-tab.svg']) }}">
+    <link
+          href="{{ route('platform.resource', ['orchid', 'favicon/orchid-pinned-tab.svg']) }}"
+          data-turbolinks-permanent
+          data-controller="layouts--favicon"
+          id="favicon"
+          rel="icon"
+    >
 @endpush
 
 <p class="h2 n-m font-thin v-center">
