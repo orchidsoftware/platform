c1092547e9b8256e993f10cd84ee4870f04d785c
tabuna <bliz48rus@gmail.com>
2019-12-04 18:55:13 +0300

    Init Laravel Dusk

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Orchid\Tests\TestBrowserCase;
+
+class LoginTest extends TestBrowserCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     * @throws \Throwable
+     */
+    public function testDisplayPage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visitRoute('platform.login')
+                ->assertSee('Sign in to your account');
+        });
+    }
+
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     * @throws \Throwable
+     */
+    public function testBasicLogin()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visitRoute('platform.login')
+                ->type('email', 'admin@admin.com')
+                ->type('password', 'password')
+                ->press('Login')
+                ->waitForLocation('/dashboard/main')
+                ->assertSee('Example screen');
+        });
+    }
+
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     * @throws \Throwable
+     */
+    public function testErrorLogin()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visitRoute('platform.login')
+                ->type('email', 'admin@admin.com')
+                ->type('password', 'error')
+                ->press('Login')
+                ->waitForText('The details you entered did not match our records. Please double-check and try again.')
+                ->assertSee('The details you entered did not match our records. Please double-check and try again.');
+        });
+    }
+
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     * @throws \Throwable
+     */
+    public function testLogout()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visitRoute('platform.login')
+                ->type('email', 'admin@admin.com')
+                ->type('password', 'password')
+                ->press('Login')
+                ->waitForLocation('/dashboard/main')
+                ->assertSee('Example screen')
+                ->clickLink('admin')
+                ->assertSee('Sign out')
+                ->click('@logout-button')
+                ->waitForLocation('/')
+                ->visitRoute('platform.main')
+                ->waitForLocation('/dashboard/login')
+                ->assertSee('Sign in to your account');
+        });
+    }
+}
