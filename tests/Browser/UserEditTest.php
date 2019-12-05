d73f0705a1fbb27c11d74c63fae5359896c02360
Alexandr Chernyaev <bliz48rus@gmail.com>
2019-12-05 22:25:24 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Browser/UserEditTest.php
+++ tests/Browser/UserEditTest.php
@@ -7,7 +7,6 @@ namespace Orchid\Tests\Browser;
 use Laravel\Dusk\Browser;
 use Orchid\Tests\TestBrowserCase;
 
-
 class UserEditTest extends TestBrowserCase
 {
     public function testListPage(): void
@@ -36,28 +35,27 @@ class UserEditTest extends TestBrowserCase
                 ->waitForText($user->name)
                 ->waitForText('Close')
                 ->waitForText('Apply')
-                ->type('user[name]', $user->name . '-async-test')
+                ->type('user[name]', $user->name.'-async-test')
                 ->press('Apply')
                 ->waitForText('User was saved.')
-                ->assertSee($user->name . '-async-test');
+                ->assertSee($user->name.'-async-test');
         });
     }
 
     public function testEditPage(): void
     {
         $this->browse(function (Browser $browser) {
-
             $user = $this->createAdminUser();
 
             $browser->loginAs($user)
                 ->visitRoute('platform.systems.users')
                 ->clickLink($user->name, 'table a')
                 ->waitForRoute('platform.systems.users.edit', $user)
-                ->type('user[name]', $user->name . '-edit-test')
+                ->type('user[name]', $user->name.'-edit-test')
                 ->press('Save')
                 ->waitForRoute('platform.systems.users')
                 ->assertSee('User was saved.')
-                ->assertSee($user->name . '-edit-test');
+                ->assertSee($user->name.'-edit-test');
         });
     }
 }
