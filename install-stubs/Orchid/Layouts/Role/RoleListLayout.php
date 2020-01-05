13d241cfed127223535796c3c0216e7ff85cd65d
Alexandr Chernyaev <bliz48rus@gmail.com>
2020-01-05 16:14:22 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- install-stubs/Orchid/Layouts/Role/RoleListLayout.php
+++ install-stubs/Orchid/Layouts/Role/RoleListLayout.php
@@ -19,7 +19,7 @@ class RoleListLayout extends Table
     /**
      * @return array
      */
-    public function columns() : array
+    public function columns(): array
     {
         return [
             TD::set('name', __('Name'))
