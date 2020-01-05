e517b0e660e2bdcbf925c93b0d4b5d0498fb2788
tabuna <bliz48rus@gmail.com>
2020-01-06 00:30:50 +0300

    revert #965

--- install-stubs/Orchid/Layouts/User/UserListLayout.php
+++ install-stubs/Orchid/Layouts/User/UserListLayout.php
@@ -11,7 +11,6 @@ use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Actions\ModalToggle;
 use Orchid\Screen\Layouts\Table;
 use Orchid\Screen\TD;
-use Orchid\Screen\Templates\Persona;
 
 class UserListLayout extends Table
 {
@@ -30,7 +29,23 @@ class UserListLayout extends Table
                 ->sort()
                 ->filter(TD::FILTER_TEXT)
                 ->render(function (User $user) {
-                    return new Persona($user->presenter());
+                    // Please use Blade templates.
+                    // This will be a simple example: view('path', ['user' => $user])
+                    $avatar = e($user->getAvatar());
+                    $name = e($user->getNameTitle());
+                    $sub = e($user->getSubTitle());
+                    $route = route('platform.systems.users.edit', $user->id);
+                    return "<a href='{$route}'>
+                                <div class='d-sm-flex flex-row flex-wrap text-center text-sm-left align-items-center'>
+                                    <span class='thumb-xs avatar m-r-xs d-none d-md-inline-block'>
+                                      <img src='{$avatar}' class='bg-light'>
+                                    </span>
+                                    <div class='ml-sm-3 ml-md-0 ml-xl-3 mt-2 mt-sm-0 mt-md-2 mt-xl-0'>
+                                      <p class='mb-0'>{$name}</p>
+                                      <small class='text-xs text-muted'>{$sub}</small>
+                                    </div>
+                                </div>
+                            </a>";
                 }),
 
             TD::set('email', __('Email'))
