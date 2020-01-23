5bd34ea0dd59ada420cc98e24ac2d39106e4f8f2
tabuna <bliz48rus@gmail.com>
2020-01-24 00:19:22 +0300

    Fixed the "Go back" button on the page 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,8 +6,8 @@
         <div class="display-1 text-muted mb-5 mt-sm-5 mt-0"><i class="icon-bug"></i> 404</div>
         <h1 class="h2 mb-3">{{ __("You request a page that doesn't exist.") }}</h1>
         <p class="h4 text-muted font-weight-normal mb-7">{{ __("It's possible you entered the wrong address or the link doesn't work.") }}</p>
-        <a class="btn btn-link m-t-md" href="javascript:history.back()">
-            <i class="fe fe-arrow-left mr-2"></i>{{ __('Go back') }}
+        <a class="btn btn-link mt-3" href="javascript:history.back()">
+            <span class="center"><i class="fe fe-arrow-left mr-2"></i>{{ __('Go back') }}</span>
         </a>
     </div>
 
