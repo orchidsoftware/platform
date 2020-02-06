6d74bca4ae818a34e0b29d27e2758d40fc4287cc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-06 09:18:24 +0000

    Deploy orchidsoftware/platform to github.com/orchidsoftware/platform.git:build

--- src/Platform/Http/Middleware/AccessMiddleware.php
+++ src/Platform/Http/Middleware/AccessMiddleware.php
@@ -4,56 +4,12 @@ declare(strict_types=1);
 
 namespace Orchid\Platform\Http\Middleware;
 
-use Carbon\Carbon;
-use Closure;
-use Illuminate\Contracts\Auth\Guard;
-use Illuminate\Contracts\Routing\ResponseFactory;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-
 /**
- * Class AccessMiddleware.
+ * @deprecated use Access
+ *
+ * Class AccessMiddleware
  */
-class AccessMiddleware
+class AccessMiddleware extends Access
 {
-    /**
-     * @var Guard
-     */
-    protected $auth;
 
-    /**
-     * AccessMiddleware constructor.
-     *
-     * @param Guard $auth
-     */
-    public function __construct(Guard $auth)
-    {
-        $this->auth = $auth;
-    }
-
-    /**
-     * @param Request $request
-     * @param Closure $next
-     *
-     * @return ResponseFactory|RedirectResponse|Response|mixed
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        Carbon::setLocale(config('app.locale'));
-
-        if ($this->auth->guest()) {
-            if ($request->ajax() || $request->wantsJson()) {
-                return response('Unauthorized.', 401);
-            }
-
-            return redirect()->guest(route('platform.login'));
-        }
-
-        if ($this->auth->user()->hasAccess('platform.index')) {
-            return $next($request);
-        }
-
-        abort(403);
-    }
 }
---
+++ tests/Unit/TurbolinksLocationTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\Unit;
+
+use Illuminate\Support\Facades\Route;
+use Orchid\Platform\Http\Middleware\TurbolinksLocation;
+use Orchid\Tests\TestUnitCase;
+
+class TurbolinksLocationTest extends TestUnitCase
+{
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Route::middleware(TurbolinksLocation::class)->group(static function () {
+
+            Route::post('turbolink-html-response-test', static function () {
+                return response()->make('Availability headers location');
+            });
+
+
+            Route::post('turbolink-json-response-test', static function () {
+                return response()->json([
+                    'Availability headers location',
+                ]);
+            });
+
+            Route::post('turbolink-stream-response-test', static function () {
+                return response()->streamDownload(function () {
+                    echo 'Not headers location';
+                }, 'test.md');
+            });
+
+            Route::post('turbolink-file-response-test', static function () {
+                return response()->download(__FILE__);
+            });
+
+
+        });
+    }
+
+    public function testHtmlResponse()
+    {
+        $response = $this->post('turbolink-html-response-test');
+        $response->assertHeader('Turbolinks-Location', url('turbolink-html-response-test'));
+    }
+
+    public function testJsonResponse()
+    {
+        $response = $this->post('turbolink-json-response-test');
+        $response->assertHeader('Turbolinks-Location', url('turbolink-json-response-test'));
+    }
+
+    public function testStreamResponse()
+    {
+        $response = $this->post('turbolink-stream-response-test');
+        $response->assertHeaderMissing('Turbolinks-Location',);
+    }
+
+    public function testFileResponse()
+    {
+        $response = $this->post('turbolink-file-response-test');
+        $response->assertHeaderMissing('Turbolinks-Location',);
+    }
+}
