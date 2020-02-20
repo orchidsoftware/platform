283b4ebaa61e7e48cd58e2cb2664f457ef99ee2e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-20 16:57:09 +0000

    Deploy orchidsoftware/platform to github.com/orchidsoftware/platform.git:build

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -17,7 +17,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '6.10.1';
+    public const VERSION = '6.10.2';
 
     /**
      * The Dashboard configuration options.
