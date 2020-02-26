eccaccbf6fc9f7841c9a73dfc3d8130e714c7690
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-26 19:29:03 +0000

    Deploy orchidsoftware/platform to github.com/orchidsoftware/platform.git:build

--- resources/views/auth/lockme.blade.php
+++ resources/views/auth/lockme.blade.php
@@ -16,11 +16,6 @@
 @enderror
 
 <div class="form-group">
-    <label class="form-label w-full">
-        {{__('Password')}}
-        <a href="{{ route('platform.password.request') }}" class="float-right small">{{__('Forgot your password?')}}</a>
-    </label>
-
     <input type="hidden" name="remember" value="true">
     <div data-controller="fields--password"
          class="input-icon"
@@ -29,14 +24,17 @@
                data-target="fields--password.password"
                name="password"
                value="{{ old('password') }}"
-               placeholder="{{__('Enter your password')}}" required>
+               placeholder="{{__('Enter your password')}}"
+               required
+               tabindex="1"
+        >
         <div class="input-icon-addon cursor" data-action="click->fields--password#change">
             <i class="icon-eye" data-target="fields--password.icon"></i>
         </div>
     </div>
 
     @error('password')
-        <span class="invalid-feedback text-danger">
+    <span class="invalid-feedback text-danger">
             {{ $errors->first('password') }}
         </span>
     @enderror
@@ -49,7 +47,7 @@
         </a>
     </div>
     <div class="form-group col-md-6 col-xs-12">
-        <button id="button-login" type="submit" class="btn btn-default btn-block">
+        <button id="button-login" type="submit" class="btn btn-default btn-block" tabindex="2">
             <i class="icon-login text-xs mr-2"></i> {{__('Login')}}
         </button>
     </div>
