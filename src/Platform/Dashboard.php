f24a1c3356db7caaf362a6809f0dc46d71a2fed4
Alexandr Chernyaev <bliz48rus@gmail.com>
2020-03-16 17:29:56 +0300

    Fixed limiting length of the name and description for the screen (#1066)

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -16,7 +16,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '7.0.1';
+    public const VERSION = '7.0.2';
 
     /**
      * The Dashboard configuration options.
