cff59876693ce44ef5dfc21a2a55c952518b9f61
adxin <46772040+adxin@users.noreply.github.com>
2020-03-17 22:14:44 +0500

    Fixed "Max file size null MB" (#1067)
    Fixed "Max file size null MB"

--- tests/Unit/SupportFormatsTest.php
+++ tests/Unit/SupportFormatsTest.php
@@ -1,10 +1,10 @@
 <?php
-
 declare(strict_types=1);
 
 namespace Orchid\Tests\Unit;
 
 use Orchid\Support\Formats;
+use Orchid\Support\Init;
 use Orchid\Tests\TestUnitCase;
 
 class SupportFormatsTest extends TestUnitCase
@@ -22,5 +22,12 @@ class SupportFormatsTest extends TestUnitCase
         $this->assertEquals(Formats::formatBytes(10000000), '9.54 MB');
         $this->assertEquals(Formats::formatBytes(1000000000000), '931.32 GB');
         $this->assertEquals(Formats::formatBytes(10000000000000), '9.09 TB');
+    }
+
+    public function testConvertBytes()
+    {
+        $this->assertEquals(Init::maxFileUpload(Init::KB), 1048576);
+        $this->assertEquals(Init::maxFileUpload(Init::MB), 1024);
+        $this->assertEquals(Init::maxFileUpload(Init::GB), 1);
     }
 }
