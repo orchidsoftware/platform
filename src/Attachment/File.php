0d7352868cfba7fba8f4bc1b048686c07c1e3825
Alexandr Chernyaev <bliz48rus@gmail.com>
2020-03-18 14:26:08 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Attachment/File.php
+++ src/Attachment/File.php
@@ -157,7 +157,7 @@ class File
             'mime'          => $this->getMimeType(),
             'extension'     => $this->getClientOriginalExtension(),
             'size'          => $this->file->getSize(),
-            'path'          => $this->date . '/',
+            'path'          => $this->date.'/',
             'hash'          => $this->hash,
             'disk'          => $this->disk,
             'group'         => $this->group,
