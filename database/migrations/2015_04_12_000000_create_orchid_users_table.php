8ff1829e9282dda3d4d1a8c90d908815424fbdcb
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2020-04-19 17:02:14 +0200

    moved empty permissions check into UserAccess.php

--- database/migrations/2015_04_12_000000_create_orchid_users_table.php
+++ database/migrations/2015_04_12_000000_create_orchid_users_table.php
@@ -13,7 +13,7 @@ class CreateOrchidUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->timestamp('last_login')->nullable();
-            $table->jsonb('permissions')->default('{}')->nullable();
+            $table->jsonb('permissions')->nullable();
         });
     }
 
