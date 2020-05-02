4be34ec1da5f43b5f72a2a2b909ed394767aafa2
tabuna <bliz48rus@gmail.com>
2020-05-02 15:51:57 +0300

    Fixed misprint. Added test for redirect many params

--- tests/Feature/Platform/AsyncScreenTest.php
+++ tests/Feature/Platform/AsyncScreenTest.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Orchid\Tests\Feature\Platform;
 
+use Illuminate\Support\Str;
 use Orchid\Tests\App\Layouts\DependentSumListener;
 use Orchid\Tests\TestFeatureCase;
 
 class AsyncScreenTest extends TestFeatureCase
 {
-    public function testAcyncDependentListenerScreen()
+    public function testAsyncDependentListenerScreen(): void
     {
         /** @var DependentSumListener $layout */
         $layout = $this->app->make(DependentSumListener::class);
@@ -29,5 +30,24 @@ class AsyncScreenTest extends TestFeatureCase
 
         $this->assertStringContainsString('value="5"', $response->getContent());
         $this->assertStringContainsString('The result of adding', $response->getContent());
+    }
+
+    public function testAsyncNotFoundScreen(): void
+    {
+        /** @var DependentSumListener $layout */
+        $layout = $this->app->make(DependentSumListener::class);
+
+        $response = $this
+            ->actingAs($this->createAdminUser())
+            ->post(route('test.dependent-listener', [
+                'method'   => Str::random(),
+                'argument' => 'asyncSum',
+            ]), [
+                'first'  => 2,
+                'second' => 3,
+            ]);
+
+
+        $response->assertNotFound();
     }
 }
