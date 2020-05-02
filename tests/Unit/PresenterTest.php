fd865f16cabce0bafd92c6c1caa6bc1eee9d4de7
tabuna <bliz48rus@gmail.com>
2020-05-02 19:55:01 +0000

    Deploying to build from  @ 8d00357c20023f363d06f4edc21ee3ef1d54c8d6 🚀

---
+++ tests/Unit/PresenterTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\Unit;
+
+use Orchid\Support\Presenter;
+use Orchid\Tests\TestUnitCase;
+
+class PresenterTest extends TestUnitCase
+{
+    public function testPresenterMethod(): void
+    {
+        $presenter = $this->getPresenterClass();
+
+        $this->assertFalse($presenter->getStatus());
+    }
+
+    public function testPresenterProperty(): void
+    {
+        $presenter = $this->getPresenterClass();
+
+        $this->assertTrue(isset($presenter->status));
+        $this->assertFalse($presenter->status);
+    }
+
+    /**
+     * @return Presenter
+     */
+    protected function getPresenterClass(): Presenter
+    {
+        $class = new class {
+
+            /**
+             * @var bool
+             */
+            public $status = false;
+        };
+
+        return new class($class) extends Presenter {
+
+            /**
+             * @return bool
+             */
+            public function getStatus(): bool
+            {
+                return $this->entity->status;
+            }
+        };
+
+
+    }
+}
