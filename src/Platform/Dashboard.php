4927f5da52b18f80a353dfd786297cce7e9c8eba
tabuna <bliz48rus@gmail.com>
2020-05-05 14:21:41 +0000

    Deploying to build from  @ ed10d51e31a94321fece1b010377c867f9182afe 🚀

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -16,7 +16,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '7.2.2';
+    public const VERSION = '7.2.3';
 
     /**
      * The Dashboard configuration options.
