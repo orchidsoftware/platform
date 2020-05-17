d4a1710c06e2f5310f86aafbbb97ad4970d742aa
Mohammed Al Ashaal <m7medalash3al@gmail.com>
2020-05-17 22:05:43 +0200

    added withoutApplyButton to modal based layouts (#1099)
    * #1095 added withoutApplyButton and withoutCloseButton to modal based layouts

--- src/Screen/Layouts/Modal.php
+++ src/Screen/Layouts/Modal.php
@@ -50,14 +50,16 @@ class Modal extends Base
     public function __construct(string $key, array $layouts = [])
     {
         $this->variables = [
-            'apply'      => __('Apply'),
-            'close'      => __('Close'),
-            'size'       => '',
-            'type'       => self::TYPE_CENTER,
-            'key'        => $key,
-            'title'      => $key,
-            'turbolinks' => true,
-            'commandBar' => [],
+            'apply'                 => __('Apply'),
+            'close'                 => __('Close'),
+            'size'                  => '',
+            'type'                  => self::TYPE_CENTER,
+            'key'                   => $key,
+            'title'                 => $key,
+            'turbolinks'            => true,
+            'commandBar'            => [],
+            'withoutApplyButton'    => false,
+            'withoutCloseButton'    => false,
         ];
 
         $this->layouts = $layouts;
@@ -90,6 +92,34 @@ class Modal extends Base
     public function applyButton(string $text): self
     {
         $this->variables['apply'] = $text;
+
+        return $this;
+    }
+
+    /**
+     * Whether to disable the apply button or not.
+     *
+     * @param bool $withoutApplyButton
+     *
+     * @return Modal
+     */
+    public function withoutApplyButton(bool $withoutApplyButton = true): self
+    {
+        $this->variables['withoutApplyButton'] = $withoutApplyButton;
+
+        return $this;
+    }
+
+    /**
+     * Whether to disable the close button or not.
+     *
+     * @param bool $withoutCloseButton
+     *
+     * @return Modal
+     */
+    public function withoutCloseButton(bool $withoutCloseButton = true): self
+    {
+        $this->variables['withoutCloseButton'] = $withoutCloseButton;
 
         return $this;
     }
