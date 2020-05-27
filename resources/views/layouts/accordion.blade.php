1fa6b32c04ed8d7aac48406a126456ae47d14c14
Ostap B <osbre@protonmail.com>
2020-05-27 17:01:09 +0300

    Change arrow icons for Accordion (#1107)

--- resources/views/layouts/accordion.blade.php
+++ resources/views/layouts/accordion.blade.php
@@ -1,20 +1,20 @@
-<div id="accardion-{{$templateSlug}}" class="accardion">
+<div id="accordion-{{$templateSlug}}" class="accordion">
     @foreach($manyForms as $name => $forms)
-        <div class="accardion-heading border-bottom @if ($loop->index) collapsed @endif"
+        <div class="accordion-heading border-bottom @if ($loop->index) collapsed @endif"
              id="heading-{{\Illuminate\Support\Str::slug($name)}}"
              data-toggle="collapse"
              data-target="#collapse-{{\Illuminate\Support\Str::slug($name)}}"
              aria-expanded="true"
              aria-controls="collapse-{{\Illuminate\Support\Str::slug($name)}}">
             <h6 class="btn btn-link btn-group-justified pt-2 pb-2 mb-0 pr-0 pl-0 v-center">
-                <span class="icon-accardion text-xs mr-2"></span> {!! $name !!}
+                <span class="icon-accordion text-xs mr-2"></span> {!! $name !!}
             </h6>
         </div>
 
         <div id="collapse-{{\Illuminate\Support\Str::slug($name)}}"
              class="border-bottom mt-2 collapse @if (!$loop->index) show @endif"
              aria-labelledby="heading-{{\Illuminate\Support\Str::slug($name)}}"
-             data-parent="#accardion-{{$templateSlug}}">
+             data-parent="#accordion-{{$templateSlug}}">
             @foreach($forms as $form)
                 {!! $form !!}
             @endforeach
