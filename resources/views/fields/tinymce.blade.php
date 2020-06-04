ce0e7d05a8095a55d2367c2a494fccddbaf9fd27
tabuna <bliz48rus@gmail.com>
2020-06-04 13:13:37 +0300

    refs #873 Remove TinyMCE

--- resources/views/fields/tinymce.blade.php
+++
@@ -1,8 +0,0 @@
-@component($typeForm, get_defined_vars())
-    <div data-controller="fields--tinymce" data-theme="{{$theme ?? 'inlite'}}">
-        <div class="tinymce border p-3" id="tinymce-wrapper-{{$id}}" style="min-height: {{ $attributes['height'] }}">
-            {!! $value !!}
-        </div>
-        <input type="hidden" @attributes($attributes)>
-    </div>
-@endcomponent
