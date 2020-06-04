c5c0590616d0bdc603be497d4280f4b1d8e2e444
Alexandr Chernyaev <bliz48rus@gmail.com>
2020-06-04 13:39:12 +0300

    refs #1073 Replace breadcrumbs package

--- resources/views/partials/breadcrumbs.blade.php
+++ resources/views/partials/breadcrumbs.blade.php
@@ -1,13 +1,17 @@
-@if (count($breadcrumbs) > 1)
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb padder m-0">
-            @foreach ($breadcrumbs as $breadcrumb)
-                @if ($breadcrumb->url && !$loop->last)
-                    <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
-                @else
-                    <li class="breadcrumb-item active">{{ $breadcrumb->title }}</li>
-                @endif
-            @endforeach
-        </ol>
-    </nav>
-@endif
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb padder m-0">
+        @foreach (Breadcrumbs::current() as $crumbs)
+            @if ($crumbs->url() && !$loop->last)
+                <li class="breadcrumb-item">
+                    <a href="{{ $crumbs->url() }}">
+                        {{ $crumbs->title() }}
+                    </a>
+                </li>
+            @else
+                <li class="breadcrumb-item active">
+                    {{ $crumbs->title() }}
+                </li>
+            @endif
+        @endforeach
+    </ol>
+</nav>
