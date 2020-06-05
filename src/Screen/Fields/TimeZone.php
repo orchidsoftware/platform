115f03aeff74c9c6c6a8d9f91b85cc415dc6b2ee
tabuna <bliz48rus@gmail.com>
2020-06-05 12:06:21 +0300

    Added trait for multiple attribute

--- src/Screen/Fields/TimeZone.php
+++ src/Screen/Fields/TimeZone.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Orchid\Screen\Fields;
 
 use DateTimeZone;
+use Orchid\Screen\Concerns\Multipliable;
 use Orchid\Screen\Field;
 
 /**
@@ -22,6 +23,8 @@ use Orchid\Screen\Field;
  */
 class TimeZone extends Field
 {
+    use Multipliable;
+
     /**
      * @var string
      */
@@ -47,7 +50,6 @@ class TimeZone extends Field
         'autofocus',
         'disabled',
         'form',
-        'multiple',
         'name',
         'required',
         'size',
@@ -63,25 +65,16 @@ class TimeZone extends Field
     }
 
     /**
-     * @return self
-     */
-    public function multiple(): self
-    {
-        $this->attributes['multiple'] = 'multiple';
-
-        return $this;
-    }
-
-    /**
      * @param int $time
      *
      * @return self
      */
     public function listIdentifiers(int $time = DateTimeZone::ALL): self
     {
-        $timeZone = collect(DateTimeZone::listIdentifiers($time))->mapWithKeys(static function ($timezone) {
-            return [$timezone => $timezone];
-        })->toArray();
+        $timeZone = collect(DateTimeZone::listIdentifiers($time))
+            ->mapWithKeys(static function ($timezone) {
+                return [$timezone => $timezone];
+            })->toArray();
 
         $this->set('options', $timeZone);
 
