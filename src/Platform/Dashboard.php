46df6f776290f9cc613800c78c29f6fdbd5a6ade
tabuna <bliz48rus@gmail.com>
2020-06-15 13:18:19 +0300

    ## 7.10.3 - 2020-06-15

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -18,7 +18,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '7.10.2';
+    public const VERSION = '7.10.3';
 
     /**
      * The Dashboard configuration options.
