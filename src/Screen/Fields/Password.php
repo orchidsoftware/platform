a7e696269b07e2ebededd01a586a9b33c340f8e5
tabuna <bliz48rus@gmail.com>
2020-06-26 12:29:20 +0300

    Added Multipliable traits

--- src/Screen/Fields/Password.php
+++ src/Screen/Fields/Password.php
@@ -24,7 +24,6 @@ use Orchid\Screen\Field;
  * @method Password max(int $value)
  * @method Password maxlength(int $value)
  * @method Password min(int $value)
- * @method Password multiple($value = true)
  * @method Password name(string $value = null)
  * @method Password pattern($value = true)
  * @method Password placeholder(string $value = null)
@@ -75,7 +74,6 @@ class Password extends Field
         'max',
         'maxlength',
         'min',
-        'multiple',
         'name',
         'pattern',
         'placeholder',
--- src/Screen/Fields/TimeZone.php
+++ src/Screen/Fields/TimeZone.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Orchid\Screen\Fields;
 
 use DateTimeZone;
+use Orchid\Screen\Concerns\Multipliable;
 use Orchid\Screen\Field;
 
 /**
@@ -22,6 +23,8 @@ use Orchid\Screen\Field;
  */
 class TimeZone extends Field
 {
+    use Multipliable;
+
     /**
      * @var string
      */
@@ -47,7 +50,6 @@ class TimeZone extends Field
         'autofocus',
         'disabled',
         'form',
-        'multiple',
         'name',
         'required',
         'size',
@@ -60,16 +62,6 @@ class TimeZone extends Field
     public function __construct()
     {
         $this->listIdentifiers();
-    }
-
-    /**
-     * @return self
-     */
-    public function multiple(): self
-    {
-        $this->attributes['multiple'] = 'multiple';
-
-        return $this;
     }
 
     /**
