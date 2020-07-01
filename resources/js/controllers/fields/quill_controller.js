d112865940bd093c578301d5fb57dd15149a4a91
mavsan <mavsan@yandex.ru>
2020-07-01 18:53:10 +1000

    Update quill_controller.js
    codeclimate

--- resources/js/controllers/fields/quill_controller.js
+++ resources/js/controllers/fields/quill_controller.js
@@ -9,11 +9,11 @@ export default class extends Controller {
         const selector = this.element.querySelector('.quill').id;
         const input = this.element.querySelector('input');
         const colors = ['#000000', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', '#444444', '#5c0000', '#663d00', '#666600', '#003700', '#002966', '#3d1466', 'custom-color'];
-        
+
         function customColor(value) {
-            return value == 'custom-color' ? window.prompt('Enter Color Code (#c0ffee or rgba(255, 0, 0, 0.5))') : value;
+            return value === 'custom-color' ? window.prompt('Enter Color Code (#c0ffee or rgba(255, 0, 0, 0.5))') : value;
         }
-        
+
         this.editor = new Quill(`#${selector}`, {
             placeholder: input.placeholder,
             readOnly: input.readOnly,
@@ -28,13 +28,13 @@ export default class extends Controller {
                         ['link', 'image', 'video', 'clean'],
                     ],
                     handlers: {
-                        'color': function (value) {
+                        'color': (value) => {
                             this.quill.format('color', customColor(value));
                         },
-                        'background': function (value) {
+                        'background': (value) => {
                             this.quill.format('background', customColor(value));
                         },
-                    }
+                    },
                 },
             },
         });
