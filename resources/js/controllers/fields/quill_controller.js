d52a41a4c44be8b47f4451e78679c7bc44eb3054
mavsan <mavsan@yandex.ru>
2020-07-01 18:28:48 +1000

    Update quill_controller.js
    добавлены кнопки выбора цвета текста и фона в редакторе Quill

--- resources/js/controllers/fields/quill_controller.js
+++ resources/js/controllers/fields/quill_controller.js
@@ -8,18 +8,34 @@ export default class extends Controller {
     connect() {
         const selector = this.element.querySelector('.quill').id;
         const input = this.element.querySelector('input');
-
+        const colors = ['#000000', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', '#444444', '#5c0000', '#663d00', '#666600', '#003700', '#002966', '#3d1466', 'custom-color'];
+        
+        function customColor(value) {
+            return value == 'custom-color' ? window.prompt('Enter Color Code (#c0ffee or rgba(255, 0, 0, 0.5))') : value;
+        }
+        
         this.editor = new Quill(`#${selector}`, {
             placeholder: input.placeholder,
             readOnly: input.readOnly,
             theme: 'snow',
             modules: {
-                toolbar: [
-                    ['bold', 'italic', 'underline', 'strike'],
-                    [{ header: '1' }, { header: '2' }, 'blockquote', 'code-block'],
-                    [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }, { align: [] }],
-                    ['link', 'image', 'video', 'clean'],
-                ],
+                toolbar: {
+                    container: [
+                        ['bold', 'italic', 'underline', 'strike'],
+                        [{background: []}, {color: colors}],
+                        [{ header: '1' }, { header: '2' }, 'blockquote', 'code-block'],
+                        [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }, { align: [] }],
+                        ['link', 'image', 'video', 'clean'],
+                    ],
+                    handlers: {
+                        'color': function (value) {
+                            this.quill.format('color', customColor(value));
+                        },
+                        'background': function (value) {
+                            this.quill.format('background', customColor(value));
+                        },
+                    }
+                },
             },
         });
 
