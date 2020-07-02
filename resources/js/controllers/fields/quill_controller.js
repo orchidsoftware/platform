43766302c91d3f1f893b28b7b935a14e321e5dc5
Maksim Klimenko <mavsan@gmail.com>
2020-07-02 12:22:17 +1000

    codeclimate

--- resources/js/controllers/fields/quill_controller.js
+++ resources/js/controllers/fields/quill_controller.js
@@ -1,5 +1,17 @@
 import {Controller} from 'stimulus';
-import Quill        from 'quill';
+import Quill from 'quill';
+
+function customColor(value) {
+    return value === 'custom-color' ? window.prompt('Enter Color Code (#c0ffee or rgba(255, 0, 0, 0.5))') : value;
+}
+
+function setColor(value) {
+    this.quill.format('color', customColor(value));
+}
+
+function setBackground(value) {
+    this.quill.format('background', customColor(value));
+}
 
 export default class extends Controller {
     /**
@@ -9,10 +21,6 @@ export default class extends Controller {
         const selector = this.element.querySelector('.quill').id;
         const input = this.element.querySelector('input');
         const colors = ['#000000', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', '#444444', '#5c0000', '#663d00', '#666600', '#003700', '#002966', '#3d1466', 'custom-color'];
-
-        function customColor(value) {
-            return value === 'custom-color' ? window.prompt('Enter Color Code (#c0ffee or rgba(255, 0, 0, 0.5))') : value;
-        }
 
         this.editor = new Quill(`#${selector}`, {
             placeholder: input.placeholder,
@@ -28,12 +36,8 @@ export default class extends Controller {
                         ['link', 'image', 'video', 'clean'],
                     ],
                     handlers: {
-                        'color': (value) => {
-                            this.quill.format('color', customColor(value));
-                        },
-                        'background': (value) => {
-                            this.quill.format('background', customColor(value));
-                        },
+                        color: setColor,
+                        background: setBackground,
                     },
                 },
             },
