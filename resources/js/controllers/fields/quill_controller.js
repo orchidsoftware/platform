3ec76d4385e1f36edb40fdc17af27f2f99965e6a
Maksim Klimenko <mavsan@gmail.com>
2020-07-02 13:34:01 +1000

    codeclimate

--- resources/js/controllers/fields/quill_controller.js
+++ resources/js/controllers/fields/quill_controller.js
@@ -5,14 +5,6 @@ function customColor(value) {
     return value === 'custom-color' ? window.prompt('Enter Color Code (#c0ffee or rgba(255, 0, 0, 0.5))') : value;
 }
 
-function setColor(value) {
-    this.quill.format('color', customColor(value));
-}
-
-function setBackground(value) {
-    this.quill.format('background', customColor(value));
-}
-
 export default class extends Controller {
     /**
      *
@@ -20,7 +12,6 @@ export default class extends Controller {
     connect() {
         const selector = this.element.querySelector('.quill').id;
         const input = this.element.querySelector('input');
-        const colors = ['#000000', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', '#444444', '#5c0000', '#663d00', '#666600', '#003700', '#002966', '#3d1466', 'custom-color'];
 
         this.editor = new Quill(`#${selector}`, {
             placeholder: input.placeholder,
@@ -28,16 +19,10 @@ export default class extends Controller {
             theme: 'snow',
             modules: {
                 toolbar: {
-                    container: [
-                        ['bold', 'italic', 'underline', 'strike'],
-                        [{background: []}, {color: colors}],
-                        [{ header: '1' }, { header: '2' }, 'blockquote', 'code-block'],
-                        [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }, { align: [] }],
-                        ['link', 'image', 'video', 'clean'],
-                    ],
+                    container: this.containerToolbar(),
                     handlers: {
-                        color: setColor,
-                        background: setBackground,
+                        color: this.setColor,
+                        background: this.setBackground,
                     },
                 },
             },
@@ -56,6 +41,28 @@ export default class extends Controller {
         this.editor.on('text-change', () => {
             input.value = this.editor.getText() ? this.editor.root.innerHTML : '';
         });
+    }
+
+    colors() {
+        return ['#000000', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', '#444444', '#5c0000', '#663d00', '#666600', '#003700', '#002966', '#3d1466', 'custom-color'];
+    }
+
+    containerToolbar() {
+        return [
+            ['bold', 'italic', 'underline', 'strike'],
+            [{background: this.colors()}, {color: this.colors()}],
+            [{header: '1'}, {header: '2'}, 'blockquote', 'code-block'],
+            [{list: 'ordered'}, {list: 'bullet'}, {indent: '-1'}, {indent: '+1'}, {align: []}],
+            ['link', 'image', 'video', 'clean'],
+        ];
+    }
+
+    setColor(value) {
+        this.quill.format('color', customColor(value));
+    }
+
+    setBackground(value) {
+        this.quill.format('background', customColor(value));
     }
 
     /**
