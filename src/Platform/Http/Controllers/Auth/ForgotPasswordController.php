46ed791f3d38fdbeb744c5d59dfc2eea258df0f3
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2020-07-09 23:56:30 +0100

    Override `sendResetLinkEmail` method to return success message in all cases
    This is to further protect against username enumeration by not disclosing if the user exists in the database.

--- src/Platform/Http/Controllers/Auth/ForgotPasswordController.php
+++ src/Platform/Http/Controllers/Auth/ForgotPasswordController.php
@@ -6,6 +6,8 @@ namespace Orchid\Platform\Http\Controllers\Auth;
 
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 use Illuminate\View\View;
 use Orchid\Platform\Http\Controllers\Controller;
 
@@ -38,5 +40,25 @@ class ForgotPasswordController extends Controller
     public function showLinkRequestForm()
     {
         return view('platform::auth.passwords.email');
+    }
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function sendResetLinkEmail(Request $request)
+    {
+        $this->validateEmail($request);
+
+        // We will send the password reset link to this user.
+        // Success message will appear to the user in all cases
+        // to protect users from login brute force.
+        $this->broker()->sendResetLink(
+            $this->credentials($request)
+        );
+
+        return $this->sendResetLinkResponse($request, Password::PASSWORD_RESET);
     }
 }
