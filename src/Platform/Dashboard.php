54596e84ee96e52187e85b2d7c4744195200c5c1
Alexandr Chernyaev <bliz48rus@gmail.com>
2020-07-11 00:47:08 +0300

    7.13.0

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -17,7 +17,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '7.12.1';
+    public const VERSION = '7.13.0';
 
     /**
      * The Dashboard configuration options.
