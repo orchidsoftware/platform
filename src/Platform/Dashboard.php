12a500466c1178e9c2335160f62d3f9c29b2e884
tabuna <bliz48rus@gmail.com>
2020-08-19 15:42:48 +0300

    refs #1205 Added `total` method for Table

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -17,7 +17,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '7.17.4';
+    public const VERSION = '7.18.0';
 
     /**
      * The Dashboard configuration options.
