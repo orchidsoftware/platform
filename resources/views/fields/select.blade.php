675a1e262c71070f1368c231da8a7f5c571f31b7
tabuna <bliz48rus@gmail.com>
2020-09-14 14:19:32 +0300

    Use strict type casting in select

--- resources/views/fields/select.blade.php
+++ resources/views/fields/select.blade.php
@@ -4,9 +4,9 @@
             @foreach($options as $key => $option)
                 <option value="{{$key}}"
                         @isset($value)
-                        @if (is_array($value) && in_array($key, $value)) selected
+                        @if (is_array($value) && in_array($key, $value, true)) selected
                         @elseif (isset($value[$key]) && $value[$key] == $option) selected
-                        @elseif ($key == $value) selected
+                        @elseif ($key === $value) selected
                         @endif
                         @endisset
                 >{{$option}}</option>
