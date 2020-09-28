d13544b64b1afcaba5612f5eada80e6051c05dbf
tabuna <bliz48rus@gmail.com>
2020-09-28 22:45:12 +0300

    Remove Rubbers layout

--- src/Screen/LayoutFactory.php
+++ src/Screen/LayoutFactory.php
@@ -12,7 +12,6 @@ use Orchid\Screen\Layouts\Columns;
 use Orchid\Screen\Layouts\Component;
 use Orchid\Screen\Layouts\Modal;
 use Orchid\Screen\Layouts\Rows;
-use Orchid\Screen\Layouts\Rubbers;
 use Orchid\Screen\Layouts\Table;
 use Orchid\Screen\Layouts\Tabs;
 use Orchid\Screen\Layouts\View;
@@ -193,17 +192,6 @@ class LayoutFactory
     public static function accordion(array $layouts): Accordion
     {
         return new class($layouts) extends Accordion {
-        };
-    }
-
-    /**
-     * @param array $layouts
-     *
-     * @return Rubbers
-     */
-    public static function rubbers(array $layouts): Rubbers
-    {
-        return new class($layouts) extends Rubbers {
         };
     }
 }
