52b4eea0a3b3575f236013603231b242f033d010
Alexandr Chernyaev <bliz48rus@gmail.com>
2020-10-11 05:06:46 +0300

    Quotes break the Quill editor (#1295)
    * ## 9.4.1 - 2020-10-11

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -17,7 +17,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '9.4.0';
+    public const VERSION = '9.4.1';
 
     /**
      * The Dashboard configuration options.
