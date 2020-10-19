14b08ad460de9d077c6e3059923452c7f1bc4889
tabuna <bliz48rus@gmail.com>
2020-10-19 19:04:01 +0300

    Fixed scaling on iOS

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>
             @yield('title','ORCHID')
                 @hasSection('description') - @endif
