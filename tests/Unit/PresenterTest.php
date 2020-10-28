48880da5f842f417ad6b84fd114dd2b3843613a2
tabuna <bliz48rus@gmail.com>
2020-10-28 15:28:38 +0300

    Added test for call method as property

--- tests/Unit/PresenterTest.php
+++ tests/Unit/PresenterTest.php
@@ -14,6 +14,7 @@ class PresenterTest extends TestUnitCase
         $presenter = $this->getPresenterClass();
 
         $this->assertFalse($presenter->getStatus());
+        $this->assertFalse($presenter->getStatus);
     }
 
     public function testPresenterProperty(): void
