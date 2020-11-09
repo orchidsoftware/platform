e42b8f94714a5ed5ba599e2debb2ce6ed10d1b77
drabodan <id__88@mail.ru>
2020-11-10 00:04:48 +0300

    Added test cases

--- tests/Unit/Screen/TDForTableTest.php
+++ tests/Unit/Screen/TDForTableTest.php
@@ -32,4 +32,40 @@ class TDForTableTest extends TestUnitCase
 
         $this->assertStringNotContainsString('div style="width:"', $view);
     }
+
+    public function testTdWidthNumeric(): void
+    {
+        $integer = 100;
+
+        $view = TD::set('name')->width($integer)->buildTd(new Repository(['name' => 'value']));
+
+        $this->assertStringContainsString('<div style="width:'.$integer.'px"', $view);
+
+        $float = 100.51;
+
+        $view = TD::set('name')->width($float)->buildTd(new Repository(['name' => 'value']));
+
+        $this->assertStringContainsString('<div style="width:'.$float.'px"', $view);
+    }
+
+    public function testTdWidthString(): void
+    {
+        $stringWithInteger = '100';
+
+        $view = TD::set('name')->width($stringWithInteger)->buildTd(new Repository(['name' => 'value']));
+
+        $this->assertStringContainsString('<div style="width:'.$stringWithInteger.'px"', $view);
+
+        $stringWithFloat = '100.50';
+
+        $view = TD::set('name')->width($stringWithFloat)->buildTd(new Repository(['name' => 'value']));
+
+        $this->assertStringContainsString('<div style="width:'.$stringWithFloat.'px"', $view);
+
+        $stringWithNotOnlyNumeric = '100em';
+
+        $view = TD::set('name')->width($stringWithNotOnlyNumeric)->buildTd(new Repository(['name' => 'value']));
+
+        $this->assertStringContainsString('<div style="width:'.$stringWithNotOnlyNumeric.'"', $view);
+    }
 }
