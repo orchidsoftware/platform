aac6a130c8732a36b406ad51cc4eb1d7e31bafd0
Salador <Salador2005@yandex.ru>
2020-11-09 11:54:14 +0000

    Add option to test

--- tests/Environment.php
+++ tests/Environment.php
@@ -33,7 +33,7 @@ trait Environment
         /* Install application */
         $this->loadLaravelMigrations();
         $this->loadMigrationsFrom(realpath('./database/migrations'));
-        $this->artisan('orchid:install --examples');
+        $this->artisan('orchid:install', ['--examples']);
 
         /* Refresh application for route/breadcrumbs/orchid provider */
         if (! $this->app['router']->has('platform.main')) {
