f7ddaf96234526b9ea717073e9e411cd7a29b785
Drabodan <id__88@mail.ru>
2020-11-09 20:11:12 +0300

    Fixed rendering of attribute style in td view (#1347)
    * Fixed redundant rendering of attribute `style` with an empty value `width` when `width` is empty in the template `td`.

--- tests/Unit/Screen/TDForTableTest.php
+++ tests/Unit/Screen/TDForTableTest.php
@@ -2,6 +2,7 @@
 
 namespace Orchid\Tests\Unit\Screen;
 
+use Orchid\Screen\Repository;
 use Orchid\Screen\TD;
 use Orchid\Tests\TestUnitCase;
 
@@ -14,5 +15,21 @@ class TDForTableTest extends TestUnitCase
         $view = TD::set('name')->popover($popover)->buildTh();
 
         $this->assertStringContainsString($popover, $view);
+    }
+
+    public function testTdWidth(): void
+    {
+        $width = '100px';
+
+        $view = TD::set('name')->width($width)->buildTd(new Repository(['name' => 'value']));
+
+        $this->assertStringContainsString('<div style="width:'.$width.'"', $view);
+    }
+
+    public function testTdWithoutWidth(): void
+    {
+        $view = TD::set('name')->buildTd(new Repository(['name' => 'value']));
+
+        $this->assertStringNotContainsString('div style="width:"', $view);
     }
 }
