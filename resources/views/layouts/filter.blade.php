1c44eee169b386d656e998671ac8118721eec47c
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2020-12-06 17:46:02 +0100

    fixed filters style

--- resources/views/layouts/filter.blade.php
+++ resources/views/layouts/filter.blade.php
@@ -1,23 +1,21 @@
 @if($filters->count() > 0)
-    <div class="row no-gutters align-items-center bg-white pt-4 px-4 pb-1 rounded mb-3" data-controller="screen--filter">
-        @foreach($filters->where('display', true) as $filter)
-            <div class="col-sm-auto align-self-start">
-                {!! $filter->render() !!}
-            </div>
-        @endforeach
-        <div class="col-sm-auto ml-auto text-right">
-            <div class="form-group">
-                <div class="btn-group" role="group">
-                    <button
-                            data-action="screen--filter#clear"
-                            class="btn btn-default">
-                        <x-orchid-icon path="refresh"/>
-                    </button>
-                    <button type="submit"
-                            form="filters"
-                            class="btn btn-default">
-                        <x-orchid-icon path="filter"/>
-                    </button>
+    <div class="no-gutters bg-white pt-4 px-4 pb-1 rounded mb-3">
+        <div class="row align-items-center" data-controller="screen--filter">
+            @foreach($filters->where('display', true) as $filter)
+                <div class="col-sm-auto align-self-start">
+                    {!! $filter->render() !!}
+                </div>
+            @endforeach
+            <div class="col-sm-auto ml-auto text-right">
+                <div class="form-group">
+                    <div class="btn-group" role="group">
+                        <button data-action="screen--filter#clear" class="btn btn-default">
+                            <x-orchid-icon path="refresh" />
+                        </button>
+                        <button type="submit" form="filters" class="btn btn-default">
+                            <x-orchid-icon path="filter" />
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
