3c9bf8f84b3898c5cad1de72e715d218ef0ee236
Alexandr Chernyaev <tabuna@users.noreply.github.com>
2020-12-15 14:33:05 +0000

    Apply fixes from StyleCI

--- src/Platform/Models/User.php
+++ src/Platform/Models/User.php
@@ -57,7 +57,7 @@ class User extends Authenticatable implements UserInterface
      * @var array
      */
     protected $casts = [
-        'permissions' => 'array',
+        'permissions'       => 'array',
         'email_verified_at' => 'datetime',
     ];
 
@@ -98,9 +98,9 @@ class User extends Authenticatable implements UserInterface
         throw_if(static::where('email', $email)->exists(), Exception::class, 'User exist');
 
         static::create([
-            'name' => $name,
-            'email' => $email,
-            'password' => Hash::make($password),
+            'name'        => $name,
+            'email'       => $email,
+            'password'    => Hash::make($password),
             'permissions' => Dashboard::getAllowAllPermission(),
         ]);
     }
