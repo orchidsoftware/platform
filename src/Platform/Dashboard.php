29f234ebc7bcea7a2bf1438792bac0e0b517f81d
Alexandr Chernyaev <bliz48rus@gmail.com>
2020-12-23 18:36:32 +0300

    Fixed bug when trying to display only hidden filter (#1471)
    * Fixed bug when trying to display only hidden filter
    
    * ## 9.14.1 - 2020-12-23
    
    * Fix styling

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -17,7 +17,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '9.14.0';
+    public const VERSION = '9.14.1';
 
     /**
      * The Dashboard configuration options.
