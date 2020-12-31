d945c6db2cb43f457bcf935724803923f0eda71f
Paul Reynolds <paul.reynolds@hotmail.co.uk>
2020-12-31 19:28:30 +0000

    set datetime-static to true in DateTimer class

--- resources/js/controllers/fields/datetime_controller.js
+++ resources/js/controllers/fields/datetime_controller.js
@@ -15,7 +15,6 @@ export default class extends Flatpickr {
         this.config = {
             locale: document.documentElement.lang,
             plugins,
-            static: true,
         };
     }
 
