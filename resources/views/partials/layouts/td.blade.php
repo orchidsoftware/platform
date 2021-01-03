707e007030744e6af65c9e1b60c4ea54fa7ca591
tabuna <bliz48rus@gmail.com>
2021-01-03 20:06:28 +0300

    Fixed the ability to specify the width in percent for columns

--- resources/views/partials/layouts/td.blade.php
+++ resources/views/partials/layouts/td.blade.php
@@ -1,5 +1,8 @@
-<td class="text-{{$align}} @if(!$width) text-truncate @endif" data-column="{{ $slug }}" colspan="{{ $colspan }}">
-    <div @empty(!$width)style="width:{{ is_numeric($width) ? $width . 'px' : $width }}"@endempty>
+<td class="text-{{$align}} @if(!$width) text-truncate @endif"
+    data-column="{{ $slug }}" colspan="{{ $colspan }}"
+    @empty(!$width)style="width:{{ is_numeric($width) ? $width . 'px' : $width }}"@endempty
+>
+    <div>
         @isset($render)
             {!! $value !!}
         @else
