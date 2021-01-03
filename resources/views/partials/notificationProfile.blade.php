9860ae08254cd0103048f800cf6a0694b17fb0bf
tabuna <bliz48rus@gmail.com>
2021-01-03 19:32:47 +0300

    Fixed usage of notification refresh period

--- resources/views/partials/notificationProfile.blade.php
+++ resources/views/partials/notificationProfile.blade.php
@@ -5,7 +5,7 @@
        data-layouts--notification-count="{{ count($notifications) }}"
        data-layouts--notification-url="{{ route('platform.api.notifications') }}"
        data-layouts--notification-method="post"
-       data-layouts--notification-interval="60"
+       data-layouts--notification-interval="{{ config('platform.notifications.interval', 60) }}"
     >
         <x-orchid-icon path="bell"/>
 
