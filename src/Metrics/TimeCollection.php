0ce5ba14cc79c0af3b379463521f7ab90ab66fb5
tabuna <bliz48rus@gmail.com>
2021-01-15 02:18:35 +0300

    Added method for display the day of the week in charts

--- src/Metrics/TimeCollection.php
+++ src/Metrics/TimeCollection.php
@@ -2,7 +2,9 @@
 
 namespace Orchid\Metrics;
 
+use Carbon\Carbon;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 class TimeCollection extends Collection
 {
@@ -15,13 +17,28 @@ class TimeCollection extends Collection
     public function toChart(string $name, \Closure $closure = null): array
     {
         $closure = $closure ?? static function ($label) {
-            return $label;
-        };
+                return $label;
+            };
 
         return [
             'name'   => $name,
             'labels' => $this->pluck('label')->map($closure)->toArray(),
             'values' => $this->pluck('value')->toArray(),
         ];
+    }
+
+    /**
+     * @return TimeCollection
+     */
+    public function showDaysOfWeek(): TimeCollection
+    {
+        return $this->transform(function (array $value) {
+
+            $day = Carbon::parse($value['label'])->dayName;
+
+            $value['label'] = Str::ucfirst($day);
+
+            return $value;
+        });
     }
 }
