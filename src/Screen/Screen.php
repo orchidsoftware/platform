40253f8107bcd2c03832a5a9d4ca4138ef921d1d
lemys lopez <lemyskaman@gmail.com>
2021-03-19 08:59:18 -0400

    fixing the name of screen trest class and minorfix on scrren

--- src/Screen/Screen.php
+++ src/Screen/Screen.php
@@ -1,6 +1,6 @@
 <?php
 
-declare (strict_types = 1);
+declare (strict_types=1);
 
 namespace Orchid\Screen;
 
@@ -23,291 +23,301 @@ use Throwable;
 /**
  * Class Screen.
  */
-abstract class Screen extends Controller {
-	use Commander;
+abstract class Screen extends Controller
+{
+    use Commander;
 
-	/**
-	 * The number of predefined arguments in the route.
-	 *
-	 * Example: dashboard/my-screen/{method?}
-	 */
-	private const COUNT_ROUTE_VARIABLES = 1;
+    /**
+     * The number of predefined arguments in the route.
+     *
+     * Example: dashboard/my-screen/{method?}
+     */
+    private const COUNT_ROUTE_VARIABLES = 1;
 
-	/**
-	 * Display header name.
-	 *
-	 * @var string
-	 */
-	public $name;
+    /**
+     * Display header name.
+     *
+     * @var string
+     */
+    public $name;
 
-	/**
-	 * Display header description.
-	 *
-	 * @var string
-	 */
-	public $description;
+    /**
+     * Display header description.
+     *
+     * @var string
+     */
+    public $description;
 
-	/**
-	 * Permission.
-	 *
-	 * @var string|array
-	 */
-	public $permission;
+    /**
+     * Permission.
+     *
+     * @var string|array
+     */
+    public $permission;
 
-	/**
-	 * @var Repository
-	 */
-	private $source;
+    /**
+     * @var Repository
+     */
+    private $source;
 
-	/**
-	 * Button commands.
-	 *
-	 * @return Action[]
-	 */
-	abstract public function commandBar(): array;
+    /**
+     * Get can transfer to the screen only
+     * user-created methods available in it.
+     *
+     * @array
+     */
+    public static function getAvailableMethods(): array
+    {
+        return array_diff(
+            get_class_methods(static::class), // Custom methods
+            get_class_methods(self::class), // Basic methods
+            ['query']// Except methods
+        );
+    }
 
-	/**
-	 * Views.
-	 *
-	 * @return Layout[]
-	 */
-	abstract public function layout(): array;
+    /**
+     * Button commands.
+     *
+     * @return Action[]
+     */
+    abstract public function commandBar(): array;
 
-	/**
-	 * @throws Throwable
-	 *
-	 * @return View
-	 */
-	public function build() {
-		return LayoutFactory::blank([
-			$this->layout(),
-		])->build($this->source);
-	}
+    /**
+     * @param string $method
+     * @param string $slug
+     *
+     * @return View
+     * @throws Throwable
+     *
+     */
+    public function asyncBuild(string $method, string $slug)
+    {
+        Dashboard::setCurrentScreen($this);
 
-	/**
-	 * @param string $method
-	 * @param string $slug
-	 *
-	 * @throws Throwable
-	 *
-	 * @return View
-	 */
-	public function asyncBuild(string $method, string $slug) {
-		Dashboard::setCurrentScreen($this);
+        abort_unless(method_exists($this, $method), 404, "Async method: {$method} not found");
 
-		abort_unless(method_exists($this, $method), 404, "Async method: {$method} not found");
+        $query = $this->callMethod($method, request()->all());
+        $source = new Repository($query);
 
-		$query = $this->callMethod($method, request()->all());
-		$source = new Repository($query);
+        /** @var Layout $layout */
+        $layout = collect($this->layout())
+            ->map(function ($layout) {
+                return is_object($layout) ? $layout : resolve($layout);
+            })
+            ->map(function (Layout $layout) use ($slug) {
+                return $layout->findBySlug($slug);
+            })
+            ->filter()
+            ->whenEmpty(function () use ($slug) {
+                abort(404, "Async template: {$slug} not found");
+            })
+            ->first();
 
-		/** @var Layout $layout */
-		$layout = collect($this->layout())
-			->map(function ($layout) {
-				return is_object($layout) ? $layout : resolve($layout);
-			})
-			->map(function (Layout $layout) use ($slug) {
-				return $layout->findBySlug($slug);
-			})
-			->filter()
-			->whenEmpty(function () use ($slug) {
-				abort(404, "Async template: {$slug} not found");
-			})
-			->first();
+        return $layout->currentAsync()->build($source);
+    }
 
-		return $layout->currentAsync()->build($source);
-	}
+    /**
+     * @param string $method
+     * @param array $parameters
+     *
+     * @return mixed
+     * @throws ReflectionException
+     *
+     */
+    private function callMethod(string $method, array $parameters = [])
+    {
+        return call_user_func_array([$this, $method],
+            $this->reflectionParams($method, $parameters)
+        );
+    }
 
-	/**
-	 * @param array $httpQueryArguments
-	 *
-	 * @throws ReflectionException
-	 *
-	 * @return Factory|\Illuminate\View\View
-	 */
-	public function view(array $httpQueryArguments = []) {
-		$query = $this->callMethod('query', $httpQueryArguments);
-		$this->source = new Repository($query);
-		$commandBar = $this->buildCommandBar($this->source);
+    /**
+     * @param string $method
+     * @param array $httpQueryArguments
+     *
+     * @return array
+     * @throws ReflectionException
+     *
+     */
+    private function reflectionParams(string $method, array $httpQueryArguments = []): array
+    {
+        $class = new ReflectionClass($this);
 
-		return view('platform::layouts.base', [
-			'name' => $this->name,
-			'description' => $this->description,
-			'commandBar' => $commandBar,
-			'layouts' => $this->build(),
-			'formValidateMessage' => $this->formValidateMessage(),
-		]);
-	}
+        if (!is_string($method)) {
+            return [];
+        }
 
-	/**
-	 * @param mixed ...$parameters
-	 *
-	 * @throws Throwable
-	 * @throws ReflectionException
-	 *
-	 * @return Factory|View|\Illuminate\View\View|mixed
-	 */
-	public function handle(...$parameters) {
-		Dashboard::setCurrentScreen($this);
-		abort_unless($this->checkAccess(), 403);
+        if (!$class->hasMethod($method)) {
+            return [];
+        }
 
-		if (request()->isMethod('GET')) {
-			return $this->redirectOnGetMethodCallOrShowView($parameters);
-		}
+        $parameters = $class->getMethod($method)->getParameters();
 
-		$method = Route::current()->parameter('method', Arr::last($parameters));
+        return collect($parameters)
+            ->map(function ($parameter, $key) use ($httpQueryArguments) {
+                return $this->bind($key, $parameter, $httpQueryArguments);
+            })->all();
+    }
 
-		$parameters = array_diff(
-			$parameters,
-			[$method]
-		);
+    /**
+     * It takes the serial number of the argument and the required parameter.
+     * To convert to object.
+     *
+     * @param int $key
+     * @param ReflectionParameter $parameter
+     * @param array $httpQueryArguments
+     *
+     * @return mixed
+     * @throws BindingResolutionException
+     *
+     */
+    private function bind(int $key, ReflectionParameter $parameter, array $httpQueryArguments)
+    {
+        $class = $parameter->getType() && !$parameter->getType()->isBuiltin()
+            ? $parameter->getType()->getName()
+            : null;
 
-		$query = request()->query();
-		$query = !is_array($query) ? [] : $query;
+        $original = array_values($httpQueryArguments)[$key] ?? null;
 
-		$parameters = array_filter($parameters);
-		$parameters = array_merge($query, $parameters);
+        if ($class === null) {
+            return $original;
+        }
 
-		$response = $this->callMethod($method, $parameters);
+        if (is_object($original)) {
+            return $original;
+        }
 
-		return $response ?? back();
-	}
+        $object = resolve($class);
 
-	/**
-	 * @param string $method
-	 * @param array  $httpQueryArguments
-	 *
-	 * @throws ReflectionException
-	 *
-	 * @return array
-	 */
-	private function reflectionParams(string $method, array $httpQueryArguments = []): array
-	{
-		$class = new ReflectionClass($this);
+        if ($original === null || !is_a($object, UrlRoutable::class)) {
+            return $object;
+        }
 
-		if (!is_string($method)) {
-			return [];
-		}
+        return tap($object->resolveRouteBinding($original), function ($object) use ($parameter) {
+            Route::current()->setParameter($parameter->getName(), $object);
+        });
+    }
 
-		if (!$class->hasMethod($method)) {
-			return [];
-		}
+    /**
+     * Views.
+     *
+     * @return Layout[]
+     */
+    abstract public function layout(): array;
 
-		$parameters = $class->getMethod($method)->getParameters();
+    /**
+     * @param mixed ...$parameters
+     *
+     * @return Factory|View|\Illuminate\View\View|mixed
+     * @throws ReflectionException
+     *
+     * @throws Throwable
+     */
+    public function handle(...$parameters)
+    {
+        Dashboard::setCurrentScreen($this);
+        abort_unless($this->checkAccess(), 403);
 
-		return collect($parameters)
-			->map(function ($parameter, $key) use ($httpQueryArguments) {
-				return $this->bind($key, $parameter, $httpQueryArguments);
-			})->all();
-	}
+        if (request()->isMethod('GET')) {
+            return $this->redirectOnGetMethodCallOrShowView($parameters);
+        }
 
-	/**
-	 * It takes the serial number of the argument and the required parameter.
-	 * To convert to object.
-	 *
-	 * @param int                 $key
-	 * @param ReflectionParameter $parameter
-	 * @param array               $httpQueryArguments
-	 *
-	 * @throws BindingResolutionException
-	 *
-	 * @return mixed
-	 */
-	private function bind(int $key, ReflectionParameter $parameter, array $httpQueryArguments) {
-		$class = $parameter->getType() && !$parameter->getType()->isBuiltin()
-		? $parameter->getType()->getName()
-		: null;
+        $method = Route::current()->parameter('method', Arr::last($parameters));
 
-		$original = array_values($httpQueryArguments)[$key] ?? null;
+        $parameters = array_diff(
+            $parameters,
+            [$method]
+        );
 
-		if ($class === null) {
-			return $original;
-		}
+        $query = request()->query();
+        $query = !is_array($query) ? [] : $query;
 
-		if (is_object($original)) {
-			return $original;
-		}
+        $parameters = array_filter($parameters);
+        $parameters = array_merge($query, $parameters);
 
-		$object = resolve($class);
+        $response = $this->callMethod($method, $parameters);
 
-		if ($original === null || !is_a($object, UrlRoutable::class)) {
-			return $object;
-		}
+        return $response ?? back();
+    }
 
-		return tap($object->resolveRouteBinding($original), function ($object) use ($parameter) {
-			Route::current()->setParameter($parameter->getName(), $object);
-		});
-	}
+    /**
+     * @return bool
+     */
+    private function checkAccess(): bool
+    {
+        return collect($this->permission)
+            ->map(static function ($item) {
+                return Auth::user()->hasAccess($item);
+            })
+            ->whenEmpty(function (Collection $permission) {
+                return $permission->push(true);
+            })
+            ->contains(true);
+    }
 
-	/**
-	 * @return bool
-	 */
-	private function checkAccess(): bool {
-		return collect($this->permission)
-			->map(static function ($item) {
-				return Auth::user()->hasAccess($item);
-			})
-			->whenEmpty(function (Collection $permission) {
-				return $permission->push(true);
-			})
-			->contains(true);
-	}
+    /**
+     * Defines the URL to represent
+     * the page based on the calculation of link arguments.
+     *
+     * @param array $httpQueryArguments
+     *
+     * @return Factory|RedirectResponse|\Illuminate\View\View
+     * @throws ReflectionException
+     *
+     */
+    protected function redirectOnGetMethodCallOrShowView(array $httpQueryArguments)
+    {
+        $expectedArg = count(Route::current()->getCompiled()->getVariables()) - self::COUNT_ROUTE_VARIABLES;
+        $realArg = count($httpQueryArguments);
 
-	/**
-	 * @return string
-	 */
-	public function formValidateMessage(): string {
-		return __('Please check the entered data, it may be necessary to specify in other languages.');
-	}
+        if ($realArg <= $expectedArg) {
+            return $this->view($httpQueryArguments);
+        }
 
-	/**
-	 * Defines the URL to represent
-	 * the page based on the calculation of link arguments.
-	 *
-	 * @param array $httpQueryArguments
-	 *
-	 *@throws ReflectionException
-	 *
-	 * @return Factory|RedirectResponse|\Illuminate\View\View
-	 */
-	protected function redirectOnGetMethodCallOrShowView(array $httpQueryArguments) {
-		$expectedArg = count(Route::current()->getCompiled()->getVariables()) - self::COUNT_ROUTE_VARIABLES;
-		$realArg = count($httpQueryArguments);
+        array_pop($httpQueryArguments);
 
-		if ($realArg <= $expectedArg) {
-			return $this->view($httpQueryArguments);
-		}
+        return redirect()->action([static::class, 'handle'], $httpQueryArguments);
+    }
 
-		array_pop($httpQueryArguments);
+    /**
+     * @param array $httpQueryArguments
+     *
+     * @return Factory|\Illuminate\View\View
+     * @throws ReflectionException
+     *
+     */
+    public function view(array $httpQueryArguments = [])
+    {
+        $query = $this->callMethod('query', $httpQueryArguments);
+        $this->source = new Repository($query);
+        $commandBar = $this->buildCommandBar($this->source);
 
-		return redirect()->action([static::class, 'handle'], $httpQueryArguments);
-	}
+        return view('platform::layouts.base', [
+            'name' => $this->name,
+            'description' => $this->description,
+            'commandBar' => $commandBar,
+            'layouts' => $this->build(),
+            'formValidateMessage' => $this->formValidateMessage(),
+        ]);
+    }
 
-	/**
-	 * @param string $method
-	 * @param array  $parameters
-	 *
-	 * @throws ReflectionException
-	 *
-	 * @return mixed
-	 */
-	private function callMethod(string $method, array $parameters = []) {
-		return call_user_func_array([$this, $method],
-			$this->reflectionParams($method, $parameters)
-		);
-	}
+    /**
+     * @return View
+     * @throws Throwable
+     *
+     */
+    public function build()
+    {
+        return LayoutFactory::blank([
+            $this->layout(),
+        ])->build($this->source);
+    }
 
-	/**
-	 * Get can transfer to the screen only
-	 * user-created methods available in it.
-	 *
-	 * @array
-	 */
-	public static function getAvailableMethods(): array
-	{
-		return array_diff(
-			get_class_methods(static::class), // Custom methods
-			get_class_methods(self::class), // Basic methods
-			['query']// Except methods
-		);
-	}
+    /**
+     * @return string
+     */
+    public function formValidateMessage(): string
+    {
+        return __('Please check the entered data, it may be necessary to specify in other languages.');
+    }
 }
