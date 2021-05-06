3b606de192e4713a331a9d5a61485fc3ecf927b8
tabuna <bliz48rus@gmail.com>
2021-05-06 23:23:12 +0300

    update php cs fixer

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -14,18 +14,22 @@ $finder = Symfony\Component\Finder\Finder::create()
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sortAlgorithm' => 'alpha'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'no_unused_imports' => true,
         'no_whitespace_before_comma_in_array' => true,
         'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline_array' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
         'phpdoc_scalar' => true,
         'unary_operator_spaces' => true,
-        'binary_operator_spaces' => ['align_double_arrow' => true, 'align_equals' => false],
+        'binary_operator_spaces' => [
+            'operators' => [
+                '=>' => 'align_single_space_minimal',
+            ],
+        ],
         'blank_line_before_statement' => [
             'statements' => ['continue', 'declare', 'return', 'throw', 'try'],
         ],
@@ -34,15 +38,15 @@ return PhpCsFixer\Config::create()
         'phpdoc_order' => true,
         'phpdoc_single_line_var_spacing' => true,
         'phpdoc_var_without_name' => true,
-        'phpdoc_separation' => true,
         'class_attributes_separation' => [
             'elements' => [
-                'method',
+                'method'  => 'one',
             ],
         ],
         'method_argument_space' => [
-            'keep_multiple_spaces_after_comma' => true,
+            'on_multiline' => 'ignore',
         ],
         'trim_array_spaces' => true,
+        'single_trait_insert_per_statement' => false,
     ])
     ->setFinder($finder);
