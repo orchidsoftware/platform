eebe04b8f463a43ceafcdf98983ab8a2f850501a
tabuna <bliz48rus@gmail.com>
2021-05-26 14:49:13 +0300

    refs #1745 Added tests for fallback setting

---
+++ tests/Feature/Platform/FallbackDisabledTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\Feature\Platform;
+
+use Illuminate\Support\Str;
+use Orchid\Platform\Dashboard;
+use Orchid\Tests\TestFeatureCase;
+
+class FallbackDisabledTest extends TestFeatureCase
+{
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function getEnvironmentSetUp($app): void
+    {
+        parent::getEnvironmentSetUp($app);
+
+        config()->set('platform.fallback', false);
+    }
+
+    public function testRouteDisabled(): void
+    {
+        $response = $this
+            ->actingAs($this->createAdminUser())
+            ->get(Dashboard::prefix('/error-test/') . Str::random());
+
+        $response
+            ->assertDontSee('orchid.software')
+            ->assertDontSee("You requested a page that doesn't exist.");
+    }
+}
---
+++ tests/Feature/Platform/FallbackEnabledTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\Feature\Platform;
+
+use Illuminate\Support\Str;
+use Orchid\Platform\Dashboard;
+use Orchid\Tests\TestFeatureCase;
+
+class FallbackEnabledTest extends TestFeatureCase
+{
+    public function testRouteEnabled(): void
+    {
+        $response = $this
+            ->actingAs($this->createAdminUser())
+            ->get(Dashboard::prefix('/error-test/') . Str::random());
+
+        $response
+            ->assertSee('orchid.software')
+            ->assertSee("You requested a page that doesn't exist.");
+    }
+}
