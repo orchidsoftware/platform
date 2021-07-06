ec9d61af9c9cd2e835e4b9f4d23bb74e458278a7
tabuna <bliz48rus@gmail.com>
2021-07-06 20:06:02 +0300

    Added "Love" confirm

--- src/Platform/Commands/InstallCommand.php
+++ src/Platform/Commands/InstallCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Orchid\Platform\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\App;
 use Orchid\Platform\Dashboard;
 use Orchid\Platform\Events\InstallEvent;
 use Orchid\Platform\Providers\FoundationServiceProvider;
@@ -35,7 +36,7 @@ class InstallCommand extends Command
     public function handle(Dashboard $dashboard)
     {
         $this->info('Installation started. Please wait...');
-        $this->info('Version: '.Dashboard::VERSION);
+        $this->info('Version: ' . Dashboard::VERSION);
 
         $this
             ->executeCommand('vendor:publish', [
@@ -49,14 +50,12 @@ class InstallCommand extends Command
             ])
             ->executeCommand('migrate')
             ->executeCommand('storage:link')
-            ->changeUserModel();
+            ->changeUserModel()
+            ->setValueEnv('SCOUT_DRIVER')
+            ->showMeLove();
 
         $this->info('Completed!');
-
-        $this
-            ->setValueEnv('SCOUT_DRIVER')
-            ->comment("To create a user, run 'artisan orchid:admin'");
-
+        $this->comment("To create a user, run 'artisan orchid:admin'");
         $this->line("To start the embedded server, run 'artisan serve'");
 
         event(new InstallEvent($this));
@@ -86,21 +85,28 @@ class InstallCommand extends Command
         return $this;
     }
 
-    private function changeUserModel(string $path = 'Models/User.php')
+    /**
+     * @param string $path
+     *
+     * @return $this
+     */
+    private function changeUserModel(string $path = 'Models/User.php'):self
     {
         $this->info('Attempting to set ORCHID User model as parent to App\User');
 
-        if (! file_exists(app_path($path))) {
+        if (!file_exists(app_path($path))) {
             $this->warn('Unable to locate "app/Models/User.php".  Did you move this file?');
             $this->warn('You will need to update this manually.');
             $this->warn('Change "extends Authenticatable" to "extends \Orchid\Platform\Models\User" in your User model');
             $this->warn('Also pay attention to the properties so that they are not overwritten.');
 
-            return;
+            return $this;
         }
 
         $user = file_get_contents(Dashboard::path('stubs/app/User.stub'));
         file_put_contents(app_path($path), $user);
+
+        return $this;
     }
 
     /**
@@ -114,7 +120,7 @@ class InstallCommand extends Command
         $str = $this->fileGetContent(app_path('../.env'));
 
         if ($str !== false && strpos($str, $constant) === false) {
-            file_put_contents(app_path('../.env'), $str.PHP_EOL.$constant.'='.$value.PHP_EOL);
+            file_put_contents(app_path('../.env'), $str . PHP_EOL . $constant . '=' . $value . PHP_EOL);
         }
 
         return $this;
@@ -127,10 +133,41 @@ class InstallCommand extends Command
      */
     private function fileGetContent(string $file)
     {
-        if (! is_file($file)) {
+        if (!is_file($file)) {
             return '';
         }
 
         return file_get_contents($file);
     }
+
+    /**
+     * @return $this
+     */
+    private function showMeLove(): self
+    {
+        if (App::runningUnitTests() || !$this->confirm('Would you like to show a little love by starting with ⭐ repo?')) {
+            return $this;
+        }
+
+        $repo = 'https://github.com/orchidsoftware/platform';
+
+        switch (PHP_OS_FAMILY) {
+            case 'Darwin':
+                exec('open ' . $repo);
+                break;
+            case 'Windows':
+                exec('start ' . $repo);
+                break;
+            case 'Linux':
+                exec('xdg-open ' . $repo);
+                break;
+            default:
+                $this->line("You can find us at " . $repo);
+        }
+
+        $this->line("Thank you! It means a lot to us!");
+
+        return $this;
+    }
+
 }
