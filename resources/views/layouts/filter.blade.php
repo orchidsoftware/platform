7dbe457aa82e29ae0d4ee7b40e7d0d6bdb1e0f0b
Grigoriy Ivanov <grigoriy.ivanov.on@gmail.com>
2021-10-13 13:49:17 +0300

    Update filter.blade.php

--- resources/views/layouts/filter.blade.php
+++ resources/views/layouts/filter.blade.php
@@ -1,19 +1,19 @@
 @if($filters->count() > 0)
-    <div class="g-0 bg-white py-4 px-4 rounded mb-3">
-        <div class="row align-items-center" data-controller="filter">
+    <div class="g-0 bg-white rounded mb-3">
+        <div class="row align-items-center p-4" data-controller="filter">
             @foreach($filters->where('display', true) as $filter)
-                <div class="col-sm-auto align-self-start">
+                <div class="col-sm-auto form-group align-self-start">
                     {!! $filter->render() !!}
                 </div>
             @endforeach
-            <div class="col-sm-auto ms-auto mt-3 text-end">
+            <div class="col-sm-auto ms-auto text-end">
                 <div class="form-group">
                     <div class="btn-group" role="group">
                         <button data-action="filter#clear" class="btn btn-default">
-                            <x-orchid-icon class="me-1" path="refresh" /> {{ __('Refresh') }}
+                            <x-orchid-icon class="me-1" path="refresh"/> {{ __('Reset') }}
                         </button>
                         <button type="submit" form="filters" class="btn btn-default">
-                            <x-orchid-icon class="me-1" path="filter" /> {{ __('Apply') }}
+                            <x-orchid-icon class="me-1" path="filter"/> {{ __('Apply') }}
                         </button>
                     </div>
                 </div>
