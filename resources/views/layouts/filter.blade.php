2d1bbfcf8d57e6e489c2ecc386092b4891145143
Grigoriy Ivanov <grigoriy.ivanov.on@gmail.com>
2021-10-14 22:57:54 +0300

    Improve TEMPLATE_LINE filter (#1911)
    * Improve TEMPLATE_LINE filter
    
    Added margin for button group. 
    Added text for buttons for better  UX
    
    * Update filter.blade.php

--- resources/views/layouts/filter.blade.php
+++ resources/views/layouts/filter.blade.php
@@ -1,8 +1,8 @@
 @if($filters->count() > 0)
-    <div class="g-0 bg-white py-4 px-4 rounded mb-3">
-        <div class="row align-items-center" data-controller="filter">
+    <div class="g-0 bg-white rounded mb-3">
+        <div class="row align-items-center p-4" data-controller="filter">
             @foreach($filters->where('display', true) as $filter)
-                <div class="col-sm-auto align-self-start">
+                <div class="col-sm-auto form-group align-self-start">
                     {!! $filter->render() !!}
                 </div>
             @endforeach
@@ -10,10 +10,10 @@
                 <div class="form-group">
                     <div class="btn-group" role="group">
                         <button data-action="filter#clear" class="btn btn-default">
-                            <x-orchid-icon path="refresh" />
+                            <x-orchid-icon class="me-1" path="refresh"/> {{ __('Reset') }}
                         </button>
                         <button type="submit" form="filters" class="btn btn-default">
-                            <x-orchid-icon path="filter" />
+                            <x-orchid-icon class="me-1" path="filter"/> {{ __('Apply') }}
                         </button>
                     </div>
                 </div>
