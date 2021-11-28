a54b9063b8a6fc3d0c7867a3f3505af53c2d44b2
tabuna <bliz48rus@gmail.com>
2021-11-28 17:36:54 +0300

    Added testing helpers

---
+++ tests/App/Screens/BaseScreenTesting.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\App\Screens;
+
+use Illuminate\Http\Request;
+use Orchid\Platform\Models\User;
+use Orchid\Screen\Action;
+use Orchid\Screen\Fields\Input;
+use Orchid\Screen\Screen;
+use Orchid\Support\Facades\Layout;
+use Orchid\Support\Facades\Toast;
+use Throwable;
+
+class BaseScreenTesting extends Screen
+{
+    /**
+     * Display header name.
+     *
+     * @var string
+     */
+    public $name = 'Base Screen Test';
+
+    /**
+     * Query data.
+     *
+     * @param  User|null  $user
+     *
+     * @return array
+     */
+    public function query(User $user = null): array
+    {
+        return [
+            'user' => $user
+        ];
+    }
+
+    /**
+     * Button commands.
+     *
+     * @return Action[]
+     */
+    public function commandBar(): array
+    {
+        return [];
+    }
+
+    /**
+     * Views.
+     *
+     * @throws Throwable
+     *
+     * @return Layout[]
+     */
+    public function layout(): array
+    {
+        return [
+            Layout::rows([
+                Input::make('user.name')->title('Name'),
+                Input::make('user.email')->title('Email')
+            ])
+        ];
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function showToast(Request $request): void
+    {
+        Toast::warning($request->get('toast', 'Hello, world! This is a toast message.'));
+    }
+}
