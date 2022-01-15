7d8191cbbedeac9b22cbfcc8bf38646f1aeac8b5
tabuna <bliz48rus@gmail.com>
2022-01-16 01:36:39 +0300

    Revert

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -16,7 +16,7 @@ $finder = Symfony\Component\Finder\Finder::create()
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@PSR12'                              => true,
+        '@PSR2'                               => true,
         'array_syntax'                        => ['syntax' => 'short'],
         'ordered_imports'                     => ['sort_algorithm' => 'alpha'],
         'no_unused_imports'                   => true,
