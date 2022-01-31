ad4e65e1458784e4711cee4792c507e203c08e29
lintaba <lintaba@gmail.com>
2022-01-31 10:24:00 +0100

    numberange names should follow the other conventions

--- resources/views/fields/numberRange.blade.php
+++ resources/views/fields/numberRange.blade.php
@@ -3,9 +3,9 @@
         <div class="col-md-6 pe-1">
             <div class="form-group">
                 <input type="text"
-                       name="{{ $attributes['name'] }}[start]"
-                       id='start_{{ \Illuminate\Support\Str::slug($attributes['name']) }}'
-                       value="{{ $value['start'] ?? null }}"
+                       name="{{ $attributes['name'] }}[min]"
+                       id='min_{{ \Illuminate\Support\Str::slug($attributes['name']) }}'
+                       value="{{ $value['min'] ?? null }}"
                        {{ $attributes }}
                        >
             </div>
@@ -14,9 +14,9 @@
         <div class="col-md-6 ps-1">
             <div class="form-group">
                 <input type="text"
-                       name="{{ $attributes['name'] }}[end]"
-                       id='end_{{ \Illuminate\Support\Str::slug($attributes['name']) }}'
-                       value="{{ $value['end'] ?? null }}"
+                       name="{{ $attributes['name'] }}[max]"
+                       id='max_{{ \Illuminate\Support\Str::slug($attributes['name']) }}'
+                       value="{{ $value['max'] ?? null }}"
                        {{ $attributes }}
                        >
             </div>
