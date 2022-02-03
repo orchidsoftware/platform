0713b6d99d621e82de4ec3cb49ff3a9a316ae8da
Drabodan <id__88@mail.ru>
2022-02-03 16:12:56 +0300

    fix: profile `view` with a long name / role

--- resources/views/partials/profile.blade.php
+++ resources/views/partials/profile.blade.php
@@ -1,13 +1,13 @@
 <div class="d-flex m-0 align-items-center p-3">
-    <div class="dropdown w-75">
+    <div class="dropdown w-100">
         <a href="#" class="nav-link p-0 d-flex align-items-center" data-bs-toggle="dropdown">
             @if($image = Auth::user()->presenter()->image())
                 <span class="thumb-sm avatar me-3">
                         <img src="{{$image}}" class="b">
                 </span>
             @endif
-            <span class="d-block small">
-                <span class="text-ellipsis text-white" style="max-width: 12em;">{{Auth::user()->presenter()->title()}}</span>
+            <span class="d-block small w-75">
+                <span class="text-ellipsis text-white">{{Auth::user()->presenter()->title()}}</span>
                 <span class="text-muted d-block text-ellipsis">{{Auth::user()->presenter()->subTitle()}}</span>
             </span>
         </a>
