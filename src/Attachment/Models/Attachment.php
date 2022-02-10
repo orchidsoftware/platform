74b8634b7bc0ac25fd45142e0043ceab76081941
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2022-02-10 10:28:04 +0100

    laravell 9 support

--- src/Attachment/Models/Attachment.php
+++ src/Attachment/Models/Attachment.php
@@ -101,7 +101,7 @@ class Attachment extends Model
         $disk = Storage::disk($this->getAttribute('disk'));
         $path = $this->physicalPath();
 
-        return $disk->exists($path) && $path !== null
+        return $disk->exists($path ?? '') && $path !== null
             ? $disk->url($path)
             : $default;
     }
