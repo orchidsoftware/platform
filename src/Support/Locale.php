43d285f44af14b28ac66667aecd0137c21406cd2
Ehsan Sabet <ehsan.sabet@hotmail.com>
2022-03-01 12:39:05 +0330

    Feature/issue 1397 (#2155)
    * Added rtlcss dev dependency
    
    * webpack generate rtl css
    
    * view direction config
    
    * support rtl direction
    
    * rtl support
    
    * fix some issue
    
    * remove extra config
    
    * add function currentDir
    
    * fix call app class
    
    * add locale param to currentDir

---
+++ src/Support/Locale.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Support;
+
+class Locale
+{
+    /*
+     * Source: https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_code
+     */
+    private const RTL = [
+        'ar', //Arabic
+        'arc', //Aramaic
+        'ckb', //Kurdish
+        'dv', //Divehi
+        'fa', //Persian
+        'ha', //Hausa
+        'he', //Hebrew
+        'khw', //Khowar
+        'ks', //Kashmiri
+        'ps', //Pashto
+        'sd', //Sindhi
+        'ur', //Urdu
+        'uz_AF', //Uzbeki Afghanistan
+        'yi', //Yiddish
+    ];
+
+    public static function currentDir(string $locale): string
+    {
+        return in_array($locale, self::RTL) ? "rtl" : "ltr";
+    }
+}
