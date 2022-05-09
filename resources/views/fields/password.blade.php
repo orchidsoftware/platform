7a7589cecdf11a7b086c04e1a020d0572054a666
Swara <swara09@gmail.com>
2022-05-09 15:11:23 +0200

    Update password.blade.php (#2230)

--- resources/views/fields/password.blade.php
+++ resources/views/fields/password.blade.php
@@ -3,14 +3,14 @@
     <div data-controller="password"
          class="input-icon"
     >
-        <input {{ $attributes }} data-target="password.password">
+        <input {{ $attributes }} data-password-target="password">
         <div class="input-icon-addon cursor" data-action="click->password#change">
 
-            <span data-target="password.iconShow">
+            <span data-password-target="iconShow">
                 <x-orchid-icon path="eye" class="small me-2"/>
             </span>
 
-            <span data-target="password.iconLock" class="none">
+            <span data-password-target="iconLock" class="none">
                 <x-orchid-icon path="lock" class="small me-2"/>
             </span>
         </div>
