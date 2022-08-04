1b8e6fb365c44f9fee2b6831a97ab918fd7f6bdd
Alexandr Chernyaev <bliz48rus@gmail.com>
2022-08-04 19:08:10 +0300

    Update metric.blade.php

--- resources/views/layouts/metric.blade.php
+++ resources/views/layouts/metric.blade.php
@@ -7,7 +7,7 @@
     <div class="row mb-2 g-3 g-mb-4">
         @foreach($metrics as $key => $metric)
             <div class="col">
-                <div class="p-4 bg-white rounded shadow-sm h-100 d-flex flex-column ">
+                <div class="p-4 bg-white rounded shadow-sm h-100 d-flex flex-column">
                     <small class="text-muted d-block mb-1">{{ __($key) }}</small>
                     <p class="h3 text-black fw-light mt-auto">
                         {{ is_array($metric) ? $metric['value'] : $metric }}
