81c0d4e7bde102eeee9c902048356c474062f55c
Nadezhda <angelforevor@mail.ru>
2022-08-04 19:07:43 +0300

    Change metrics view (#2327)

--- resources/views/layouts/metric.blade.php
+++ resources/views/layouts/metric.blade.php
@@ -7,9 +7,9 @@
     <div class="row mb-2 g-3 g-mb-4">
         @foreach($metrics as $key => $metric)
             <div class="col">
-                <div class="p-4 bg-white rounded shadow-sm h-100">
+                <div class="p-4 bg-white rounded shadow-sm h-100 d-flex flex-column ">
                     <small class="text-muted d-block mb-1">{{ __($key) }}</small>
-                    <p class="h3 text-black fw-light">
+                    <p class="h3 text-black fw-light mt-auto">
                         {{ is_array($metric) ? $metric['value'] : $metric }}
 
                         @if(isset($metric['diff']) && (float)$metric['diff'] !== 0.0)
