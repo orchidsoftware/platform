4749bbf26d3412b0e2bcb7182a2d6647f48a8c38
tabuna <bliz48rus@gmail.com>
2022-09-11 03:22:54 +0300

    refs #2362 Removed authorize method for relation request

--- src/Platform/Http/Requests/RelationRequest.php
+++ src/Platform/Http/Requests/RelationRequest.php
@@ -9,16 +9,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class RelationRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return $this->user()->hasAccess('platform.index');
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
