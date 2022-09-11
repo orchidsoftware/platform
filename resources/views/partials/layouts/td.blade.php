30e9ba60de5df072bd270665e0e74e1f4af05315
tabuna <bliz48rus@gmail.com>
2022-09-11 03:56:18 +0300

    Improved avatar and column display for small spaces

--- resources/views/partials/layouts/td.blade.php
+++ resources/views/partials/layouts/td.blade.php
@@ -1,6 +1,6 @@
 <td class="text-{{$align}} @if(!$width) text-truncate @endif"
     data-column="{{ $slug }}" colspan="{{ $colspan }}"
-    @empty(!$width)style="width:{{ is_numeric($width) ? $width . 'px' : $width }}"@endempty
+    @empty(!$width)style="min-width:{{ is_numeric($width) ? $width . 'px' : $width }};"@endempty
 >
     <div>
         @isset($render)
