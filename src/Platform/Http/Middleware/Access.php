9acc1d3ffc22d27417fa059d4eddc8006dc9dc5a
Takhir Latypov <silver2prod@gmail.com>
2022-09-18 23:05:35 +0300

    Added unaccessed method to Screen (#2374)
    Developer can override default http response when access to screen is denied

--- src/Platform/Http/Middleware/Access.php
+++ src/Platform/Http/Middleware/Access.php
@@ -13,6 +13,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
+use Orchid\Screen\Screen;
 
 /**
  * Class Access.
@@ -56,7 +57,7 @@ class Access
 
         // The current user is already signed in.
         // It means that he does not have the privileges to view.
-        abort(403);
+        abort(Screen::unaccessed());
     }
 
     /**
