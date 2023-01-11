43cb521019765c383338f975aa85c730e2bc76f9
tabuna <bliz48rus@gmail.com>
2023-01-11 09:50:36 +0300

    refs #2339 Added tests for route binding

--- tests/Feature/App/RouteResolveScreenTest.php
+++ tests/Feature/App/RouteResolveScreenTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Orchid\Tests\Feature\App;
 
 use Illuminate\Support\Facades\Route;
+use Orchid\Platform\Models\User;
+use Orchid\Tests\App\Screens\ModelRouteBindScreen;
 use Orchid\Tests\App\Screens\RouteResolveScreen;
 use Orchid\Tests\TestFeatureCase;
 
@@ -21,5 +23,95 @@ class RouteResolveScreenTest extends TestFeatureCase
             ->assertOk()
             ->assertDontSee('test')
             ->assertSee('Hello Word');
+    }
+
+    public function testImplicitBinding(): void
+    {
+        Route::screen('bind/users/{user}', ModelRouteBindScreen::class)
+            ->middleware(config('platform.middleware.private'))
+            ->name('bind.implicit-binding');
+
+        $user = $this->createAdminUser();
+
+        $this
+            ->actingAs($user)
+            ->get(route('bind.implicit-binding', $user->id))
+            ->assertOk()
+            ->assertSee($user->id)
+            ->assertSee($user->email);
+    }
+
+    public function testImplicitBindingWhenAllowNull(): void
+    {
+        Route::screen('bind/users/{user}', ModelRouteBindScreen::class)
+            ->middleware(config('platform.middleware.private'))
+            ->name('bind.implicit-binding');
+
+        $user = $this->createAdminUser();
+
+        $this
+            ->actingAs($user)
+            ->get(route('bind.implicit-binding', 0))
+            ->assertOk()
+            ->assertSee('User ID')
+            ->assertSee('User Name');
+    }
+
+    /*
+     * TODO: Support
+     *
+    public function testCustomizingKey():void
+    {
+        Route::screen('bind/users/{user:email}', ModelRouteBindScreen::class)
+            ->middleware(config('platform.middleware.private'))
+            ->name('bind.customizing-key');
+
+        $user = $this->createAdminUser();
+
+        $this
+            ->actingAs($user)
+            ->get(route('bind.customizing-key', $user->email))
+            ->assertOk()
+            ->assertSee($user->id)
+            ->assertSee($user->email);
+    }
+    */
+
+    public function testExplicitBinding(): void
+    {
+        Route::model('model', User::class);
+
+        Route::screen('bind/users/{model}', ModelRouteBindScreen::class)
+            ->middleware(config('platform.middleware.private'))
+            ->name('bind.explicit-binding');
+
+        $user = $this->createAdminUser();
+
+        $this
+            ->actingAs($user)
+            ->get(route('bind.explicit-binding', $user->id))
+            ->assertOk()
+            ->assertSee($user->id)
+            ->assertSee($user->email);
+    }
+
+    public function testResolutionLogic(): void
+    {
+        Route::bind('bind', function ($value) {
+            return User::where('email', $value)->firstOrFail();
+        });
+
+        Route::screen('bind/users/{bind}', ModelRouteBindScreen::class)
+            ->middleware(config('platform.middleware.private'))
+            ->name('bind.resolution-logic');
+
+        $user = $this->createAdminUser();
+
+        $this
+            ->actingAs($user)
+            ->get(route('bind.resolution-logic', $user->email))
+            ->assertOk()
+            ->assertSee($user->id)
+            ->assertSee($user->email);
     }
 }
