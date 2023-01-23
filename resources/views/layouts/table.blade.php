b48b893a02f8c8a192f921c674ec4a92abca0dba
tabuna <bliz48rus@gmail.com>
2023-01-24 01:08:42 +0300

    Fixed show icon for table Changed message for empty/nofound data Changed don't show headers for one column

--- resources/views/layouts/table.blade.php
+++ resources/views/layouts/table.blade.php
@@ -21,13 +21,17 @@
                     'table-bordered' => $bordered,
                     'table-hover'    => $hoverable,
                ])>
-            <thead>
-                <tr>
-                    @foreach($columns as $column)
-                        {!! $column->buildTh() !!}
-                    @endforeach
-                </tr>
-            </thead>
+
+            @if($showHeader)
+                <thead>
+                    <tr>
+                        @foreach($columns as $column)
+                            {!! $column->buildTh() !!}
+                        @endforeach
+                    </tr>
+                </thead>
+            @endif
+
             <tbody>
 
             @foreach($rows as $source)
@@ -51,16 +55,21 @@
     </div>
 
     @if($rows->isEmpty())
-        <div class="text-center py-5 w-100">
-            <h3 class="fw-light">
-                @isset($iconNotFound)
-                    <x-orchid-icon :path="$iconNotFound" class="block m-b"/>
-                @endisset
+        <div class="d-md-flex align-items-center px-md-0 px-2 pt-4 pb-5 w-100 text-md-start text-center">
 
-                {!!  $textNotFound !!}
-            </h3>
+            @isset($iconNotFound)
+                <div class="col-auto mx-md-4 mb-3 mb-md-0">
+                    <x-orchid-icon :path="$iconNotFound" class="block h1"/>
+                </div>
+            @endisset
 
-            {!! $subNotFound !!}
+            <div>
+                <h3 class="fw-light">
+                    {!!  $textNotFound !!}
+                </h3>
+
+                 {!! $subNotFound !!}
+            </div>
         </div>
     @else
 
--- src/Screen/Layouts/Table.php
+++ src/Screen/Layouts/Table.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Orchid\Screen\Layouts;
 
 use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Support\Collection;
 use Orchid\Screen\Layout;
 use Orchid\Screen\Repository;
 use Orchid\Screen\TD;
@@ -77,6 +79,7 @@ abstract class Table extends Layout
             'hoverable'    => $this->hoverable(),
             'slug'         => $this->getSlug(),
             'onEachSide'   => $this->onEachSide(),
+            'showHeader'   => $this->hasHeader($columns, $rows),
             'title'        => $this->title,
         ]);
     }
@@ -98,7 +101,7 @@ abstract class Table extends Layout
      */
     protected function iconNotFound(): string
     {
-        return 'icon-table';
+        return 'table';
     }
 
     /**
@@ -106,7 +109,11 @@ abstract class Table extends Layout
      */
     protected function textNotFound(): string
     {
-        return __('There are no records in this view');
+        if (count(request()->query()) !== 0) {
+            return __('No results found for your current filters');
+        }
+
+        return __('There are no objects currently displayed');
     }
 
     /**
@@ -114,7 +121,11 @@ abstract class Table extends Layout
      */
     protected function subNotFound(): string
     {
-        return '';
+        if (count(request()->query()) !== 0) {
+            return __('Try adjusting your filter settings or removing it altogether to see more data');
+        }
+
+        return 'Import or create objects, or check back later for updates';
     }
 
     /**
@@ -165,6 +176,21 @@ abstract class Table extends Layout
     protected function onEachSide(): int
     {
         return 3;
+    }
+
+    /**
+     * @param \Illuminate\Support\Collection                                  $columns
+     * @param \Illuminate\Support\Collection|\Illuminate\Pagination\Paginator $row
+     *
+     * @return bool
+     */
+    protected function hasHeader(Collection $columns, Collection|Paginator $row): bool
+    {
+        if ($columns->count() < 2) {
+            return false;
+        }
+
+        return !empty(request()->query()) || $row->isNotEmpty();
     }
 
     /**
