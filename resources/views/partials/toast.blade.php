69dc56b7a21f0ca02def3bc6cec3cacdc031e7b3
tabuna <bliz48rus@gmail.com>
2023-02-05 19:54:41 +0300

    Improve toast

--- resources/views/partials/toast.blade.php
+++ resources/views/partials/toast.blade.php
@@ -1,12 +1,12 @@
 <div class="toast-wrapper" data-controller="toast">
     <template id="toast">
-        <div class="toast"
+        <div class="toast rounded shadow-sm bg-white mb-3"
              role="alert"
              aria-live="assertive"
              aria-atomic="true"
              data-bs-delay="5000"
              data-bs-autohide="true">
-            <div class="toast-body p-3 bg-white rounded shadow-sm d-flex">
+            <div class="toast-body p-3 d-flex">
                 <p class="mb-0">
                     <span class="text-{type}">
                         <x-orchid-icon path="bs.circle-fill" class="me-2"/>
@@ -20,10 +20,10 @@
 
 
     @if (session()->has(\Orchid\Alert\Toast::SESSION_MESSAGE))
-        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true"
+        <div class="toast rounded shadow-sm bg-white mb-3" role="alert" aria-live="assertive" aria-atomic="true"
              data-bs-delay="{{ session(\Orchid\Alert\Toast::SESSION_DELAY) }}"
              data-bs-autohide="{{ session(\Orchid\Alert\Toast::SESSION_AUTO_HIDE) }}">
-            <div class="toast-body p-3 bg-white rounded shadow-sm d-flex">
+            <div class="toast-body p-3 d-flex">
                 <p class="mb-0 me-1">
                     <span class="text-{{ session(\Orchid\Alert\Toast::SESSION_LEVEL) }}">
                         <x-orchid-icon path="bs.circle-fill" class="me-2"/>
