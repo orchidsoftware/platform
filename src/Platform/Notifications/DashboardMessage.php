b4d4715efd3de89e3344db4c3f22c1bac7d3c9d2
Swara <swara09@gmail.com>
2023-02-21 12:29:19 +0100

    Fixed Bug: enums have no default serialization

--- src/Platform/Notifications/DashboardMessage.php
+++ src/Platform/Notifications/DashboardMessage.php
@@ -28,7 +28,7 @@ class DashboardMessage extends DatabaseMessage
         $this->data['time'] = Carbon::now();
 
         if (empty($this->data['type'])) {
-            $this->data['type'] = Color::INFO;
+            $this->data['type'] = Color::INFO->name;
         }
     }
 
