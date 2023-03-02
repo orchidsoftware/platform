65a5bcd58fb329a488d7a4e81a5709e91f4703df
George Repiev <33565251+ega22a@users.noreply.github.com>
2023-03-02 16:50:34 +0500

    Fixed an oversight.

--- src/Screen/Fields/Relation.php
+++ src/Screen/Fields/Relation.php
@@ -26,7 +26,7 @@ use Orchid\Support\Assert;
  * @method Relation placeholder(string $placeholder = null)
  * @method Relation popover(string $value = null)
  * @method Relation title(string $value = null)
- * @method Relation allowAdd($value = true)
+ * @method Relation allowAdd($value = false)
  */
 class Relation extends Field
 {
@@ -50,7 +50,7 @@ class Relation extends Field
         'relationSearchColumns' => null,
         'chunk'                 => 10,
         'allowEmpty'            => '',
-        'allowAdd'              => true,
+        'allowAdd'              => false,
     ];
 
     /**
