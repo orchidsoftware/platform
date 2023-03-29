94b426e4778c81f4ecf129bea94bb3eedc1db786
Nadezhda <angelforevor@mail.ru>
2023-03-29 03:11:14 +0300

    Test for issue 2517 (#2542)

---
+++ tests/App/Screens/ItemAddChildScreen.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\App\Screens;
+
+use Illuminate\Http\Request;
+use Orchid\Screen\Action;
+use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Fields\Input;
+use Orchid\Screen\Screen;
+use Orchid\Support\Facades\Layout;
+use Orchid\Support\Facades\Toast;
+
+class ItemAddChildScreen extends Screen
+{
+    public $parent_id;
+    public $item;
+
+    /**
+     * Fetch data to be displayed on the screen.
+     *
+     * @param int $parent_id
+     *
+     * @return array
+     */
+    public function query(int $parent_id): iterable
+    {
+        return [
+            'parent_id'       => $parent_id,
+        ];
+    }
+
+    /**
+     * The name of the screen displayed in the header.
+     *
+     * @return string|null
+     */
+    public function name(): ?string
+    {
+        return 'Add child';
+    }
+
+    /**
+     * Display header description.
+     *
+     * @return string|null
+     */
+    public function description(): ?string
+    {
+        return '';
+    }
+
+    /**
+     * The screen's action buttons.
+     *
+     * @return Action[]
+     */
+    public function commandBar(): iterable
+    {
+        return [
+
+            Button::make(__('Save'))
+                ->icon('check')
+                ->method('addChild'),
+        ];
+    }
+
+    /**
+     * @return \Orchid\Screen\Layout[]
+     */
+    public function layout(): iterable
+    {
+        return [
+
+            Layout::rows([
+                Input::make('item.name')
+                    ->title('Name')
+                    ->required()
+                    ->placeholder('Enter task name')
+                    ->help('The name of the task to be created.'),
+
+            ]),
+
+        ];
+    }
+
+    public function addChild(Request $request, int $parent_id)
+    {
+        // Validate form data, save task to database, etc.
+
+        $request->validate([
+            'item.name' => 'required|max:255',
+        ]);
+
+        Toast::info('Item with paretn_id='.$parent_id.' saved');
+
+        return redirect()->route('test.items');
+    }
+}
---
+++ tests/Feature/Issue2517Test.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\Feature\Platform;
+
+use Orchid\Tests\TestFeatureCase;
+
+class Issue2517Test extends TestFeatureCase
+{
+    public function testIssue2517(): void
+    {
+        $this
+            ->actingAs($this->createAdminUser())
+            ->get(route('test.items'))
+            ->assertSuccessful()
+            ->assertSee('/dashboard/items/create"', false);
+
+        $this->get(route('test.item.addchild', 1))
+            ->assertSuccessful();
+        $this->followingRedirects()
+            ->post(route('test.item.addchild', ['parentId' => 1, 'method' => 'addChild']), ['item' => ['name' => 'name 7']])
+            ->assertSuccessful() //странно, в живую там 302
+
+            ->assertSee('Item with paretn_id=1 saved')
+            ->assertSee('/dashboard/items/create"', false)
+            ->assertDontSee('/dashboard/item/1/addChild/create"', false)
+            ->assertDontSeeText('/dashboard/item/1/addChild/create"', false);
+    }
+}
