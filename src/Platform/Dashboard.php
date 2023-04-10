e6edf7c427bc3d9e1794fa880d2b641f86039b5d
tabuna <bliz48rus@gmail.com>
2023-04-10 20:15:36 +0300

    Build assets 14.0.0-alpha4

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -20,7 +20,7 @@ class Dashboard
     /**
      * ORCHID Version.
      */
-    public const VERSION = '14.0.0-alpha3';
+    public const VERSION = '14.0.0-alpha4';
 
     /**
      * Slug for main menu.
