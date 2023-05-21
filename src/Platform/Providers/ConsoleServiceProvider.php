60d776ebd56923b4ad3eb9437da56ab81e10168c
tabuna <bliz48rus@gmail.com>
2023-05-21 21:12:03 +0300

    refs #2611 Fixed tests

--- src/Platform/Providers/ConsoleServiceProvider.php
+++ src/Platform/Providers/ConsoleServiceProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Orchid\Platform\Providers;
@@ -41,7 +42,10 @@ class ConsoleServiceProvider extends ServiceProvider
         TabMenuCommand::class,
     ];
 
-    public function boot(): void
+    /**
+     * Register bindings the service provider.
+     */
+    public function register(): void
     {
         AboutCommand::add('Orchid Platform', fn() => [
             'Version' => Dashboard::version(),
