a2758a0517a7cbf3c7220d31db6e36d032b3e246
Grigoriy Ivanov <grigoriy.ivanov.on@gmail.com>
2023-05-23 20:58:22 +0300

    Added ability to override footer in full workspace template

--- resources/views/workspace/full.blade.php
+++ resources/views/workspace/full.blade.php
@@ -4,6 +4,8 @@
 
     <div class="mt-3 mt-md-4 workspace">
         @yield('workspace')
+        
+        @includeFirst([config('platform.template.footer'), 'platform::footer'])
     </div>
 
 @endsection
