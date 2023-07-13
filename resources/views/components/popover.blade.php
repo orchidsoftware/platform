65acc47e60515803c717e715a9ec2fd1bfe0c5ee
Drabodan <id__88@mail.ru>
2023-07-13 17:50:14 +0300

    fix: missing `bs` in popover data attributes

--- resources/views/components/popover.blade.php
+++ resources/views/components/popover.blade.php
@@ -2,13 +2,13 @@
      role="button"
      data-controller="popover"
      data-action="click->popover#trigger"
-     data-container="body"
-     data-toggle="popover"
+     data-bs-container="body"
+     data-bs-toggle="popover"
      tabindex="0"
-     data-trigger="focus"
-     data-placement="{{ $placement }}"
-     data-delay-show="300"
-     data-delay-hide="200"
+     data-bs-trigger="click"
+     data-bs-placement="{{ $placement }}"
+     data-bs-delay-show="300"
+     data-bs-delay-hide="200"
      data-bs-content="{{ $content }}">
     <x-orchid-icon path="bs.question-lg" width="1em" height="1em"/>
 </sup>
