aa3d1cceff07161e503ed53b9dbb07cbb1bde820
Grigoriy Ivanov <grigoriy.ivanov.on@gmail.com>
2023-10-16 16:04:40 +0300

    Update td.blade.php

--- resources/views/partials/layouts/td.blade.php
+++ resources/views/partials/layouts/td.blade.php
@@ -1,4 +1,4 @@
-<td class="text-{{$align}} @if(!$width) text-truncate @endif @if(!$class){{ $class }}@endif"
+<td class="text-{{$align}} @if(!$width) text-truncate @endif {{ $class }}"
     data-column="{{ $slug }}" colspan="{{ $colspan }}"
         @style([
         "min-width:$width;" => $width,
