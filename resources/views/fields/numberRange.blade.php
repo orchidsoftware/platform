d780d7feccaf85c1fc0a51ca48be524ff312f28c
Алексей Осипенко <68446532+XhonorSkillline@users.noreply.github.com>
2023-11-24 23:21:27 +0200

    Update numberRange.blade.php (#2750)

--- resources/views/fields/numberRange.blade.php
+++ resources/views/fields/numberRange.blade.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-md-6 pe-1">
             <div class="form-group">
-                <input type="text"
+                <input type="{{ $attributes['type'] }}"
                        name="{{ $attributes['name'] }}[min]"
                        id='min_{{ \Illuminate\Support\Str::slug($attributes['name']) }}'
                        value="{{ $value['min'] ?? null }}"
@@ -14,7 +14,7 @@
 
         <div class="col-md-6 ps-1">
             <div class="form-group">
-                <input type="text"
+                <input type="{{ $attributes['type'] }}"
                        name="{{ $attributes['name'] }}[max]"
                        id='max_{{ \Illuminate\Support\Str::slug($attributes['name']) }}'
                        value="{{ $value['max'] ?? null }}"
