d3c201d31002cc5c2aa04d16f0513c3c0ddb7acd
ShamazinGG <nikbelikita1@yandex.ru>
2024-04-12 01:01:52 +0300

    fix Select filter resets after apply others

--- resources/views/fields/select.blade.php
+++ resources/views/fields/select.blade.php
@@ -8,14 +8,11 @@
     >
         <select {{ $attributes }}>
             @foreach($options as $key => $option)
-                <option value="{{$key}}"
-                        @isset($value)
-                            @if (is_array($value) && in_array($key, $value)) selected
-                            @elseif (isset($value[$key]) && $value[$key] == $option) selected
-                            @elseif ($key == $value) selected
-                            @endif
-                        @endisset
-                >{{$option}}</option>
+                <option value="{{ $key }}"
+                        @if(isset($value) && ((is_array($value) && in_array($key, $value)) || $key == $value))
+                            selected
+                        @endif
+                >{{ $option }}</option>
             @endforeach
         </select>
     </div>
