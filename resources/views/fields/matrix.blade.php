15f275ff1494e4a8197fea0f7ec6641040f0c692
Vitalik <ercogx@gmail.com>
2024-04-21 02:59:19 +0300

    Added a possibility to change 'Add row' label for matrix field (#2824)
    * Added a possibility to change 'Add row' label for matrix field
    
    * Move translation 'Add row' label to be historically

--- resources/views/fields/matrix.blade.php
+++ resources/views/fields/matrix.blade.php
@@ -25,7 +25,7 @@
                 <a href="#" data-action="matrix#addRow" class="btn btn-block small text-muted">
                     <x-orchid-icon path="bs.plus-circle" class="me-2"/>
 
-                    <span>{{ __('Add row') }}</span>
+                    <span>{{ __($addRowLabel) }}</span>
                 </a>
             </th>
         </tr>
