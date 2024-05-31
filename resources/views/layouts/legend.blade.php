8e58f590e3411c239d45c44607eb82a5556b0955
tabuna <bliz48rus@gmail.com>
2024-06-01 01:46:27 +0300

    Improve fieldset template

--- resources/views/layouts/legend.blade.php
+++ resources/views/layouts/legend.blade.php
@@ -1,4 +1,4 @@
-<fieldset class="mb-3" data-async>
+<fieldset class="mb-3">
 
     @empty(!$title)
         <div class="col p-0 px-3">
@@ -8,18 +8,16 @@
         </div>
     @endempty
 
-    <div class="bg-white rounded shadow-sm p-4 py-4 d-flex flex-column">
-        <dl class="d-block m-0">
-            @foreach($columns as $column)
-                <div class="d2-grid py-3 {{ $loop->first ? '' : 'border-top' }}">
-                    <dt class="text-muted fw-normal me-3">
-                        {!! $column->buildDt($repository) !!}
-                    </dt>
-                    <dd class="text-black">
-                        {!! $column->buildDd($repository) !!}
-                    </dd>
-                </div>
-            @endforeach
-        </dl>
-    </div>
+    <dl class="bg-white rounded shadow-sm p-4 py-4 d-flex flex-column">
+        @foreach($columns as $column)
+            <div class="d2-grid py-3 {{ $loop->first ? '' : 'border-top' }}">
+                <dt class="text-muted fw-normal me-3">
+                    {!! $column->buildDt($repository) !!}
+                </dt>
+                <dd class="text-black">
+                    {!! $column->buildDd($repository) !!}
+                </dd>
+            </div>
+        @endforeach
+    </dl>
 </fieldset>
