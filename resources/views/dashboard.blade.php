357283ffad0868a7622b1ed3f6e4512ef4f24185
Alexandr Chernyaev <bliz48rus@gmail.com>
2024-08-13 23:58:16 +0300

    Refactor to eliminate technical debt by removing the deprecated MENU_MAIN constant

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,7 +20,7 @@
             @include('platform::partials.search')
 
             <ul class="nav flex-column mb-md-1 mb-auto ps-0">
-                {!! Dashboard::renderMenu(\Orchid\Platform\Dashboard::MENU_MAIN) !!}
+                {!! Dashboard::renderMenu() !!}
             </ul>
 
             <div class="h-100 w-100 position-relative to-top cursor d-none d-md-flex mt-md-5"
