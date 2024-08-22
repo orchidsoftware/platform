dfe37c3651288d34d69233f54a49d04df525d458
Alexandr Chernyaev <bliz48rus@gmail.com>
2024-08-23 01:03:55 +0300

    Fixed hidden drop-down on row table

--- resources/views/actions/dropdown.blade.php
+++ resources/views/actions/dropdown.blade.php
@@ -4,7 +4,6 @@
         type="button"
         data-bs-toggle="dropdown"
         aria-expanded="false"
-        data-bs-popper-config='{"strategy": "fixed"}'
     >
         @isset($icon)
             <x-orchid-icon :path="$icon" class="overflow-visible"/>
--- resources/views/layouts/table.blade.php
+++ resources/views/layouts/table.blade.php
@@ -8,7 +8,7 @@
     </fieldset>
 @endempty
 
-<div class="bg-white rounded shadow-sm mb-3"
+<div class="bg-white rounded shadow-sm mb-3 overflow-hidden"
      data-controller="table"
      data-table-slug="{{$slug}}"
 >
