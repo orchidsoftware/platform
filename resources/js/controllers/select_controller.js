b6fe0a7617411b7f6560d85596e2d9f8fef2d92a
Lucisu <46996317+Lucisu@users.noreply.github.com>
2024-08-28 16:04:44 -0300

    Update select_controller.js to add AJAX options

--- resources/js/controllers/select_controller.js
+++ resources/js/controllers/select_controller.js
@@ -18,14 +18,14 @@ export default class extends ApplicationController {
             plugins.push('clear_button');
         }
 
-        this.choices = new TomSelect(select, {
+        const options = {
             create: this.data.get('allow-add') === 'true',
             allowEmptyOption: true,
             maxOptions: 'null',
             placeholder: select.getAttribute('placeholder') === 'false' ? '' : select.getAttribute('placeholder'),
             preload: true,
             plugins,
-            maxItems: select.getAttribute('maximumSelectionLength') || (select.hasAttribute('multiple') ? null : 1),
+            maxItems: select.getAttribute('maximumSelectionLength') || select.hasAttribute('multiple') ? null : 1,
             render: {
                 option_create: (data, escape) => `<div class="create">${this.data.get('message-add')} <strong>${escape(data.input)}</strong>&hellip;</div>`,
                 no_results: () => `<div class="no-results">${this.data.get('message-notfound')}</div>`,
@@ -35,7 +35,51 @@ export default class extends ApplicationController {
                 this.setTextboxValue('');
                 this.refreshOptions(false);
             }
-        });
+        };
+
+        const ajaxOptionsUrl = select.getAttribute('ajaxoptionsurl');
+
+        if (ajaxOptionsUrl) {
+            options['valueField']  = select.getAttribute('ajaxvaluefield') ?? 'value';
+            options['labelField']  = select.getAttribute('ajaxlabelfield') ?? 'label';
+            options['searchField'] = [options['valueField'], options['labelField']];
+
+            options['load'] = async (query, callback) => {
+                axios
+                .get(ajaxOptionsUrl, { query })
+                .then((response) => {
+                    console.log(1);
+                    callback(response.data.items);
+                });
+            };
+        
+            options['render'] = {
+                option: function(item, escape) {
+                    return `<div class="py-2 d-flex">
+                                <div>
+                                    <div class="mb-1">
+                                        <span class="h4">
+                                            ${ escape(item[options['labelField']]) }
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>`;
+                },
+                item: function(item, escape) {
+                    return `<div class="py-2 d-flex">
+                                <div>
+                                    <div class="mb-1">
+                                        <span class="h4">
+                                            ${ escape(item[options['labelField']]) }
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>`;
+                }
+            }
+        }
+
+        this.choices = new TomSelect(select, options);
     }
 
     /**
