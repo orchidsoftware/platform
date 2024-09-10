09ec2c32ffe589a9a8251aeabb8d06644cbe6902
sergey <ssergdubovets@gmail.com>
2024-09-10 15:34:59 +0300

    Add test

---
+++ tests/App/Filters/AutofillFilter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Orchid\Tests\App\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+use Orchid\Filters\Autofill;
+use Orchid\Filters\Filter;
+use Orchid\Screen\Field;
+use Orchid\Screen\Fields\Input;
+
+class AutofillFilter extends Filter
+{
+	use Autofill;
+	
+    /**
+     * @var array
+     */
+    public $parameters = [
+        'value', 'inner.value'
+    ];
+
+    public function name(): string
+    {
+        return 'Autofill';
+    }
+
+    public function run(Builder $builder): Builder
+    {
+        return $builder;
+    }
+
+    /**
+     * @return Field[]
+     */
+    public function display(): array
+    {
+        return [
+            Input::make('value'),
+            Input::make('inner.value')
+        ];
+    }
+}
--- tests/Unit/Screen/Layouts/SelectionTest.php
+++ tests/Unit/Screen/Layouts/SelectionTest.php
@@ -7,6 +7,7 @@ namespace Orchid\Tests\Unit\Screen\Layouts;
 use Orchid\Filters\Filter;
 use Orchid\Screen\LayoutFactory;
 use Orchid\Screen\Repository;
+use Orchid\Tests\App\Filters\AutofillFilter;
 use Orchid\Tests\App\Filters\HiddenFilter;
 use Orchid\Tests\App\Layouts\GroupNameAndEmail;
 use Orchid\Tests\TestUnitCase;
@@ -33,6 +34,38 @@ class SelectionTest extends TestUnitCase
         collect($layout->filters())
             ->map(fn (string $filter) => resolve($filter))->each(function (Filter $filter) use ($html) {
                 $this->assertStringContainsString($filter->render(), (string) $html);
+            });
+    }
+
+    public function testDisplayAutofillFilters(): void
+    {
+        $layout = LayoutFactory::selection([
+            AutofillFilter::class,
+        ]);
+
+        // Test with empty request
+        $html = (string) $layout->build(new Repository);
+
+        collect($layout->filters())
+            ->map(fn (string $filter) => resolve($filter))->each(function (Filter $filter) use ($html) {
+                $this->assertStringContainsString($filter->render(), (string) $html);
+            });
+        
+        // Test with parameterized request
+        request()->merge([
+            'value' => 'test value',
+            'inner' => [
+                'value' => 'test inner value',
+            ]
+        ]);
+        
+        $htmlParameterized = (string) $layout->build(new Repository);
+        
+        collect($layout->filters())
+            ->map(fn (string $filter) => resolve($filter))->each(function (Filter $filter) use ($html, $htmlParameterized) {
+                $render = $filter->render();
+                $this->assertStringContainsString($render, $htmlParameterized);
+                $this->assertStringNotContainsString($render, $html);
             });
     }
 }
