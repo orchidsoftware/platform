dc50e5cf9e7fd198cc007e599e2fe7e64cc8b958
Alexandr Chernyaev <bliz48rus@gmail.com>
2024-10-23 18:30:28 +0300

    Fixed tests

--- resources/views/partials/layouts/td.blade.php
+++ resources/views/partials/layouts/td.blade.php
@@ -1,4 +1,4 @@
-<td class="text-balance text-{{$align}} @if(!$width) text-truncate @endif {{ $class }}"
+<td class="text-{{$align}} text-balance @if(!$width) text-truncate @endif {{ $class }}"
     data-column="{{ $slug }}" colspan="{{ $colspan }}"
         @style([
         "min-width:$width;" => $width,
