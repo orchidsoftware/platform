e09db0dc5faca7e86db7354e31b6ef2838ae748c
Alexandr Chernyaev <bliz48rus@gmail.com>
2024-11-24 01:24:42 +0300

    Adjust template paddings for improved layout consistency

--- resources/views/layouts/persona.blade.php
+++ resources/views/layouts/persona.blade.php
@@ -1,13 +1,11 @@
-<a href="{{ $url }}">
-    <div class="d-sm-flex flex-row flex-wrap align-items-center">
-        @empty(!$image)
-            <span class="thumb-sm avatar me-sm-3 ms-md-0 me-xl-3 d-none d-md-inline-block">
-              <img src="{{ $image }}" class="bg-light">
-            </span>
-        @endempty
-        <div class="mt-2 mt-sm-0 mt-md-2 mt-xl-0">
-            <p class="mb-0">{{ $title }}</p>
-            <small class="text-muted">{{ $subTitle }}</small>
-        </div>
+<a href="{{ $url }}" class="d-flex align-items-center gap-md-3">
+    @empty(!$image)
+    <span class="thumb-sm avatar d-none d-md-inline-block">
+      <img src="{{ $image }}" class="bg-light">
+    </span>
+    @endempty
+    <div class="text-balance">
+        <p class="mb-0">{{ $title }}</p>
+        <small class="text-muted">{{ $subTitle }}</small>
     </div>
 </a>
--- resources/views/partials/profile.blade.php
+++ resources/views/partials/profile.blade.php
@@ -1,8 +1,8 @@
 <div class="profile-container d-flex align-items-stretch p-3 rounded lh-sm position-relative overflow-hidden">
 
-    <a href="{{ route(config('platform.profile', 'platform.profile')) }}" class="col-10 d-flex align-items-center me-3">
+    <a href="{{ route(config('platform.profile', 'platform.profile')) }}" class="col-10 d-flex align-items-center gap-3">
         @if($image = Auth::user()->presenter()->image())
-            <img src="{{$image}}"  alt="{{ Auth::user()->presenter()->title()}}" class="thumb-sm avatar b me-3" type="image/*">
+            <img src="{{$image}}"  alt="{{ Auth::user()->presenter()->title()}}" class="thumb-sm avatar b" type="image/*">
         @endif
 
         <small class="d-flex flex-column lh-1 col-9">
