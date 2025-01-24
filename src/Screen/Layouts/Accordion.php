aa91eb4b4a58c8f2cbca9a529737f5db07d54a4f
Babaiev Ihor <75082815+bald-cat@users.noreply.github.com>
2025-01-24 14:10:49 +0200

    Added activeAccordion method to Accordion.php (#2939)
    * Added activeAccordion method to Accordion.php
    
    * Change activeAccordion method to Accordion.php
    
    * Fixed method
    
    * change open() method

--- src/Screen/Layouts/Accordion.php
+++ src/Screen/Layouts/Accordion.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Orchid\Screen\Layouts;
 
+use Illuminate\Support\Arr;
 use Orchid\Screen\Layout;
 use Orchid\Screen\Repository;
 
@@ -22,6 +23,7 @@ abstract class Accordion extends Layout
      */
     protected $variables = [
         'stayOpen' => false,
+        'open' => [],
     ];
 
     /**
@@ -32,6 +34,7 @@ abstract class Accordion extends Layout
     public function __construct(array $layouts = [])
     {
         $this->layouts = $layouts;
+        $this->variables['open'] = [array_key_first($this->layouts)];
     }
 
     /**
@@ -55,4 +58,19 @@ abstract class Accordion extends Layout
 
         return $this;
     }
+
+    /**
+     * Set active accordion(s).
+     *
+     * @param string|array $activeAccordion
+     *
+     * @return $this
+     */
+    public function open(string|array $activeAccordion): self
+    {
+        $this->variables['open'] = Arr::wrap($activeAccordion);
+
+        return $this;
+    }
+
 }
