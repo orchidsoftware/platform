616106202d6c0857401b4b59a830b0b54cb915d2
Alexandr Chernyaev <bliz48rus@gmail.com>
2025-02-03 16:47:44 +0300

    Improve code on accordion layout

--- resources/views/layouts/accordion.blade.php
+++ resources/views/layouts/accordion.blade.php
@@ -1,22 +1,29 @@
-<div id="accordion-{{$templateSlug}}" class="accordion mb-3">
+<div id="accordion-{{ $templateSlug }}" class="accordion mb-3">
     @foreach($manyForms as $name => $forms)
-        <div class="accordion-heading @if (!in_array($name, $open)) collapsed @endif"
-             id="heading-{{\Illuminate\Support\Str::slug($name)}}"
-             data-bs-toggle="collapse"
-             data-bs-target="#collapse-{{\Illuminate\Support\Str::slug($name)}}"
-             aria-expanded="{{ in_array($name, $open) ? 'true' : 'false' }}"
-             aria-controls="collapse-{{\Illuminate\Support\Str::slug($name)}}">
-            <button type="button" class="btn btn-link btn-group-justified pt-2 pb-2 mb-0 pe-0 ps-0 d-flex align-items-center">
-                <x-orchid-icon path="bs.chevron-right" class="small me-2"/> {!! $name !!}
-            </button>
-        </div>
+        @php
+            $collapseId = 'collapse-' . \Illuminate\Support\Str::slug($name);
+            $collapseIsOpen = in_array($name, $open);
+        @endphp
 
-        <div id="collapse-{{\Illuminate\Support\Str::slug($name)}}"
-             class="mt-2 collapse @if (in_array($name, $open)) show @endif"
-             aria-labelledby="heading-{{\Illuminate\Support\Str::slug($name)}}"
-             @if (!$stayOpen)
-                 data-bs-parent="#accordion-{{$templateSlug}}"
-                @endif
+        <a
+            href="#{{ $collapseId }}"
+            data-bs-target="#{{ $collapseId }}"
+            class="accordion-heading nav-link py-2 px-1 d-flex align-items-center"
+            data-bs-toggle="collapse"
+            aria-expanded="{{ $collapseIsOpen ? 'true' : 'false' }}"
+            role="button"
+            aria-controls="{{ $collapseId }}"
+        >
+            <x-orchid-icon path="bs.chevron-right" class="small me-2" />
+            {!! $name !!}
+        </a>
+
+        <div
+            id="{{ $collapseId }}"
+            class="mt-2 collapse @if ($collapseIsOpen) show @endif"
+            @if (! $stayOpen)
+                data-bs-parent="#accordion-{{ $templateSlug }}"
+            @endif
         >
             @foreach($forms as $form)
                 {!! $form !!}
