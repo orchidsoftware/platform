a846de142f557f7d4a2d459cdc3292569437ab85
Alexandr Chernyaev <bliz48rus@gmail.com>
2025-03-27 03:47:21 +0300

    refs #2978 Improve published stubs (#2982)
    * refs #2978 Improve published stubs
    * refs #2978 Added tests for publish stubs
    ---------
    
    Co-authored-by: tabuna <5102591+tabuna@users.noreply.github.com>

--- src/Platform/Commands/GeneratorCommand.php
+++ src/Platform/Commands/GeneratorCommand.php
@@ -7,13 +7,17 @@ use Orchid\Platform\Dashboard;
 abstract class GeneratorCommand extends \Illuminate\Console\GeneratorCommand
 {
     /**
+     * Resolve the fully-qualified path to the stub.
+     *
      * @param string $stub
      *
      * @return string
      */
     protected function resolveStubPath(string $stub): string
     {
-        return file_exists($path = $this->laravel->basePath('stubs/orchid/platform/'.trim($stub, '/')))
+        $path = $this->laravel->basePath('stubs/orchid/platform/'.trim($stub, '/'));
+
+        return file_exists($path)
             ? $path
             : Dashboard::path('stubs/'.$stub);
     }
---
+++ tests/Console/StubPublishCommandTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchid\Tests\Console;
+
+use Illuminate\Filesystem\Filesystem;
+use Orchid\Tests\TestConsoleCase;
+
+class StubPublishCommandTest extends TestConsoleCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->filesystem = new Filesystem();
+        $this->stubsPath = base_path('stubs/orchid/platform');
+        $this->filesystem->ensureDirectoryExists($this->stubsPath);
+    }
+
+    /**
+     * Clears the directory after each test.
+     */
+    protected function tearDown(): void
+    {
+        $this->filesystem->deleteDirectory($this->stubsPath);
+        parent::tearDown();
+    }
+
+    /**
+     * Test using the --force option to overwrite an existing file.
+     */
+    public function testForcePublish(): void
+    {
+        $file = 'screen.stub';
+
+        // Simulate an existing file.
+        file_put_contents($this->stubsPath.'/'.$file, 'Existing content');
+
+        $this->artisan('orchid:stubs', ['--force' => true])
+            ->expectsOutputToContain('Stubs published successfully.')
+            ->assertOk();
+
+        // Check that the file was overwritten.
+        $this->assertNotEquals('Existing content', file_get_contents($this->stubsPath.'/'.$file));
+    }
+
+    /**
+     * Test using the --existing option to overwrite only existing files.
+     */
+    public function testExistingPublish(): void
+    {
+        $file = 'screen.stub';
+
+        // Simulate an existing file.
+        file_put_contents($this->stubsPath.'/'.$file, 'Existing content');
+
+        $this->artisan('orchid:stubs', ['--existing' => true])
+            ->expectsOutputToContain('Stubs published successfully.')
+            ->assertOk();
+
+        // Check that the file was overwritten.
+        $this->assertNotEquals('Existing content', file_get_contents($this->stubsPath.'/'.$file));
+    }
+
+    /**
+     * Test without any options to check normal file publishing behavior.
+     */
+    public function testPublishWithoutOptions(): void
+    {
+        $this->artisan('orchid:stubs')
+            ->expectsOutputToContain('Stubs published successfully.')
+            ->assertOk();
+    }
+}
