36f4195a487c7ba21f4d9234d444c6f8b7026449
Alexandr Chernyaev <bliz48rus@gmail.com>
2025-03-31 02:39:48 +0300

    ## 14.50.0 - 2025-03-31

--- src/Platform/Dashboard.php
+++ src/Platform/Dashboard.php
@@ -23,7 +23,7 @@ class Dashboard
      *
      * @deprecated Use `Dashboard::version()` instead.
      */
-    public const VERSION = '14.49.0';
+    public const VERSION = '14.50.0';
 
     /**
      * @deprecated
