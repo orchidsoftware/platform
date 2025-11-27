534d297d4a9860ffa69ac12aa8b4300f66928ebe
Bandi Csongor <bandicsongor@gmail.com>
2025-11-27 12:21:31 +0200

    Avoid invalid image request in Firefox (adjustments for PR #3061) (#3064)
    Co-authored-by: csongor.bandi@lifeishard.ro <csongor.bandi@lifeishard.ro>

--- resources/views/fields/attach.blade.php
+++ resources/views/fields/attach.blade.php
@@ -56,7 +56,10 @@
                 <input type="hidden" name="{name}" value="{id}">
 
 
-                <img class="attach-image rounded border user-select-none overflow-hidden" src="{url}" title="{original_name}"/>
+                <img class="attach-image rounded border user-select-none overflow-hidden"
+                     src="{url}"
+                     loading="lazy"
+                     title="{original_name}"/>
 
                 {{--
                     <object class="attach-image rounded border user-select-none" border="0" data="{url}" type="{mime}" title="test"  load="lazy" controls allowfullscreen autoplay="false">
