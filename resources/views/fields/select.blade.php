14b63445dc806340545ef80225fcc1f949b7b72f
csongor.bandi@lifeishard.ro <csongor.bandi@lifeishard.ro>
2026-03-28 12:46:16 +0200

    feat: Enhance select component to support optgroup for nested options

--- resources/views/fields/select.blade.php
+++ resources/views/fields/select.blade.php
@@ -1,21 +1,30 @@
 @component($typeForm, get_defined_vars())
     <div data-controller="select"
-        data-select-placeholder="{{$attributes['placeholder'] ?? ''}}"
-        data-select-allow-empty="{{ $allowEmpty }}"
-        data-select-message-notfound="{{ __('No results found') }}"
-        data-select-allow-add="{{ var_export($allowAdd, true) }}"
-        data-select-message-add="{{ __('Add') }}"
+         data-select-placeholder="{{$attributes['placeholder'] ?? ''}}"
+         data-select-allow-empty="{{ $allowEmpty }}"
+         data-select-message-notfound="{{ __('No results found') }}"
+         data-select-allow-add="{{ var_export($allowAdd, true) }}"
+         data-select-message-add="{{ __('Add') }}"
     >
+        @php
+            $isSelected = fn ($k, $opt) => isset($value) && (
+                (is_array($value) && in_array($k, $value)) ||
+                (isset($value[$k]) && $value[$k] == $opt) ||
+                $k == $value
+            );
+        @endphp
+
         <select {{ $attributes }}>
             @foreach($options as $key => $option)
-                <option value="{{$key}}"
-                        @isset($value)
-                            @if (is_array($value) && in_array($key, $value)) selected
-                            @elseif (isset($value[$key]) && $value[$key] == $option) selected
-                            @elseif ($key == $value) selected
-                            @endif
-                        @endisset
-                >{{$option}}</option>
+                @if(is_string($key) && is_array($option))
+                    <optgroup label="{{ $key }}">
+                        @foreach($option as $subKey => $subOption)
+                            <option value="{{ $subKey }}" @selected($isSelected($subKey, $subOption))>{{ $subOption }}</option>
+                        @endforeach
+                    </optgroup>
+                @else
+                    <option value="{{ $key }}" @selected($isSelected($key, $option))>{{ $option }}</option>
+                @endif
             @endforeach
         </select>
     </div>
